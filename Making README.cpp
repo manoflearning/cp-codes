72e3139cdd3b7f5b0f3ccc5a65cd6580dd72ab15
manoflearning <66547669+manoflearning@users.noreply.github.com>
2020-08-09 18:37:32 +0900

    Update Making README

--- Making README.cpp
+++ Making README.cpp
@@ -5,6 +5,7 @@
 #include <list>
 #include <vector>
 #include <algorithm>
+#include <ctime>
 using namespace std;
 
 struct Contents {
@@ -18,11 +19,28 @@ struct Contents {
 
 vector<Contents> contents;
 
+void printTheNumOfSolvedProblem();
 int searchingDir(string path, int dep);
-int isFile(_finddata_t fd);
+bool isFile(_finddata_t fd);
 bool isExeFile(string s);
+void printLastRenewalTime();
 
 int main() {
+	cout<<"# PS-Codes\n";
+	cout<<"알고리즘 문제 정답 코드 저장용입니다. <br/>\n";
+
+	cout<<"<br/>\n";
+
+	printTheNumOfSolvedProblem();
+
+	cout<<"<br/>\n";
+
+	printLastRenewalTime();
+
+    return 0;
+}
+
+void printTheNumOfSolvedProblem() {
 	string path = "D:\\0JWK\\0programming\\PS-Codes\\Codes";
 	searchingDir(path, 0);
 
@@ -32,9 +50,7 @@ int main() {
 	for(int i = 1; i < contents.size(); i++) {
 		cout<<"**"<<contents[i].name<<"**에서 **"<<contents[i].cnt<<"**문제, <br/>\n";
 	}
-	cout<<"총합 **"<<contents[0].cnt<<"**문제를 풀었습니다.<br/>";
-
-    return 0;
+	cout<<"총합 **"<<contents[0].cnt<<"**문제를 풀었습니다.<br/>\n";
 }
 
 int searchingDir(string path, int dep) {
@@ -71,11 +87,25 @@ int searchingDir(string path, int dep) {
 	return ret;
 }
 
-int isFile(_finddata_t fd) {
+bool isFile(_finddata_t fd) {
 	if (fd.attrib & _A_SUBDIR) return 0;
 	else return 1;
 }
 
 bool isExeFile(string s) {
-	return s.back() == 'e';
+	if(s.back() == 'e') return 1;
+	else return 0;
+}
+
+void printLastRenewalTime() {
+	struct tm curr_tm;
+	time_t curr_time = time(nullptr);
+
+	localtime_r(&curr_time, &curr_tm);
+
+	int year = curr_tm.tm_year + 1900;
+	int month = curr_tm.tm_mon + 1;
+	int day = curr_tm.tm_mday;
+
+	cout<<'('<<year<<'/'<<month<<'/'<<day<<"에 마지막으로 갱신함.) <br/>\n";
 }
