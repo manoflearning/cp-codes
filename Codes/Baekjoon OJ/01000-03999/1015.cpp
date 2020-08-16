32bfa97a1aed4a78627860a18e2af5f39fb8e201
manoflearning <77jwk0724@gmail.com>
2020-08-17 03:04:27 +0900

    Update 6588

--- Codes/Baekjoon OJ/01000-03999/1015.cpp
+++ Codes/Baekjoon OJ/01000-03999/1015.cpp
@@ -10,43 +10,23 @@ const int INF = 1e9 + 7;
 const int MOD = 1e9 + 7;
 const int dy[] = { 0, 0, 1, -1 };
 const int dx[] = { 1, -1, 0, 0 };
-const int MAX = 1e6;
-
-bool isPrime[MAX + 1];
-vector<int> prime;
-
-void getPrime() {
-	fill(isPrime, isPrime + MAX + 1, true);
-
-	for (ll i = 4; i <= MAX; i += 2)
-		isPrime[i] = false;
-	for (ll i = 3; i <= MAX; i++) {
-		if (isPrime[i]) prime.push_back(i);
-		for (ll j = i * i; j <= MAX; j += i * 2)
-			isPrime[j] = false;
-	}
-}
 
 int main() {
     cin.tie(NULL); cout.tie(NULL);
     ios_base::sync_with_stdio(false);
     
-    getPrime();
+    int N; cin>>N;
+    vector<int> a(N);
+    for(int& i : a) cin>>i;
 
-    while(true) {
-        int x; cin>>x;
-        if(x==0) break;
+    vector<pii> b(N);
+    for(int i=0; i<N; i++) b[i] = {a[i], i};
 
-        bool yes = 0;
-        for(int i=0; i<prime.size() && prime[i]<=x-prime[i]; i++) {
-            if(isPrime[x-prime[i]]) {
-                cout<<x<<" = "<<prime[i]<<" + "<<x-prime[i]<<'\n';
-                yes = 1;
-                break;
-            }
-        }
-        if(!yes) cout<<"Goldbach's conjecture is wrong.\n";
-    }
+    sort(b.begin(), b.end());
+
+    vector<int> res(N);
+    for(int i=0; i<N; i++) res[b[i].second] = i;
+    for(int i=0; i<N; i++) cout<<res[i]<<' ';
 
     return 0;
 }
