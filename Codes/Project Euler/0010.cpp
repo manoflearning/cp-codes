27801090c19d6ca19c642f2b5d24d02d9ea4b340
Jeong Woo-kyung <77jwk0724@gmail.com>
2020-11-22 12:37:08 +0900

    Create 0010.cpp

---
+++ Codes/Project Euler/0010.cpp
@@ -0,0 +1,39 @@
+#define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+#include <cassert>
+using namespace std;
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+const int INF = 1e9 + 7;
+const int MOD = 1e9 + 7;
+const int dy[] = { 0, 0, 1, -1 };
+const int dx[] = { 1, -1, 0, 0 };
+const int MAX = 2e6;
+
+bool isPrime[MAX + 1];
+ll res=2;
+
+void getPrime() {
+    fill(isPrime, isPrime + MAX + 1, true);
+
+    for (ll i = 4; i <= MAX; i += 2)
+        isPrime[i] = false;
+    for (ll i = 3; i <= MAX; i++) {
+        if (isPrime[i]) res+=i;
+        for (ll j = i * i; j <= MAX; j += i * 2)
+            isPrime[j] = false;
+    }
+}
+
+int main() {
+    cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    
+    getPrime();
+    
+    cout<<res;
+    
+    return 0;    
+}
