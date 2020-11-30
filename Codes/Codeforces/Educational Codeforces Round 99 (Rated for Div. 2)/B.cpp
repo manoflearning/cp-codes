7d59630b9bc647ebbeb2e3bb339150caa6689f49
manoflearning <77jwk0724@gmail.com>
2020-12-01 01:37:01 +0900

    codeforces

---
+++ Codes/Codeforces/Educational Codeforces Round 99 (Rated for Div. 2)/B.cpp
@@ -0,0 +1,32 @@
+#define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+#include <cassert>
+using namespace std;
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+const int INF = 1e9 + 7;
+const int MOD = 1e9 + 7;
+const int dy[] = { 0, 0, 1, -1 };
+const int dx[] = { 1, -1, 0, 0 };
+const int MAX = 1e6;
+
+int main() {
+	cin.tie(NULL); cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int tc; cin >> tc;
+
+	while (tc--) {
+		int x; cin >> x;
+
+		int f = 1;
+		while (f * (f + 1) / 2 < x) f++;
+
+		int left = f * (f + 1) / 2 - x;
+		cout << f + (left == 1) << '\n';
+	}
+
+	return 0;
+}
