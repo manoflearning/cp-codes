f9de512a0463d625c18e0c8744a9e2b9cefeb004
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-01-04 23:21:27 +0900

    Update 4796.cpp

--- Codes/Baekjoon OJ/04000-06999/4796.cpp
+++ Codes/Baekjoon OJ/04000-06999/4796.cpp
@@ -1,22 +1,32 @@
-#include <iostream>
-#include <algorithm>
-#define ll long long
+#define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+#include <cassert>
 using namespace std;
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
 
-ll L, P, V;
+const int INF = 1e9 + 7;
+const int MOD = 1e9 + 7;
+const int dy[] = { 0, 0, 1, -1 };
+const int dx[] = { 1, -1, 0, 0 };
 
 int main() {
 	cin.tie(NULL); cout.tie(NULL);
 	ios_base::sync_with_stdio(false);
-
-	for (int test = 1; ; test++) {
-		cin >> L >> P >> V;
-
-		if (L == 0) break;
-
-		ll ans = (V / P) * L + min((V % P), L);
-		cout << "Case " << test << ": " << ans << '\n';
+	
+	for (int tc = 1; ; tc++) {
+	    int l, p, v;
+	    cin >> l >> p >> v;
+	    
+	    if (l == 0 && p == 0 && v == 0) break;
+	    
+	    int a = v / p, b = v % p;
+	    
+	    int ans = l * a + min(b, l);
+	    
+	    cout << "Case " << tc << ": " << ans << '\n';
 	}
-
+	
 	return 0;
 }
