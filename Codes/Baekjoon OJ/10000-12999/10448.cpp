53541eb5638b83f24a130411e304e413a16336d0
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-01-04 23:09:42 +0900

    Update 10448.cpp

--- Codes/Baekjoon OJ/10000-12999/10448.cpp
+++ Codes/Baekjoon OJ/10000-12999/10448.cpp
@@ -1,39 +1,46 @@
-#include <iostream>
+#define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+#include <cassert>
+using namespace std;
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
 
-#define gauss(x) (x)*(x+1)/2
+const int INF = 1e9 + 7;
+const int MOD = 1e9 + 7;
+const int dy[] = { 0, 0, 1, -1 };
+const int dx[] = { 1, -1, 0, 0 };
 
-int exactly_three(int K);
+vector<int> a;
 
 int main() {
-	int N;
+	cin.tie(NULL); cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
 	
-	std::cin>>N;
-	
-	for(int i=0; i<N; i++) {
-		int K;
-		std::cin>>K;
-		
-		std::cout<<exactly_three(K)<<'\n';
+	for (int i = 1; i * (i + 1) / 2 <= 1000; i++) {
+	    a.push_back(i * (i + 1) / 2);
 	}
 	
-	return 0;
-}
-
-int exactly_three(int K) {
-	int mn=2;
+	int t; cin >> t;
 	
-	for(int i=1; ; i++)
-		if(gauss(i)>=K) {
-			mn = i-1;
-			break;
-		}
-
-	for(int i=1; i<=mn; i++)
-		for(int l=1; l<=mn; l++)
-			for(int j=1; j<=mn; j++) {
-				if(gauss(i) + gauss(l) + gauss(j) == K)
-					return 1;
-			}
-			
+	while (t--) {
+	    int n; cin >> n;
+	    
+	    int ans = 0;
+	    
+	    for (int i = 0; i < a.size(); i++) {
+	        for (int j = i; j < a.size(); j++) {
+	            for (int k = j; k < a.size(); k++) {
+	                if (a[i] + a[j] + a[k] == n) {
+	                    ans = 1;
+	                    i = j = k = a.size() - 1;
+	                }
+	            }
+	        }
+	    }
+	    
+	    cout << ans << '\n';
+	}
+	
 	return 0;
 }
