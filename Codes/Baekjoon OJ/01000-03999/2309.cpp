7de63f85416dff4fadb0a88e3c0f17fc02339982
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-01-04 22:43:04 +0900

    Update 2309.cpp

--- Codes/Baekjoon OJ/01000-03999/2309.cpp
+++ Codes/Baekjoon OJ/01000-03999/2309.cpp
@@ -1,30 +1,47 @@
-#include <iostream>
-#include <algorithm>
+#define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+#include <cassert>
+using namespace std;
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+const int INF = 1e9 + 7;
+const int MOD = 1e9 + 7;
+const int dy[] = { 0, 0, 1, -1 };
+const int dx[] = { 1, -1, 0, 0 };
+
+int a[9], sum;
+vector<int> b;
 
 int main() {
-	int nine_dwarf[9];
+	cin.tie(NULL); cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
 	
-	for(int i=0; i<9; i++)
-		std::cin>>nine_dwarf[i];
-		
-	std::sort(nine_dwarf, nine_dwarf+9);
+	for (int i = 0; i < 9; i++) {
+	    cin >> a[i];
+	    sum += a[i];
+	}
+	    
+	    
+	for (int i = 0; i < 9; i++) {
+	    for (int j = i + 1; j < 9; j++) {
+	        int res = sum - a[i] - a[j];
+	        
+	        if (res == 100) {
+	            for (int k = 0; k < 9; k++) {
+	                if (k == i || k == j) continue;
+	                b.push_back(a[k]);
+	            }
+	            i = j = 9;
+	            break;
+	        }
+	    }
+	}
 	
-	for(int i=0; i<8; i++)
-		for(int l=i+1; l<9; l++) {
-			int sum = 0;
-			
-			for(int k=0; k<9; k++)
-				if(k!=i && k!=l)
-					sum+=nine_dwarf[k];
-			
-			if(sum==100) {
-				for(int k=0; k<9; k++)
-					if(k!=i && k!=l)
-						std::cout<<nine_dwarf[k]<<'\n';
-						
-				i=8, l=9;
-			}
-		}
+	sort(b.begin(), b.end());
+	
+	for (int i : b) cout << i << '\n';
 	
 	return 0;
 }
