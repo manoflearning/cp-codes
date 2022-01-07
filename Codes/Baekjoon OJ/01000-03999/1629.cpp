85b7fa839909e2dde215b19f196b1b6efe441516
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-01-07 23:01:17 +0900

    Update 1629.cpp

--- Codes/Baekjoon OJ/01000-03999/1629.cpp
+++ Codes/Baekjoon OJ/01000-03999/1629.cpp
@@ -1,25 +1,38 @@
-#include <iostream>
-#define ll long long
+#define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+#include <cassert>
 using namespace std;
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
 
-ll A, C;
+const int INF = 1e9 + 7;
+const int MOD = 1e9 + 7;
+const int dy[] = { 0, 0, 1, -1 };
+const int dx[] = { 1, -1, 0, 0 };
 
-ll f(int x); //f(x) := A^x mod C
+ll a, b, c;
+
+void input() {
+    cin >> a >> b >> c;
+}
+
+ll f(ll b) {
+    if (b == 1) return a % c;
+    
+    ll ret = f(b / 2);
+    
+    if (b & 1) return (ret * ret % c) * a % c;
+    else return ret * ret % c;
+}
 
 int main() {
 	cin.tie(NULL); cout.tie(NULL);
 	ios_base::sync_with_stdio(false);
-
-	int B;
-	cin >> A >> B >> C;
-
-	cout << f(B) << '\n';
-
+	
+	input();
+	
+	cout << f(b);
+	
 	return 0;
-}
-
-ll f(int x) {
-	if (x == 1) return A % C;
-	ll prv = f(x >> 1);
-	return (prv * prv % C) * (x % 2 ? A : 1) % C;
 }
