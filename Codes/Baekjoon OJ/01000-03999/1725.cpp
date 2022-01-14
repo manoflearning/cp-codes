6a90affdc885772e193e22f2f1b7b50302c0bc3a
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-01-14 20:25:02 +0900

    Update 1725.cpp

--- Codes/Baekjoon OJ/01000-03999/1725.cpp
+++ Codes/Baekjoon OJ/01000-03999/1725.cpp
@@ -58,56 +58,60 @@ int main() {
 }
 
 //stack
-/*#include <iostream>
-#include <stack>
-#include <algorithm>
+/*#define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+#include <cassert>
 using namespace std;
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
 
-const int MAX = 100000;
+const int INF = 1e9 + 7;
+const int MOD = 1e9 + 7;
+const int dy[] = { 0, 0, 1, -1 };
+const int dx[] = { 1, -1, 0, 0 };
 
-int n;
-int histo[MAX];
-int leftHisto[MAX], rightHisto[MAX];
+int n, a[100005];
+stack<int> st;
+
+void input() {
+    cin >> n;
+    for (int i = 0; i < n; i++) cin >> a[i];
+}
+
+int f() {
+    int ret = 0;
+    
+    for (int i = 0; i < n; i++) {
+        while (!st.empty() && a[st.top()] >= a[i]) {
+            int x = st.top();
+            st.pop();
+            
+            ret = max(ret, a[x] * (i - 1 - (!st.empty() ? st.top() : -1)));
+        }
+        
+        //ret = max(ret, a[i] * (i - (!st.empty() ? st.top() : -1)));
+            
+        st.push(i);
+    }
+    
+    while (!st.empty()) {
+        int x = st.top();
+        st.pop();
+            
+        ret = max(ret, a[x] * (n - 1 - (!st.empty() ? st.top() : -1)));
+    }
+    
+    return ret;
+}
 
 int main() {
-	scanf("%d", &n);
-
-	for (int i = 0; i < n; i++)
-		scanf("%d", &histo[i]);
-
-	for (int i = 0; i < n; i++)
-		leftHisto[i] = -1, rightHisto[i] = n;
-
-	stack<int> height;
-
-	for (int i = 0; i < n; i++) {
-		while (true) {
-			if (height.empty()) break;
-			else if (histo[i] > histo[height.top()]) {
-				leftHisto[i] = height.top();
-				break;
-			}
-			else if (histo[i] <= histo[height.top()]) {
-				rightHisto[height.top()] = i;
-				height.pop();
-			}
-		}
-		
-		height.push(i);
-	}
-
-	int ans = 0;
-	for (int i = 0; i < n; i++)
-		ans = max(ans, (rightHisto[i] - leftHisto[i] - 1) * histo[i]);
-
-	printf("%d", ans);
-
+	cin.tie(NULL); cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	
+	input();
+	
+	cout << f();
+	
 	return 0;
 }*/
-/*//////////////////////////////////////////////////////////////////////
-문제 해법		: 스택의 활용
-접근 방식		: 
-결정적 깨달음		: 
-오답 원인		: 1. 
-				  2. 
-*///////////////////////////////////////////////////////////////////////
