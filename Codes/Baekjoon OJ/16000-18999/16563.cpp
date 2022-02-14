c0898d29d0cdb33987206e6dcb4bee13220e3453
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-02-14 12:48:54 +0900

    Update 16563.cpp

--- Codes/Baekjoon OJ/16000-18999/16563.cpp
+++ Codes/Baekjoon OJ/16000-18999/16563.cpp
@@ -1,4 +1,84 @@
-// 내가 생각한 풀이
+// 정해
+#define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+#include <cassert>
+using namespace std;
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define fr first
+#define sc second
+#define vt vector
+#define pb push_back
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+
+#define EXPAND( x ) x // Use this if MS Visual Studio doesn't expand __VA_ARGS__ correctly
+#define F_OR(i, a, b, s) for (int i = (a); (s) > 0 ? i < (b) : i > (b); i += (s))
+#define F_OR1(e) F_OR(i, 0, e, 1)
+#define F_OR2(i, e) F_OR(i, 0, e, 1)
+#define F_OR3(i, b, e) F_OR(i, b, e, 1)
+#define F_OR4(i, b, e, s) F_OR(i, b, e, s)
+#define GET5(a, b, c, d, e, ...) e
+#define F_ORC(...) EXPAND( GET5(__VA_ARGS__, F_OR4, F_OR3, F_OR2, F_OR1) )
+#define FOR(...) EXPAND( F_ORC(__VA_ARGS__ )(__VA_ARGS__) )
+#define EACH(x, a) for (auto& x : a)
+
+const int INF = 1e9 + 7;
+const int MOD = 1e9 + 7;
+const int dy[] = { 0, 0, 1, -1, 1, 1, -1, -1 };
+const int dx[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+const int MAX = 5e6;
+
+int mnFac[MAX + 1];
+int n, a[(int)1e6 + 5];
+
+void getPrime() {
+	FOR(i, 2, MAX + 1) mnFac[i] = i;
+
+	for (ll i = 2; i <= MAX; i++) {
+		if (mnFac[i] == i) {
+			for (ll j = i * i; j <= MAX; j += i) 
+				if (mnFac[j] == j) mnFac[j] = i;
+		}
+	}
+}
+
+void input() {
+	cin >> n;
+	FOR(n) cin >> a[i];
+}
+
+void f(int x) {
+	while (x > 1) {
+		cout << mnFac[x] << ' ';
+		x /= mnFac[x];
+	}
+}
+
+void solve() {
+	getPrime();
+
+	input();
+
+	FOR(n) {
+		f(a[i]);
+		cout << '\n';
+	}
+}
+
+int main() {
+	cin.tie(NULL); cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	solve();
+
+	return 0;
+}
+
+// 내 풀이
 #define _USE_MATH_DEFINES
 #include <bits/stdc++.h>
 #include <cassert>
