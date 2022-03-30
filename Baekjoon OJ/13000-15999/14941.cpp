7d5797c7e298855780184eed2a0c7493eb79c474
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-03-30 20:03:12 +0900

    Create 14941.cpp

---
+++ Baekjoon OJ/13000-15999/14941.cpp
@@ -0,0 +1,97 @@
+#define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+#include <cassert>
+using namespace std;
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define fr first
+#define sc second
+#define vt vector
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+
+#define EXPAND( x ) x // Use this if MS Visual Studio doesn't expand __VA_ARGS__ correctly
+#define F_OR(i, a, b, s) for (int i = (a); (s) > 0 ? i < (b) : i > (b); i += (s))
+#define F_OR1(e) F_OR(i, 0, e, 1)
+#define F_OR2(i, e) F_OR(i, 0, e, 1)
+#define F_OR3(i, b, e) F_OR(i, b, e, 1)
+#define F_OR4(i, b, e, s) F_OR(i, b, e, s)
+#define GET5(a, b, c, d, e, ...) e
+#define F_ORC(...) EXPAND( GET5(__VA_ARGS__, F_OR4, F_OR3, F_OR2, F_OR1) )
+#define FOR(...) EXPAND( F_ORC(__VA_ARGS__ )(__VA_ARGS__) )
+#define EACH(x, a) for (auto& x : a)
+
+const double EPS = 1e-9;
+const int INF = 1e9 + 7;
+const int MOD = 1e9 + 7;
+const int dy[] = { 0, 0, 1, -1, 1, 1, -1, -1 };
+const int dx[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+const int MAX = 1e5;
+
+bool isPrime[MAX + 1], mul3[MAX + 1];
+vector<int> prime(1, 2);
+ll psum1[MAX + 1], psum2[MAX + 1];
+
+void getPrime() {
+	fill(isPrime + 2, isPrime + MAX + 1, 1);
+
+	for (ll i = 4; i <= MAX; i += 2)
+		isPrime[i] = 0;
+	for (ll i = 3; i <= MAX; i++) {
+		if (isPrime[i]) prime.push_back(i);
+		for (ll j = i * i; j <= MAX; j += i * 2)
+			isPrime[j] = 0;
+	}
+}
+
+void init() {
+    getPrime();
+
+    FOR(sz(prime)) {
+        int p = prime[i];
+        if (i % 2 == 0) {
+            mul3[p] = 1;
+            psum1[p] += 3 * p;
+            psum2[p] -= p;
+        }
+        else {
+            psum1[p] -= p;
+            psum2[p] += 3 * p;
+        }
+    }
+
+    FOR(i, 1, MAX + 1) {
+        psum1[i] += psum1[i - 1];
+        psum2[i] += psum2[i - 1];
+    }
+}
+
+int main() {
+	#ifndef ONLINE_JUDGE
+	// Enter the absolute path of the local file input.txt, output.txt
+	// Or just enter the "input.txt", "output.txt"
+    freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
+    freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
+	#endif
+
+	cin.tie(NULL); cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+    init();
+
+    int tc; cin >> tc;
+
+    FOR(tc) {
+        int a, b;
+        cin >> a >> b;
+
+        int st = *lower_bound(all(prime), a);
+        if (mul3[st]) cout << psum1[b] - psum1[a - 1] << '\n';
+        else cout << psum2[b] - psum2[a - 1] << '\n';
+    }
+
+	return 0;
+}
