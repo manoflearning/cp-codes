af166274037fbaa4cf33177f2734536b97cf8b35
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-04-01 08:56:30 +0900

    Create 10655.cpp

---
+++ Baekjoon OJ/10000-12999/10655.cpp
@@ -0,0 +1,67 @@
+#define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+#include <cassert>
+using namespace std;
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define fr first
+#define sc second
+#define vt vector
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+
+#define EXPAND( x ) x // Use this if MS Visual Studio doesn't expand __VA_ARGS__ correctly
+#define F_OR(i, a, b, s) for (int i = (a); (s) > 0 ? i < (b) : i > (b); i += (s))
+#define F_OR1(e) F_OR(i, 0, e, 1)
+#define F_OR2(i, e) F_OR(i, 0, e, 1)
+#define F_OR3(i, b, e) F_OR(i, b, e, 1)
+#define F_OR4(i, b, e, s) F_OR(i, b, e, s)
+#define GET5(a, b, c, d, e, ...) e
+#define F_ORC(...) EXPAND( GET5(__VA_ARGS__, F_OR4, F_OR3, F_OR2, F_OR1) )
+#define FOR(...) EXPAND( F_ORC(__VA_ARGS__ )(__VA_ARGS__) )
+#define EACH(x, a) for (auto& x : a)
+
+const double EPS = 1e-9;
+const int INF = 1e9 + 7;
+const int MOD = 1e9 + 7;
+const int dy[] = { 0, 0, 1, -1, 1, 1, -1, -1 };
+const int dx[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+int n;
+pii a[101010];
+ll ans, mx;
+
+int main() {
+	#ifndef ONLINE_JUDGE
+	// Enter the absolute path of the local file input.txt, output.txt
+	// Or just enter the "input.txt", "output.txt"
+    freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
+    freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
+	#endif
+
+	cin.tie(NULL); cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+    cin >> n;
+    FOR(n) {
+        cin >> a[i].fr >> a[i].sc;
+    }
+
+    FOR(i, 1, n) {
+        if (i != n - 1) {
+            ll res = 0;
+            res += fabs(a[i].fr - a[i - 1].fr) + fabs(a[i].sc - a[i - 1].sc);
+            res += fabs(a[i + 1].fr - a[i].fr) + fabs(a[i + 1].sc - a[i].sc);
+            res -= fabs(a[i + 1].fr - a[i - 1].fr) + fabs(a[i + 1].sc - a[i - 1].sc);
+            mx = max(mx, res);
+        }
+
+        ans += fabs(a[i].fr - a[i - 1].fr) + fabs(a[i].sc - a[i - 1].sc);
+    }
+
+    cout << ans - mx;
+
+	return 0;
+}
