1e7b7d898765d56c078ebe78b81b422050a61c4d
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-04-06 18:13:00 +0900

    .

--- Baekjoon OJ/10000-12999/10090.cpp
+++ Baekjoon OJ/10000-12999/10090.cpp
@@ -29,6 +29,47 @@ const int MOD = 1e9 + 7;
 const int dy[] = { 0, 0, 1, -1, 1, 1, -1, -1 };
 const int dx[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
+struct Seg {
+	int flag;  // array size
+	vector<int> t;
+
+	void build(int N) {
+		for (flag = 1; flag < N; flag <<= 1);
+		t.resize(2 * flag);
+	}
+	void modify(int p) {  // set value at position p
+		for (t[p += flag - 1] = 1; p > 1; p >>= 1) t[p >> 1] = t[p] + t[p ^ 1];
+	}
+	ll query(int l, int r) {
+		return query(l, r, 1, 1, flag);
+	}
+	ll query(int l, int r, int n, int nl, int nr) {  // sum on interval [l, r]
+		if (r < nl || nr < l) return 0;
+		if (l <= nl && nr <= r) return t[n];
+
+		int mid = (nl + nr) / 2;
+		return query(l, r, n << 1, nl, mid) + query(l, r, n << 1 | 1, mid + 1, nr);
+	}
+}seg;
+
+int n;
+vt<int> a;
+
+void input() {
+    cin >> n;
+    a.resize(n + 1);
+    FOR(n) cin >> a[i + 1];
+}
+
+ll f() {
+    ll ret = 0;
+    FOR(i, 1, n + 1) {
+        ret += seg.query(a[i] + 1, n);
+        seg.modify(a[i]);
+    }
+    return ret;
+}
+
 int main() {
 	#ifndef ONLINE_JUDGE
 	// Enter the absolute path of the local file input.txt, output.txt
@@ -40,7 +81,11 @@ int main() {
 	cin.tie(NULL); cout.tie(NULL);
 	ios_base::sync_with_stdio(false);
 
-	
+	input();
+
+    seg.build(n);
+
+    cout << f();
 
 	return 0;
 }
