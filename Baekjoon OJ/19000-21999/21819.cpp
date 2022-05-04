5244fb1b0e2c718725a436a5bea037eb35530e5c
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-05-04 18:52:48 +0900

    .

---
+++ Baekjoon OJ/19000-21999/21819.cpp
@@ -0,0 +1,77 @@
+#define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+#include <cassert>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define fr first
+#define sc second
+#define vt vector
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+
+#define EXPAND( x ) x // Use this if MS Visual Studio doesn't expand __VA_ARGS__ correctly
+#define F_OR(i, a, b, s) for (int i = (a); (s) > 0 ? i < (b) : i > (b); i += (s))
+#define F_OR1(e) F_OR(i, 0, e, 1)
+#define F_OR2(i, e) F_OR(i, 0, e, 1)
+#define F_OR3(i, b, e) F_OR(i, b, e, 1)
+#define F_OR4(i, b, e, s) F_OR(i, b, e, s)
+#define GET5(a, b, c, d, e, ...) e
+#define F_ORC(...) EXPAND( GET5(__VA_ARGS__, F_OR4, F_OR3, F_OR2, F_OR1) )
+#define FOR(...) EXPAND( F_ORC(__VA_ARGS__ )(__VA_ARGS__) )
+#define EACH(x, a) for (auto& x : a)
+
+const double EPS = 1e-9;
+const int INF = 1e9 + 7;
+const int MOD = 1e9 + 7;
+const int dy[] = { 0, 0, 1, -1, 1, 1, -1, -1 };
+const int dx[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+ll n, k, L;
+vt<ll> a;
+
+void input() {
+    cin >> n >> k >> L;
+    a.resize(n);
+    EACH(i, a) cin >> i;
+}
+
+int main() {
+	#ifndef ONLINE_JUDGE
+	// Enter the absolute path of the local file input.txt, output.txt
+	// Or just enter the "input.txt", "output.txt"
+    freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
+    freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
+	#endif
+
+	cin.tie(NULL); cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	input();
+
+    sort(a.rbegin(), a.rend());
+
+    ll l = 0, r = n;
+
+    while (l < r) {
+        ll h = (l + r + 1) / 2;
+        
+        ll res = 1, cnt = 0;
+        
+        FOR(i, h) {
+            if (h - a[i] > k) res = 0;
+            
+            if (h > a[i]) cnt += h - a[i];
+        }
+
+        if (res && cnt <= k * L) l = h;
+        else r = h - 1;
+    }
+
+    cout << l;
+
+	return 0;
+}
