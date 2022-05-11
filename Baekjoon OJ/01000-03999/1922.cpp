7a346a9fd2856b4b75e8ef17a7cd7b1df0eb5507
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-05-11 14:30:49 +0900

    .

--- Baekjoon OJ/01000-03999/1922.cpp
+++ Baekjoon OJ/01000-03999/1922.cpp
@@ -1,68 +1,101 @@
-#include <iostream>
-#include <vector>
-#include <cstring>
-#include <algorithm>
+#define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+#include <cassert>
 using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define fr first
+#define sc second
+#define vt vector
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
 
-const int MAX = 1e5;
+#define EXPAND( x ) x // Use this if MS Visual Studio doesn't expand __VA_ARGS__ correctly
+#define F_OR(i, a, b, s) for (int i = (a); (s) > 0 ? i < (b) : i > (b); i += (s))
+#define F_OR1(e) F_OR(i, 0, e, 1)
+#define F_OR2(i, e) F_OR(i, 0, e, 1)
+#define F_OR3(i, b, e) F_OR(i, b, e, 1)
+#define F_OR4(i, b, e, s) F_OR(i, b, e, s)
+#define GET5(a, b, c, d, e, ...) e
+#define F_ORC(...) EXPAND( GET5(__VA_ARGS__, F_OR4, F_OR3, F_OR2, F_OR1) )
+#define FOR(...) EXPAND( F_ORC(__VA_ARGS__ )(__VA_ARGS__) )
+#define EACH(x, a) for (auto& x : a)
 
-struct nnd {
-	int n1, n2, d;
+const double EPS = 1e-9;
+const int INF = 1e9 + 7;
+const int MOD = 1e9 + 7;
+const int dy[] = { 0, 0, 1, -1, 1, 1, -1, -1 };
+const int dx[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+const int MAX = 202020;
+
+struct wv {
+	ll w;
+	int v;
 };
 
-int N, M, p[MAX + 5];
-nnd E[MAX + 5];
+struct wuv {
+	ll w;
+	int u, v;
+	bool operator<(const wuv& rhs) const {
+		return w < rhs.w;
+	}
+};
 
-bool cmp(nnd a, nnd b) {
-	return a.d < b.d;
+int n, m;
+vt<wuv> a;
+vt<wv> adj[MAX];
+
+void input() {
+	cin >> n >> m;
+	a.resize(m);
+	for (auto& i : a) 
+	cin >> i.u >> i.v >> i.w;
 }
 
-int find(int x);
-void merge(int a, int b);
-
-int main() {
-	cin.tie(NULL); cout.tie(NULL);
-	ios_base::sync_with_stdio(false);
-
-	memset(p, -1, sizeof(p));
-
-	cin >> N >> M;
-
-	for (int i = 0; i < M; i++) {
-		cin >> E[i].n1 >> E[i].n2 >> E[i].d;
-	}
-	//간선의 가중치를 기준으로 오름차순 정렬
-	sort(E, E + M, cmp);
-
-	int ans = 0;
-	//간선을 차례로 순회
-	for (int i = 0; i < M; i++) {
-		//만약 간선의 두 정점이 같은 컴포넌트에 포함되어 있다면, 간선을 추가하지 않음.
-		if (find(E[i].n1) == find(E[i].n2)) continue;
-		ans += E[i].d;
-		merge(E[i].n1, E[i].n2);
-	}
-
-	cout << ans;
-
-	return 0;
-}
+vector<int> p(MAX + 1, -1);
 
 int find(int x) {
 	if (p[x] < 0) return x;
-	p[x] = find(p[x]);
-	return p[x];
+	return p[x] = find(p[x]);
 }
+
 void merge(int a, int b) {
 	int A = find(a), B = find(b);
 	if (A == B) return;
 	p[A] += p[B];
 	p[B] = A;
 }
-/*////////////////////////////////////////////////////////////////////
-문제 해법		: 최소 스패닝 트리, 크루스칼 알고리즘
-결정적 깨달음		: 
-시간복잡도		: O(|E|lg|E|)
-오답 원인		: 1. 
-				  2. 
-*/////////////////////////////////////////////////////////////////////
+
+int main() {
+	#ifndef ONLINE_JUDGE
+	// Enter the absolute path of the local file input.txt, output.txt
+	// Or just enter the "input.txt", "output.txt"
+    freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
+    freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
+	#endif
+
+	cin.tie(NULL); cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	
+	input();
+	
+	sort(a.begin(), a.end());
+
+	ll ans = 0;
+
+	EACH(i, a) {
+		if (find(i.u) == find(i.v)) continue;
+		merge(i.u, i.v);
+		adj[i.u].push_back({ i.w, i.v });
+		adj[i.v].push_back({ i.w, i.u });
+		ans += i.w;
+	}
+
+	cout << ans;
+
+	return 0;
+}
