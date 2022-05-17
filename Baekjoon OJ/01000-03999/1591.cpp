41755f6cbd0f804a1bfda941e5d3b25faf2732a1
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-05-17 19:03:15 +0900

    .

--- Baekjoon OJ/01000-03999/1591.cpp
+++ Baekjoon OJ/01000-03999/1591.cpp
@@ -30,6 +30,70 @@ const int MOD = 1e9 + 7;
 const int dy[] = { 0, 0, 1, -1, 1, 1, -1, -1 };
 const int dx[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
+int n, m;
+vt<int> a[1010];
+pair<vt<int>, vt<int>> suba[1010];
+pii edge[1010];
+vt<vt<int>> b;
+
+struct vidx {
+    int v, idx;
+};
+
+int N, M;
+vt<vidx> adj[2020];
+int ind[2020], outd[2020], nxt[2020], vi[2020][2020];
+vt<int> ans;
+
+void input() {
+    cin >> n >> m;
+    FOR(n - m + 1) {
+        a[i].resize(m);
+        EACH(j, a[i]) cin >> j;
+    }
+}
+
+void cc() {
+    FOR(n - m + 1) {
+        vt<int> v, u;
+        FOR(j, m) {
+            if (j < m - 1) v.push_back(a[i][j]);
+            if (j > 0) u.push_back(a[i][j]);
+        }
+
+        suba[i] = { v, u };
+        b.push_back(v);
+        b.push_back(u);
+    }
+
+    sort(all(b));
+    b.erase(unique(all(b)), b.end());
+
+    FOR(n - m + 1) {
+        edge[i].fr = lower_bound(all(b), suba[i].fr) - b.begin();
+        edge[i].sc = lower_bound(all(b), suba[i].sc) - b.begin();
+    }
+}
+
+void makingGraph() {
+    N = sz(b), M = n - m + 1;
+    FOR(M) {
+        adj[edge[i].fr].push_back({ edge[i].sc, i });
+        ind[edge[i].sc]++;
+        outd[edge[i].fr]++;
+    }
+}
+
+void dfs(int now) {
+    for (int& i = nxt[now]; i < sz(adj[now]); i++) {
+        if (i < sz(adj[now]) && !vi[now][i]) {
+            vi[now][i] = 1;
+            dfs(adj[now][i].v);
+        }
+    }
+    ans.push_back(now);
+}
+
 int main() {
 	#ifndef ONLINE_JUDGE
 	// Enter the absolute path of the local file input.txt, output.txt
@@ -41,7 +105,24 @@ int main() {
 	cin.tie(NULL); cout.tie(NULL);
 	ios_base::sync_with_stdio(false);
 	
-	
+	input();
+
+    cc();
+
+    makingGraph();
+
+    FOR(i, N) {
+        if (ind[i] + 1 == outd[i] && !vi[i][0]) {
+            dfs(i);
+        }
+    }
+
+    if (ans.empty()) dfs(0);
+
+    FOR(i, sz(ans) - 1, 0, -1) {
+        cout << b[ans[i]][0] << ' ';
+    }
+    EACH(i, b[ans[0]]) cout << i << ' ';
 
 	return 0;
 }
