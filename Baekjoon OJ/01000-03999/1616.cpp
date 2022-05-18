2d7ade196ca5023fae77161886cd3124e70f41c8
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-05-18 10:27:22 +0900

    .

--- Baekjoon OJ/01000-03999/1616.cpp
+++ Baekjoon OJ/01000-03999/1616.cpp
@@ -1,134 +1,73 @@
-#define _USE_MATH_DEFINES
-#include <bits/stdc++.h>
-#include <cassert>
+#include <iostream>
+#include <cmath>
+#include <vector>
 using namespace std;
-#define ll long long
-#define ull unsigned long long
-#define ld long double
-#define pii pair<int, int>
-#define pll pair<ll, ll>
-#define fr first 
-#define sc second
-#define vt vector
-#define all(c) (c).begin(), (c).end()
-#define sz(x) (int)(x).size()
 
-#define EXPAND( x ) x // Use this if MS Visual Studio doesn't expand __VA_ARGS__ correctly
-#define F_OR(i, a, b, s) for (int i = (a); (s) > 0 ? i < (b) : i > (b); i += (s))
-#define F_OR1(e) F_OR(i, 0, e, 1)
-#define F_OR2(i, e) F_OR(i, 0, e, 1)
-#define F_OR3(i, b, e) F_OR(i, b, e, 1)
-#define F_OR4(i, b, e, s) F_OR(i, b, e, s)
-#define GET5(a, b, c, d, e, ...) e
-#define F_ORC(...) EXPAND( GET5(__VA_ARGS__, F_OR4, F_OR3, F_OR2, F_OR1) )
-#define FOR(...) EXPAND( F_ORC(__VA_ARGS__ )(__VA_ARGS__) )
-#define EACH(x, a) for (auto& x : a)
+const int MAXV = 5 * 1e6;
 
-const double EPS = 1e-9;
-const int INF = 1e9 + 7;
-const int MOD = 1e9 + 7;
-const int dy[] = { 0, 0, 1, -1, 1, 1, -1, -1 };
-const int dx[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+int K, M, KpowM, res[2 * MAXV + 5], rTop;
+int** adj, aTop[MAXV + 5];
 
-int k, m;
-ll val, pk[11];
-vt<ll> a;
-vt<pll> e;
-
-int N, M;
-vt<int> adj[5050505], vi[5050505];
-int ind[5050505], outd[5050505], nxt[5050505];
-vt<int> ans;
-
-void com(int idx) {
-	if (idx == m) {
-		a.push_back(val);
-		return;
-	}
-
-	FOR(i, k) {
-		val += i;
-		val *= k;
-		com(idx + 1);
-		val /= k;
-		val -= i;
-	}
-}
-
-void makingGraph() {
-	FOR(sz(a)) {
-		int v = a[i] % pk[m - 1], u = a[i] - (a[i] % pk[1]);
-
-		e.push_back({ v, u });
-	}
-
-	N = pk[m - 1], M = sz(e);
-	FOR(M) {
-		adj[e[i].fr].push_back(e[i].sc);
-		ind[e[i].sc]++;
-		outd[e[i].fr]++;
-	}
-}
-
-int doesCircuitExist() {
-	FOR(N) {
-		if (ind[i] != outd[i]) return 0;
-	}
-	return 1;
-}
-
-void dfs(int now) {
-	for (int& i = nxt[now]; i < sz(adj[now]); i++) {
-		if (i < sz(adj[now]) && !vi[now][i]) {
-			vi[now][i] = 1;
-			dfs(adj[now][i]);
-		}
-	}
-	ans.push_back(now);
-}
+void DFS(int v);
 
 int main() {
-	#ifndef ONLINE_JUDGE
-	// Enter the absolute path of the local file input.txt, output.txt
-	// Or just enter the "input.txt", "output.txt"
-    freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
-    freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
-	#endif
-
 	cin.tie(NULL); cout.tie(NULL);
 	ios_base::sync_with_stdio(false);
+
+	cin >> K >> M;
 	
-	cin >> k >> m;
-
-	pk[0] = 1;
-	for (int i = 1; i <= m; i++) {
-		pk[i] = pk[i - 1] * k;
-	}
-
-	com(0);
-
-	// if m = 1
-	if (m == 1) {
-		EACH(i, b) cout << i[0] << ' ';
+	//예외 처리
+	if (M == 1) {
+		for (int v = 0; v < K; v++)
+			cout << v << ' ';
 		return 0;
 	}
+	
+	KpowM = pow(K, M);
 
-	// otherwise
-	cc();
+	//그래프 동적 할당
+	adj = new int*[KpowM / K + 5];
+	for (int i = 0; i < KpowM / K + 5; i++)
+		adj[i] = new int[K];
+	
+	//그래프 만들기: k진수 연산
+	for (int UtoV = 0; UtoV < KpowM; UtoV++) {
+		int u = UtoV / K, v = UtoV % (KpowM / K);
+		
+		adj[u][aTop[u]++] = v;
+	}
 
-	makingGraph();
+	//오일러 회로 찾기
+	DFS(0);
 
-	if (!doesCircuitExist()) {
+	//예외 처리
+	if (rTop - 1 != KpowM) {
 		cout << -1;
 		return 0;
 	}
-
-	FOR(i, N) vi[i].resize(sz(adj[i]));
-
-	dfs(0);
 	
-	EACH(i, c[ans[0]]) cout << i << ' ';
-	FOR(i, 1, sz(ans)) cout << c[ans[i]].back() << ' ';
+	//출력
+	for (int v = rTop - 2; v >= 0; v--)
+		cout << res[v] % K << ' ';
+
+	//할당 해제
+	for (int i = 0; i < KpowM / K + 5; i++)
+		delete[] adj[i];
+	delete[] adj;
 
 	return 0;
 }
+
+void DFS(int v) {
+	while (aTop[v] > 0) {
+		DFS(adj[v][--aTop[v]]);
+	}
+	res[rTop++] = v;
+}
+/*////////////////////////////////////////////////////////////////////
+문제 해법		: 오일러 회로, k진수 연산
+결정적 깨달음		:
+시간복잡도		:
+오답 원인		: 1.
+				  2.
+*/////////////////////////////////////////////////////////////////////
