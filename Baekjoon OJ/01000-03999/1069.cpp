59938833d7f81191251a5c9a4418c713e45ed165
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-06-10 14:27:10 +0900

    .

---
+++ Baekjoon OJ/01000-03999/1069.cpp
@@ -0,0 +1,67 @@
+#define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+#include <cassert>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define fr first 
+#define sc second
+#define vt vector
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+
+#define EXPAND( x ) x // Use this if MS Visual Studio doesn't expand __VA_ARGS__ correctly
+#define F_OR(i, a, b, s) for (int i = (a); (s) > 0 ? i < (b) : i > (b); i += (s))
+#define F_OR1(e) F_OR(i, 0, e, 1)
+#define F_OR2(i, e) F_OR(i, 0, e, 1)
+#define F_OR3(i, b, e) F_OR(i, b, e, 1)
+#define F_OR4(i, b, e, s) F_OR(i, b, e, s)
+#define GET5(a, b, c, d, e, ...) e
+#define F_ORC(...) EXPAND( GET5(__VA_ARGS__, F_OR4, F_OR3, F_OR2, F_OR1) )
+#define FOR(...) EXPAND( F_ORC(__VA_ARGS__ )(__VA_ARGS__) )
+#define EACH(x, a) for (auto& x : a)
+
+const double EPS = 1e-9;
+const int INF = 1e9 + 7;
+const int MOD = 1e9 + 7;
+const int dy[] = { 0, 0, 1, -1, 1, 1, -1, -1 };
+const int dx[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+ll x, y, d, t;
+
+int main() {
+	#ifndef ONLINE_JUDGE
+	// Enter the absolute path of the local file input.txt, output.txt
+	// Or just enter the "input.txt", "output.txt"
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
+	#endif
+
+	cin.tie(NULL); cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+    cout << fixed;
+    cout.precision(9);
+
+    cin >> x >> y >> d >> t;
+
+	if (d <= t) {
+        cout << sqrt(pow(x, 2) + pow(y, 2));
+        return 0;
+    }
+
+    ll cnt = sqrt(pow(x, 2) + pow(y, 2)) / d;
+
+    if (cnt == 0) {
+        cout << min<double>({ sqrt(pow(x, 2) + pow(y, 2)), t + d - sqrt(pow(x, 2) + pow(y, 2)), (double)2 * t });
+        return 0;
+    }
+
+    double ans = min<double>(cnt * t + sqrt(pow(x, 2) + pow(y, 2)) - cnt * d, (cnt + 1) * t);
+    cout << ans;
+
+	return 0;
+}
