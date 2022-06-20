0aa30e643035f8aea30abe12c92448c24f22d62b
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-06-20 11:40:20 +0900

    boj

--- Baekjoon OJ/01000-03999/1395.cpp
+++ Baekjoon OJ/01000-03999/1395.cpp
@@ -1,88 +1,116 @@
-#include <iostream>
-#include <vector>
+#define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+#include <cassert>
 using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define fr first 
+#define sc second
+#define vt vector
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
 
-int flag;
-vector<int> sgtr;
-vector<bool> lazy;
+#define EXPAND( x ) x // Use this if MS Visual Studio doesn't expand __VA_ARGS__ correctly
+#define F_OR(i, a, b, s) for (int i = (a); (s) > 0 ? i < (b) : i > (b); i += (s))
+#define F_OR1(e) F_OR(i, 0, e, 1)
+#define F_OR2(i, e) F_OR(i, 0, e, 1)
+#define F_OR3(i, b, e) F_OR(i, b, e, 1)
+#define F_OR4(i, b, e, s) F_OR(i, b, e, s)
+#define GET5(a, b, c, d, e, ...) e
+#define F_ORC(...) EXPAND( GET5(__VA_ARGS__, F_OR4, F_OR3, F_OR2, F_OR1) )
+#define FOR(...) EXPAND( F_ORC(__VA_ARGS__ )(__VA_ARGS__) )
+#define EACH(x, a) for (auto& x : a)
 
-int cnt(int L, int R, int n, int nL, int nR);
-void update(int L, int R, int n, int nL, int nR);
+const double EPS = 1e-9;
+const int INF = 1e9 + 7;
+const int MOD = 1e9 + 7;
+const int dy[] = { 0, 0, 1, -1, 1, 1, -1, -1 };
+const int dx[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+struct Seg { // 1-based
+	int flag;  // array size
+	vector<ll> t, lazy;
+
+	void build(int N) {
+		for (flag = 1; flag < N; flag <<= 1);
+		t.resize(2 * flag);
+		lazy.resize(2 * flag);
+
+		//for (int i = flag; i < flag + N; i++) cin >> t[i];
+		//for (int i = flag - 1; i >= 1; i--) t[i] = t[i << 1] + t[i << 1 | 1];
+	}
+	void modify(int l, int r) {
+		return modify(l, r, 1, 1, flag);
+	}
+	// add a value to all elements in interval [l, r]
+	void modify(int l, int r, int n, int nl, int nr) {
+		propagate(n, nl, nr);
+
+		if (r < nl || nr < l) return;
+		if (l <= nl && nr <= r) {
+			lazy[n]++;
+			propagate(n, nl, nr);
+			return;
+		}
+
+		int mid = (nl + nr) >> 1;
+		modify(l, r, n << 1, nl, mid);
+		modify(l, r, n << 1 | 1, mid + 1, nr);
+
+		t[n] = t[n << 1] + t[n << 1 | 1];
+	}
+	ll query(int l, int r) {
+		return query(l, r, 1, 1, flag);
+	}
+	ll query(int l, int r, int n, int nl, int nr) {  // sum on interval [l, r]
+		propagate(n, nl, nr);
+
+		if (r < nl || nr < l) return 0;
+		if (l <= nl && nr <= r) return t[n];
+
+		int mid = (nl + nr) / 2;
+		return query(l, r, n << 1, nl, mid) + query(l, r, n << 1 | 1, mid + 1, nr);
+	}
+	void propagate(int n, int nl, int nr) {
+		if (lazy[n] != 0) {
+			if (n < flag) {
+				lazy[n << 1] += lazy[n];
+				lazy[n << 1 | 1] += lazy[n];
+			}
+			if (lazy[n] & 1) t[n] = (nr - nl + 1) - t[n];
+			lazy[n] = 0;
+		}
+	}
+}seg;
+
+int n, m;
 
 int main() {
+	#ifndef ONLINE_JUDGE
+	// Enter the absolute path of the local file input.txt, output.txt
+	// Or just enter the "input.txt", "output.txt"
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
+	#endif
+
 	cin.tie(NULL); cout.tie(NULL);
 	ios_base::sync_with_stdio(false);
 
-	int n, m;
 	cin >> n >> m;
-
-	for (flag = 1; flag < n; flag *= 2);
-	sgtr.resize(2 * flag);
-	lazy.resize(2 * flag);
-
-	while (m--) {
-		int a, b, c;
-		cin >> a >> b >> c;
-
-		if (a == 0) update(b, c, 1, 1, flag);
-		else cout << cnt(b, c, 1, 1, flag) << '\n';
+	seg.build(n);
+	FOR(m) {
+		int op, x, y;
+		cin >> op >> x >> y;
+		if (op == 0) {
+			seg.modify(x, y);
+		}
+		if (op == 1) {
+			cout << seg.query(x, y) << '\n';
+		}
 	}
 
 	return 0;
 }
-
-int cnt(int L, int R, int n, int nL, int nR) {
-	if (lazy[n]) {
-		sgtr[n] = (nR - nL + 1) - sgtr[n];
-		if (n < flag) {
-			if (lazy[2 * n]) lazy[2 * n] = false;
-			else lazy[2 * n] = true;
-			if (lazy[2 * n + 1]) lazy[2 * n + 1] = false;
-			else lazy[2 * n + 1] = true;
-		}
-		lazy[n] = false;
-	}
-
-	if (R < nL || nR < L) return 0;
-	if (L <= nL && nR <= R) return sgtr[n];
-
-	int mid = (nL + nR) / 2;
-	return cnt(L, R, 2 * n, nL, mid) + cnt(L, R, 2 * n + 1, mid + 1, nR);
-}
-void update(int L, int R, int n, int nL, int nR) {
-	if (lazy[n]) {
-		sgtr[n] = (nR - nL + 1) - sgtr[n];
-		if (n < flag) {
-			if (lazy[2 * n]) lazy[2 * n] = false;
-			else lazy[2 * n] = true;
-			if (lazy[2 * n + 1]) lazy[2 * n + 1] = false;
-			else lazy[2 * n + 1] = true;
-		}
-		lazy[n] = false;
-	}
-
-	if (R < nL || nR < L) return;
-	if (L <= nL && nR <= R) {
-		sgtr[n] = (nR - nL + 1) - sgtr[n];
-		if (n < flag) {
-			if (lazy[2 * n]) lazy[2 * n] = false;
-			else lazy[2 * n] = true;
-			if (lazy[2 * n + 1]) lazy[2 * n + 1] = false;
-			else lazy[2 * n + 1] = true;
-		}
-		return;
-	}
-
-	int mid = (nL + nR) / 2;
-	update(L, R, 2 * n, nL, mid);
-	update(L, R, 2 * n + 1, mid + 1, nR);
-
-	sgtr[n] = sgtr[2 * n] + sgtr[2 * n + 1];
-}
-/*////////////////////////////////////////////////////////////////////
-문제 해법		: 세그트리 w 레이지 프로퍼게이션
-접근 방식		: 
-결정적 깨달음		: 
-오답 원인		: 1.
-				  2.
-*/////////////////////////////////////////////////////////////////////
--- Baekjoon OJ/10000-12999/12844.cpp
+++ Baekjoon OJ/10000-12999/12844.cpp
@@ -3,31 +3,50 @@
 #include <cassert>
 using namespace std;
 #define ll long long
+#define ull unsigned long long
+#define ld long double
 #define pii pair<int, int>
 #define pll pair<ll, ll>
+#define fr first 
+#define sc second
+#define vt vector
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
 
+#define EXPAND( x ) x // Use this if MS Visual Studio doesn't expand __VA_ARGS__ correctly
+#define F_OR(i, a, b, s) for (int i = (a); (s) > 0 ? i < (b) : i > (b); i += (s))
+#define F_OR1(e) F_OR(i, 0, e, 1)
+#define F_OR2(i, e) F_OR(i, 0, e, 1)
+#define F_OR3(i, b, e) F_OR(i, b, e, 1)
+#define F_OR4(i, b, e, s) F_OR(i, b, e, s)
+#define GET5(a, b, c, d, e, ...) e
+#define F_ORC(...) EXPAND( GET5(__VA_ARGS__, F_OR4, F_OR3, F_OR2, F_OR1) )
+#define FOR(...) EXPAND( F_ORC(__VA_ARGS__ )(__VA_ARGS__) )
+#define EACH(x, a) for (auto& x : a)
+
+const double EPS = 1e-9;
 const int INF = 1e9 + 7;
 const int MOD = 1e9 + 7;
-const int dy[] = { 0, 0, 1, -1 };
-const int dx[] = { 1, -1, 0, 0 };
+const int dy[] = { 0, 0, 1, -1, 1, 1, -1, -1 };
+const int dx[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
-struct Seg {
+struct Seg { // 1-based
 	int flag;  // array size
 	vector<ll> t, lazy;
 
-	void build(int N) {
+	void build(int N, const vt<ll>& a) {
 		for (flag = 1; flag < N; flag <<= 1);
 		t.resize(2 * flag);
 		lazy.resize(2 * flag);
 
-		for (int i = flag; i < flag + N; i++) cin >> t[i];
+		for (int i = flag; i < flag + N; i++) t[i] = a[i - flag];
 		for (int i = flag - 1; i >= 1; i--) t[i] = t[i << 1] ^ t[i << 1 | 1];
 	}
-	void modify(int l, int r, int value) {
+	void modify(int l, int r, ll value) {
 		return modify(l, r, 1, 1, flag, value);
 	}
 	// add a value to all elements in interval [l, r]
-	void modify(int l, int r, int n, int nl, int nr, int value) {
+	void modify(int l, int r, int n, int nl, int nr, ll value) {
 		propagate(n, nl, nr);
 
 		if (r < nl || nr < l) return;
@@ -67,26 +86,37 @@ struct Seg {
 	}
 }seg;
 
+int n;
+vt<ll> a;
+
 int main() {
-    cin.tie(NULL); cout.tie(NULL);
-    ios_base::sync_with_stdio(false);
+	#ifndef ONLINE_JUDGE
+	// Enter the absolute path of the local file input.txt, output.txt
+	// Or just enter the "input.txt", "output.txt"
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
+	#endif
 
-	int n, m;
+	cin.tie(NULL); cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
 	cin >> n;
-	seg.build(n);
+	a.resize(n);
+	EACH(i, a) cin >> i;
+	seg.build(n, a);
 
-	cin >> m;
-	while (m--) {
-		int q, a, b, c;
-		cin >> q >> a >> b;
-		if (q == 1) {
-			cin >> c;
-			seg.modify(a + 1, b + 1, c);
+	int q; cin >> q;
+	while (q--) {
+		ll op, x, y;
+		cin >> op >> x >> y;
+		if (op == 1) {
+			ll z; cin >> z;
+			seg.modify(x + 1, y + 1, z);
 		}
-		else {
-			cout << seg.query(a + 1, b + 1) << '\n';
+		if (op == 2) {
+			cout << seg.query(x + 1, y + 1) << '\n';
 		}
 	}
 
-    return 0;
+	return 0;
 }
