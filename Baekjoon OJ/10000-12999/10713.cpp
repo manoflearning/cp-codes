30e07b7512dfb54c6334daec43cb2900b55c8bbd
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-06-20 11:54:08 +0900

    boj

---
+++ Baekjoon OJ/10000-12999/10713.cpp
@@ -0,0 +1,123 @@
+#define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+#include <cassert>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define fr first 
+#define sc second
+#define vt vector
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+
+#define EXPAND( x ) x // Use this if MS Visual Studio doesn't expand __VA_ARGS__ correctly
+#define F_OR(i, a, b, s) for (int i = (a); (s) > 0 ? i < (b) : i > (b); i += (s))
+#define F_OR1(e) F_OR(i, 0, e, 1)
+#define F_OR2(i, e) F_OR(i, 0, e, 1)
+#define F_OR3(i, b, e) F_OR(i, b, e, 1)
+#define F_OR4(i, b, e, s) F_OR(i, b, e, s)
+#define GET5(a, b, c, d, e, ...) e
+#define F_ORC(...) EXPAND( GET5(__VA_ARGS__, F_OR4, F_OR3, F_OR2, F_OR1) )
+#define FOR(...) EXPAND( F_ORC(__VA_ARGS__ )(__VA_ARGS__) )
+#define EACH(x, a) for (auto& x : a)
+
+const double EPS = 1e-9;
+const int INF = 1e9 + 7;
+const int MOD = 1e9 + 7;
+const int dy[] = { 0, 0, 1, -1, 1, 1, -1, -1 };
+const int dx[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+struct Seg { // 1-based
+	int flag;  // array size
+	vector<ll> t, lazy;
+
+	void build(int N) {
+		for (flag = 1; flag < N; flag <<= 1);
+		t.resize(2 * flag);
+		lazy.resize(2 * flag);
+
+		//for (int i = flag; i < flag + N; i++) cin >> t[i];
+		//for (int i = flag - 1; i >= 1; i--) t[i] = t[i << 1] + t[i << 1 | 1];
+	}
+	void modify(int l, int r) {
+		return modify(l, r, 1, 1, flag);
+	}
+	// add a value to all elements in interval [l, r]
+	void modify(int l, int r, int n, int nl, int nr) {
+		propagate(n, nl, nr);
+
+		if (r < nl || nr < l) return;
+		if (l <= nl && nr <= r) {
+			lazy[n]++;
+			propagate(n, nl, nr);
+			return;
+		}
+
+		int mid = (nl + nr) >> 1;
+		modify(l, r, n << 1, nl, mid);
+		modify(l, r, n << 1 | 1, mid + 1, nr);
+
+		t[n] = t[n << 1] + t[n << 1 | 1];
+	}
+	ll query(int l, int r) {
+		return query(l, r, 1, 1, flag);
+	}
+	ll query(int l, int r, int n, int nl, int nr) {  // sum on interval [l, r]
+		propagate(n, nl, nr);
+
+		if (r < nl || nr < l) return 0;
+		if (l <= nl && nr <= r) return t[n];
+
+		int mid = (nl + nr) / 2;
+		return query(l, r, n << 1, nl, mid) + query(l, r, n << 1 | 1, mid + 1, nr);
+	}
+	void propagate(int n, int nl, int nr) {
+		if (lazy[n] != 0) {
+			if (n < flag) {
+				lazy[n << 1] += lazy[n];
+				lazy[n << 1 | 1] += lazy[n];
+			}
+			t[n] += lazy[n] * (nr - nl + 1);
+			lazy[n] = 0;
+		}
+	}
+}seg;
+
+int n, m;
+ll p[101010], a[101010], b[101010], c[101010];
+
+int main() {
+	#ifndef ONLINE_JUDGE
+	// Enter the absolute path of the local file input.txt, output.txt
+	// Or just enter the "input.txt", "output.txt"
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
+	#endif
+
+	cin.tie(NULL); cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	cin >> n >> m;
+    FOR(i, 1, m + 1) cin >> p[i];
+    FOR(i, 1, n) {
+        cin >> a[i] >> b[i] >> c[i];
+    }
+    seg.build(n);
+    FOR(i, 1, m) {
+        int u = min(p[i], p[i + 1]), v = max(p[i], p[i + 1]);
+        seg.modify(u, v - 1);
+    }
+
+    ll ans = 0;
+    FOR(i, 1, n) {
+        ll res = seg.query(i, i);
+        ans += min(res * a[i], res * b[i] + c[i]);
+        //cout << res << ' ';
+    }
+    cout << ans;
+
+	return 0;
+}
