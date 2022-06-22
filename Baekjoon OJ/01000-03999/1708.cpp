1de53e51288a2e650d2984da0f0f82257370189b
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-06-23 00:32:50 +0900

    boj

--- Baekjoon OJ/01000-03999/1708.cpp
+++ Baekjoon OJ/01000-03999/1708.cpp
@@ -1,71 +1,82 @@
+// BOJ 1708 AC Code
+// https://www.acmicpc.net/problem/1708
 #include <iostream>
+#include <set>
 #include <vector>
 #include <algorithm>
-#include <stack>
 #define ll long long
 using namespace std;
 
 struct point {
-	ll x, y, px, py;
-	point() : point(0, 0, 1, 0) {}
-	point(int sx, int sy): point(sx, sy, 1, 0) {}
-	point(int sx, int sy, int spx, int spy): x(sx), y(sy), px(spx), py(spy) {}
-	bool operator <(point& O) {
-		if (py * O.px != px * O.py) 
-			return py * O.px < px * O.py;
-		if (y != O.y) return y < O.y;
-		return x < O.x;
-	}
+	ll x, y;
+    bool operator<(const point& rhs) const {
+        if (x != rhs.x) return x < rhs.x;
+		else return y < rhs.y;
+    }
 };
 
+int N;
 vector<point> p;
+vector<int> st, en;
 
-ll ccw(point& a, point& b, point& c);
+ll ccw(point& a, point& b, point& c) {
+	// res > 0 -> ccw, res < 0 -> cw, res = 0 -> colinear
+	ll res = (b.x - a.x) * (c.y - a.y) - (c.x - a.x) * (b.y - a.y);
+	return (res > 0 ? 1 : (res < 0 ? -1 : 0));
+}
 
 int main() {
+	#ifndef ONLINE_JUDGE
+	// Enter the absolute path of the local file input.txt, output.txt
+	// Or just enter the "input.txt", "output.txt"
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
+	#endif
+
 	cin.tie(NULL); cout.tie(NULL);
 	ios_base::sync_with_stdio(false);
 
-	int N; cin >> N;
+	cin >> N;
 
-	p.resize(N);
 	for (int i = 0; i < N; i++) {
 		int x, y;
 		cin >> x >> y;
-		p[i] = point(x, y);
+		p.push_back({ x, y });
 	}
 
 	sort(p.begin(), p.end());
 
-	for (int i = 1; i < N; i++) {
-		p[i].px = p[i].x - p[0].x;
-		p[i].py = p[i].y - p[0].y;
-	}
-
-	sort(p.begin() + 1, p.end());
-
-	stack<int> s;
-	s.push(0);
-	s.push(1);
+	st.push_back(0);
+	st.push_back(1);
 	for (int next = 2; next < N; next++) {
-		while (s.size() >= 2) {
-			int first, second;
-			first = s.top();
-			s.pop();
-			second = s.top();
+		while (st.size() >= 2) {
+			int first = st.back();
+			st.pop_back();
+			int second = st.back();
 			if (ccw(p[second], p[first], p[next]) > 0) {
-				s.push(first);
+				st.push_back(first);
 				break;
 			}
 		}
-		s.push(next);
+		st.push_back(next);
 	}
 
-	cout << s.size() << '\n';
+    en.push_back(N - 1);
+    en.push_back(N - 2);
+    for (int next = N - 3; next >= 0; next--) {
+		while (en.size() >= 2) {
+			int first = en.back();
+			en.pop_back();
+			int second = en.back();
+			if (ccw(p[second], p[first], p[next]) > 0) {
+				en.push_back(first);
+				break;
+			}
+		}
+		en.push_back(next);
+	}
+
+	cout << (int)st.size() + (int)en.size() - 2;
 
 	return 0;
-}
-
-ll ccw(point& a, point& b, point& c) {
-	return (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
 }
