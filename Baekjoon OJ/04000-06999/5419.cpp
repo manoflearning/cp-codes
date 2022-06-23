7a51e1ea52d37e76df44e27f5e9669190dec20c4
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-06-24 02:25:20 +0900

    boj

--- Baekjoon OJ/04000-06999/5419.cpp
+++ Baekjoon OJ/04000-06999/5419.cpp
@@ -1,117 +1,116 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <vector>
-#define ll long long
+#define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+#include <cassert>
 using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define fr first 
+#define sc second
+#define vt vector
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+
+#define EXPAND( x ) x // Use this if MS Visual Studio doesn't expand __VA_ARGS__ correctly
+#define F_OR(i, a, b, s) for (int i = (a); (s) > 0 ? i < (b) : i > (b); i += (s))
+#define F_OR1(e) F_OR(i, 0, e, 1)
+#define F_OR2(i, e) F_OR(i, 0, e, 1)
+#define F_OR3(i, b, e) F_OR(i, b, e, 1)
+#define F_OR4(i, b, e, s) F_OR(i, b, e, s)
+#define GET5(a, b, c, d, e, ...) e
+#define F_ORC(...) EXPAND( GET5(__VA_ARGS__, F_OR4, F_OR3, F_OR2, F_OR1) )
+#define FOR(...) EXPAND( F_ORC(__VA_ARGS__ )(__VA_ARGS__) )
+#define EACH(x, a) for (auto& x : a)
+
+const double EPS = 1e-9;
+const int INF = 1e9 + 7;
+const int MOD = 1e9 + 7;
+const int dy[] = { 0, 0, 1, -1, 1, 1, -1, -1 };
+const int dx[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+int flag;  // array size
+struct Seg {  // 1-based
+	vector<ll> t;
+
+	void build(int n) {
+		t.clear();
+		for (flag = 1; flag < n; flag <<= 1);
+		t.resize(2 * flag);
+
+		//for (int i = flag; i < flag + n; i++) cin >> t[i];
+		//for (int i = flag - 1; i >= 1; i--) t[i] = t[i << 1] + t[i << 1 | 1];
+	}
+	void modify(int p) {  // set value at position p
+		for (t[p += flag - 1]++; p > 1; p >>= 1) t[p >> 1] = t[p] + t[p ^ 1];
+	}
+	ll query(int l, int r, int n = 1, int nl = 1, int nr = flag) {  // sum on interval [l, r]
+		if (r < nl || nr < l) return 0;
+		if (l <= nl && nr <= r) return t[n];
+
+		int mid = (nl + nr) / 2;
+		return query(l, r, n << 1, nl, mid) + query(l, r, n << 1 | 1, mid + 1, nr);
+	}
+}seg;
 
 struct point {
-	int x, y;
-	point() : point(x, y) {}
-	point(int sx, int sy) : x(sx), y(sy) {}
-	bool operator <(point& O) {
-		if (x != O.x) return x < O.x;
-		return y > O.y;
+	ll x, y;
+	bool operator<(const point& rhs) const {
+		if (x != rhs.x) return x < rhs.x;
+		return y > rhs.y;
 	}
 };
 
-vector<point> p;
-int flag;
-vector<ll> sgtr, lazy;
+int n;
+vt<point> a;
 
-void init();
-bool cmp(point& a, point& b);
-ll sum(int L, int R, int n, int nL, int nR);
-void update(int L, int R, int n, int nL, int nR, int v);
-void propagate(int n, int nL, int nR);
+void init() {
+	n = 0;
+	a.clear();
+}
 
 int main() {
+	#ifndef ONLINE_JUDGE
+	// Enter the absolute path of the local file input.txt, output.txt
+	// Or just enter the "input.txt", "output.txt"
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
+	#endif
+
 	cin.tie(NULL); cout.tie(NULL);
 	ios_base::sync_with_stdio(false);
 
-	int T; cin >> T;
-
-	while (T--) {
+	int tc; cin >> tc;
+	while (tc--) {
 		init();
 
-		int N; cin >> N;
+		cin >> n;
+		a.resize(n);
+		EACH(i, a) cin >> i.x >> i.y;
 
-		for (int i = 0; i < N; i++) {
-			int x, y;
-			cin >> x >> y;
-			p.push_back(point(x, y));
+		vt<ll> b;
+		EACH(i, a) {
+			b.push_back(i.x);
+			b.push_back(i.y);
 		}
-
-		sort(p.begin(), p.end(), cmp);
-
-		int h = 1;
-		for (int i = 0; i < p.size(); i++) {
-			if (i + 1 != p.size() && p[i].y == p[i + 1].y) p[i].y = h;
-			else p[i].y = h++;
+		sort(all(b));
+		b.erase(unique(all(b)), b.end());
+		EACH(i, a) {
+			i.x = lower_bound(all(b), i.x) - b.begin() + 1;
+			i.y = lower_bound(all(b), i.y) - b.begin() + 1;
 		}
-		
-		for (flag = 1; flag < h; flag *= 2);
-		sgtr.resize(2 * flag);
-		lazy.resize(2 * flag);
+		seg.build(sz(b));
 
-		sort(p.begin(), p.end());
-
+		sort(all(a));
 		ll ans = 0;
-		for (int i = 0; i < p.size(); i++) {
-			ans += sum(p[i].y, h, 1, 1, flag);
-			update(p[i].y, p[i].y, 1, 1, flag, 1);
+		EACH(i, a) {
+			ans += seg.query(i.y, sz(b));
+			seg.modify(i.y);
 		}
 
 		cout << ans << '\n';
 	}
 
 	return 0;
-}
-
-void init() {
-	p.clear();
-	sgtr.clear();
-	lazy.clear();
-}
-
-bool cmp(point& a, point& b) {
-	return a.y < b.y;
-}
-
-ll sum(int L, int R, int n, int nL, int nR) {
-	propagate(n, nL, nR);
-
-	if (R < nL || nR < L) return 0;
-	if (L <= nL && nR <= R) return sgtr[n];
-
-	int mid = (nL + nR) / 2;
-	return sum(L, R, 2 * n, nL, mid) + sum(L, R, 2 * n + 1, mid + 1, nR);
-}
-
-void update(int L, int R, int n, int nL, int nR, int v) {
-	propagate(n, nL, nR);
-
-	if (R < nL || nR < L) return;
-	if (L <= nL && nR <= R) {
-		lazy[n] = v;
-		propagate(n, nL, nR);
-		return;
-	}
-
-	int mid = (nL + nR) / 2;
-	update(L, R, 2 * n, nL, mid, v);
-	update(L, R, 2 * n + 1, mid + 1, nR, v);
-
-	sgtr[n] = sgtr[2 * n] + sgtr[2 * n + 1];
-}
-
-void propagate(int n, int nL, int nR) {
-	if (lazy[n] != 0) {
-		if (n < flag) {
-			lazy[2 * n] += lazy[n];
-			lazy[2 * n + 1] += lazy[n];
-		}
-		sgtr[n] += lazy[n] * (nR - nL + 1);
-		lazy[n] = 0;
-	}
 }
