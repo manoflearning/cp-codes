c982be19370492372c7d4e53a67bae543ae11771
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-06-28 15:26:14 +0900

    boj

--- Baekjoon OJ/13000-15999/13206.cpp
+++ Baekjoon OJ/13000-15999/13206.cpp
@@ -1,4 +1,98 @@
+// solution 2: sieve of eratosthenes
 #define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+#include <cassert>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define fr first 
+#define sc second
+#define vt vector
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+
+#define EXPAND( x ) x // Use this if MS Visual Studio doesn't expand __VA_ARGS__ correctly
+#define F_OR(i, a, b, s) for (int i = (a); (s) > 0 ? i < (b) : i > (b); i += (s))
+#define F_OR1(e) F_OR(i, 0, e, 1)
+#define F_OR2(i, e) F_OR(i, 0, e, 1)
+#define F_OR3(i, b, e) F_OR(i, b, e, 1)
+#define F_OR4(i, b, e, s) F_OR(i, b, e, s)
+#define GET5(a, b, c, d, e, ...) e
+#define F_ORC(...) EXPAND( GET5(__VA_ARGS__, F_OR4, F_OR3, F_OR2, F_OR1) )
+#define FOR(...) EXPAND( F_ORC(__VA_ARGS__ )(__VA_ARGS__) )
+#define EACH(x, a) for (auto& x : a)
+
+const double EPS = 1e-9;
+const int INF = 1e9 + 7;
+const int MOD = 1e9 + 7;
+const int dy[] = { 0, 0, 1, -1, 1, 1, -1, -1 };
+const int dx[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+const int MAX = 1010;
+
+bool isPrime[MAX + 1];
+vector<int> prime(1, 2);
+
+void getPrime() {
+	fill(isPrime + 2, isPrime + MAX + 1, 1);
+
+	for (ll i = 4; i <= MAX; i += 2)
+		isPrime[i] = 0;
+	for (ll i = 3; i <= MAX; i++) {
+		if (isPrime[i]) prime.push_back(i);
+		for (ll j = i * i; j <= MAX; j += i * 2)
+			isPrime[j] = 0;
+	}
+}
+
+int main() {
+	#ifndef ONLINE_JUDGE
+	// Enter the absolute path of the local file input.txt, output.txt
+	// Or just enter the "input.txt", "output.txt"
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
+	#endif
+
+	cin.tie(NULL); cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+    getPrime();
+
+	int tc; cin >> tc;
+    while (tc--) {
+        int n; cin >> n;
+
+        vt<ll> a(n);
+        EACH(i, a) cin >> i;
+
+        vt<int> cnt(1010);
+        EACH(val, a) {
+            ll x = val;
+            EACH(p, prime) {
+                if (p > x) break;
+                int res = 0;
+                while (x % p == 0) x /= p, res++;
+                cnt[p] = max(cnt[p], res);
+            }
+        }
+
+        ll ans = 1;
+        FOR(1010) {
+            //cout << i << ' ' << cnt[i] << '\n';
+            while (cnt[i]--) ans = ans * i % MOD;
+        }
+
+        cout << ans << '\n';
+    }
+
+	return 0;
+}
+
+// solution 1: sqrt factorization
+/*#define _USE_MATH_DEFINES
 #include <bits/stdc++.h>
 #include <cassert>
 using namespace std;
@@ -70,4 +164,4 @@ int main() {
     }
 
 	return 0;
-}
+}*/
