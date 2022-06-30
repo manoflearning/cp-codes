9e36e243932a7424287effac22140964876d40f4
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-06-30 22:12:55 +0900

    boj

--- Baekjoon OJ/13000-15999/13307.cpp
+++ Baekjoon OJ/13000-15999/13307.cpp
@@ -33,23 +33,14 @@ const int dx[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 struct point {
     ll x, y;
     bool operator<(const point& rhs) const {
-        if (x != rhs.x) return x < rhs.x;
+        if (x ^ rhs.x) return x < rhs.x;
         return y < rhs.y;
     }
 };
 
 int n;
-point p[3030], p2[3030];
-pll ans[1010];
-
-void input() {
-    cin >> n;
-    FOR(3 * n) {
-        ll x, y;
-        cin >> x >> y;
-        p[i].x = x, p[i].y = y;
-    }
-}
+point p[3030];
+pii ans[1010];
 
 ll ccw(const point& a, const point& b, const point& c) {
 	// res > 0 -> ccw, res < 0 -> cw, res = 0 -> colinear
@@ -57,112 +48,120 @@ ll ccw(const point& a, const point& b, const point& c) {
 	return (res > 0 ? 1 : (res < 0 ? -1 : 0));
 }
 
-bool cmp(const int& a, const int& b) { return p[a] < p[b]; }
-
-//point o = { 0, 0 };
-bool cmp2(const int& a, const int& b){
-    //return ccw(o, p[a], p[b]) < 0;
-    return p2[b].x * p2[a].y < p2[a].x * p2[b].y;
+point o = { 0, 0 };
+bool cmp(const int& a, const int& b) { 
+    return ccw(o, p[a], p[b]) > 0;
 }
 
-int getB(vt<int>& a) {
-    sort(all(a), cmp);
+void input() {
+    cin >> n;
+    FOR(3 * n) {
+        ll x, y;
+        cin >> x >> y;
+        p[i] = { x, y };
+    }
+}
 
-    // calculate lower hull
-    vt<int> dh;
-	dh.push_back(a[0]);
-	dh.push_back(a[1]);
-	for (int next = 2; next < sz(a); next++) {
-		while (dh.size() >= 2) {
-			int first = dh.back();
-			dh.pop_back();
-			int second = dh.back();
-			if (ccw(p[second], p[first], p[a[next]]) > 0) {
-				dh.push_back(first);
-				break;
-			}
-		}
-		dh.push_back(a[next]);
-	}
-
-	// calculate upper hull
-    vt<int> uh;
-	uh.push_back(a[sz(a) - 1]);
-	uh.push_back(a[sz(a) - 2]);
-	for (int next = sz(a) - 3; next >= 0; next--) {
-		while (uh.size() >= 2) {
-			int first = uh.back();
-			uh.pop_back();
-			int second = uh.back();
-			if (ccw(p[second], p[first], p[a[next]]) > 0){
-				uh.push_back(first);
-				break;
-			}
-		}
-		uh.push_back(a[next]);
-	}
-
-    int ret = 0, x = -1;
-    EACH(i, dh) if (i < n) { ret = 1; x = i; }
-    EACH(i, uh) if (i < n) { ret = 1; x = i; }
-
-    if (ret) {
-        FOR(sz(a)) {
-            if (a[i] == x) { swap(a[0], a[i]); break; }
-        }
+int getBlue(vt<int>& a) {
+    // (1. x값이 가장 작은, 2. x값이 가장 작은 점이 여러 개일 경우 y값이 가장 작은) 점 찾기
+    FOR(i, 1, sz(a)) {
+        if (p[a[i]] < p[a[0]]) swap(a[0], a[i]);
     }
 
-    return ret;
+    // p[a[0]]을 기준으로 해서 각도 정렬 (반시계 방향)
+    o = p[a[0]];
+    sort(a.begin() + 1, a.end(), cmp);
+
+    // convex hull 찾기
+    vt<int> st;
+    st.push_back(a[0]);
+	st.push_back(a[1]);
+	for (int i = 2; i < sz(a); i++) {
+        int next = a[i];
+		while (st.size() >= 2) {
+			int first = st.back();
+			st.pop_back();
+			int second = st.back();
+			if (ccw(p[second], p[first], p[next]) > 0) {
+				st.push_back(first);
+				break;
+			}
+		}
+		st.push_back(next);
+	}
+
+    //EACH(i, st) cout << i << ' ';
+    //cout << '\n';
+
+    // convex hull 상에서 파란 점이 있는지 확인
+    int bit = 0, x = -1;
+    EACH(i, st) if (i < n) { bit = 1; x = i; }
+
+    // 만약 파란 점이 없다면 0을 반환
+    if (!bit) return 0;
+
+    // 만약 파란 점이 있다면 파란 점을 기준으로 각도 정렬
+    FOR(i, sz(a)) if (a[i] == x) { swap(a[0], a[i]); break; }
+    o = p[a[0]];
+    sort(a.begin() + 1, a.end(), cmp);
+
+    return 1; // 1을 반환
 }
 
 void dc(vt<int>& a) {
     if (a.empty()) return;
 
-    int bit = getB(a);
-    FOR(i, 1, sz(a)) {
-        p2[a[i]].x = p[a[i]].x - p[a[0]].x;
-        p2[a[i]].y = p[a[i]].y - p[a[0]].y;
-    }
-    //o = p[a[0]];
-    sort(a.begin() + 1, a.end(), cmp2);
+    int bit = getBlue(a);
+
     //EACH(i, a) cout << i << ' ';
     //cout << '\n';
+
+    // 만약 파란 점이 있다면
     if (bit) {
         int res = 0, idx1 = -1, idx2 = -1;
         FOR(i, 1, sz(a)) {
             if (a[i] < n) res += 2;
             else res -= 1;
-            
+
             if (res == -1 && idx1 == -1) idx1 = i;
             if (res == -2 && idx2 == -1) idx2 = i;
         }
-        
+
         ans[a[0]] = { a[idx1], a[idx2] };
 
         vt<int> b, c, d;
-        FOR(i, 1, idx1) b.push_back(a[i]);
-        FOR(i, idx1 + 1, idx2) c.push_back(a[i]);
-        FOR(i, idx2 + 1, sz(a)) d.push_back(a[i]);
-
+        FOR(i, 1, idx1) b.push_back(a[i]); // [1, idx1 - 1] 구간
+        FOR(i, idx1 + 1, idx2) c.push_back(a[i]); // [idx1 + 1, idx2 - 1] 구간
+        FOR(i, idx2 + 1, sz(a)) d.push_back(a[i]); // [idx2 + 1, sz(a) - 1] 구간
+        
         dc(b); dc(c); dc(d);
     }
+    // 만약 파란 점이 없다면
     else {
-        int res = 0, idx = -1;
-        FOR(i, sz(a)) {
-            if (a[i] < n) res += 2;
-            else res -= 1;
-            
-            if (res == 0 && idx == -1) idx = i;
-        }
+        // 점들의 합이 0인 [1, j] 구간 찾기
+        // convex hull 상의 어떤 점에서 시작하더라도, 그러한 구간을 찾을 수 있음
+        for (int i = 0; i < 2; i++) {
+            int res = 0;
+            vt<int> b;
+            for (int j = 1; j < sz(a); j++) {
+                if (a[j] < n) res += 2;
+                else res -= 1;
+                b.push_back(a[j]);
+                
+                if (res == 0) {
+                    dc(b);
+                    b.clear();
 
-        vt<int> b, c;
-        for (int i = 0; i < sz(a); i++) {
-            if (1 <= i && i <= idx) b.push_back(a[i]);
-            else c.push_back(a[i]);
+                    b.push_back(a[0]);
+                    FOR(k, j + 1, sz(a)) b.push_back(a[k]);
+                    dc(b);
+                    return;
+                }
+            }
+
+            reverse(a.begin() + 1, a.end());
         }
-        assert(sz(a) > sz(b));
-        assert(sz(a) > sz(c));
-        dc(b); dc(c);
+        assert(0);
     }
 }
 
@@ -183,7 +182,10 @@ int main() {
     FOR(3 * n) a.push_back(i);
     dc(a);
 
-    FOR(i, n) cout << ans[i].fr - n + 1 << ' ' << ans[i].sc - n + 1 << '\n';
+    FOR(n) {
+        cout << ans[i].fr - n + 1 << ' ';
+        cout << ans[i].sc - n + 1 << '\n';
+    }
 
 	return 0;
 }
