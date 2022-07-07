463d8a7ec1ba17d2aaefac7d509de21e0d4a1b8d
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-07-07 11:53:43 +0900

    boj

--- Baekjoon OJ/13000-15999/13537.cpp
+++ Baekjoon OJ/13000-15999/13537.cpp
@@ -1,4 +1,139 @@
+// BOJ 13537 AC Code
+// https://www.acmicpc.net/problem/13537
 #include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+#define sz(x) (int)(x).size()
+#define all(c) (c).begin(), (c).end()
+
+const int MAX = 1 << 17;
+
+struct MergeSortTree {
+    vector<int> tree[MAX << 1];
+
+    void build(const vector<int>& arr) {
+        for (int i = 0; i < sz(arr); i++) 
+            tree[i + 1 + MAX].push_back(arr[i]);
+
+        for (int i = MAX - 1; i >= 1; i--) {
+            tree[i].resize(sz(tree[i << 1]) + sz(tree[i << 1 | 1]));
+            merge(all(tree[i << 1]), all(tree[i << 1 | 1]), tree[i].begin());
+        }
+    }
+
+    int query(int l, int r, int k, int n = 1, int nl = 0, int nr = MAX - 1) { // 0-indexed, query on interval [l, r]
+        if (nr < l || r < nl) return 0;
+        if (l <= nl && nr <= r)
+            return tree[n].end() - upper_bound(all(tree[n]), k);
+
+        int mid = (nl + nr) >> 1;
+        return query(l, r, k, n << 1, nl, mid) + query(l, r, k, n << 1 | 1, mid + 1, nr);
+    }
+}mstree;
+
+int n;
+vector<int> arr;
+
+int main() {
+	#ifndef ONLINE_JUDGE
+	// Enter the absolute path of the local file input.txt, output.txt
+	// Or just enter the "input.txt", "output.txt"
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
+	#endif
+
+    cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    cin >> n;
+    arr.resize(n);
+    for (auto& i : arr) cin >> i;
+
+    mstree.build(arr);
+
+    int q; cin >> q;
+    while (q--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        int ans = mstree.query(a, b, c);
+        cout << ans << '\n';
+    }
+    
+    return 0;
+}
+
+/*#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+#define sz(x) (int)(x).size()
+#define all(c) (c).begin(), (c).end()
+
+const int MAX = 1 << 17;
+
+struct MergeSortTree {
+    vector<int> tree[1 << 18];
+
+    void add(int idx, int val) {
+        tree[idx + MAX].push_back(val);
+    }
+
+    void build(const vector<int>& arr) {
+        for (int i = 0; i < sz(arr); i++) add(i + 1, arr[i]);
+
+        for (int i = MAX - 1; i >= 1; i--) {
+            tree[i].resize(sz(tree[i << 1]) + sz(tree[i << 1 | 1]));
+            merge(all(tree[i << 1]), all(tree[i << 1 | 1]), tree[i].begin());
+        }
+    }
+
+    int query(int l, int r, int k) {
+        l += MAX, r += MAX;
+        int ret = 0;
+        while (l <= r) {
+            if (l & 1) ret += tree[l].end() - upper_bound(all(tree[l]), k), l++;
+            if (~r & 1) ret += tree[r].end() - upper_bound(all(tree[r]), k), r--;
+            l >>= 1, r >>= 1;
+        }
+        return ret;
+    }
+}mstree;
+
+int n;
+vector<int> arr;
+
+int main() {
+    #ifndef ONLINE_JUDGE
+	// Enter the absolute path of the local file input.txt, output.txt
+	// Or just enter the "input.txt", "output.txt"
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
+	#endif
+
+    cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    cin >> n;
+    arr.resize(n);
+    for (auto& i : arr) cin >> i;
+
+    mstree.build(arr);
+
+    int q; cin >> q;
+    while (q--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        int ans = mstree.query(a, b, c);
+        cout << ans << '\n';
+    }
+    
+    return 0;
+}*/
+
+/*#include <iostream>
 #include <queue>
 #include <algorithm>
 using namespace std;
@@ -62,4 +197,4 @@ int main() {
 	for (int i = 0; i < M; i++) cout << ans[i] << '\n';
 
 	return 0;
-}
+}*/
