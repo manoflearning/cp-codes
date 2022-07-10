07bc4c004303b4b3470c5f079a068cb593daed2e
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-07-10 20:52:51 +0900

    boj

--- Baekjoon OJ/01000-03999/2749.cpp
+++ Baekjoon OJ/01000-03999/2749.cpp
@@ -1,54 +1,88 @@
-#include <iostream>
-#include <vector>
-#define ll long long
-#define vvl vector<vector<ll>>
+#define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+#include <cassert>
 using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define fr first 
+#define sc second
+#define vt vector
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
 
-const int MOD = 1e6;
+#define EXPAND( x ) x // Use this if MS Visual Studio doesn't expand __VA_ARGS__ correctly
+#define F_OR(i, a, b, s) for (int i = (a); (s) > 0 ? i < (b) : i > (b); i += (s))
+#define F_OR1(e) F_OR(i, 0, e, 1)
+#define F_OR2(i, e) F_OR(i, 0, e, 1)
+#define F_OR3(i, b, e) F_OR(i, b, e, 1)
+#define F_OR4(i, b, e, s) F_OR(i, b, e, s)
+#define GET5(a, b, c, d, e, ...) e
+#define F_ORC(...) EXPAND( GET5(__VA_ARGS__, F_OR4, F_OR3, F_OR2, F_OR1) )
+#define FOR(...) EXPAND( F_ORC(__VA_ARGS__ )(__VA_ARGS__) )
+#define EACH(x, a) for (auto& x : a)
 
-vvl arr;
+const double EPS = 1e-9;
+const int INF = 1e9 + 7;
+const int MOD = 1000000;
+const int dy[] = { 0, 0, 1, -1, 1, 1, -1, -1 };
+const int dx[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
-vvl F(ll x); //F(x)는 x번째 피보나치 수
-vvl mul(vvl& a, vvl& b);
+struct Matrix {
+	vt<vt<ll>> a;
+	Matrix operator*(const Matrix& rhs) const {
+		Matrix ret;
+		ret.a.resize(sz(a), vt<ll>(sz(rhs.a[0])));
+		for (int y = 0; y < sz(ret.a); y++) {
+			for (int x = 0; x < sz(ret.a[y]); x++) {
+				ll sum = 0;
+				for (int i = 0; i < sz(a[y]); i++) {
+					sum = (sum + a[y][i] * rhs.a[i][x]) % MOD;
+				}
+				ret.a[y][x] = sum;
+			}
+		}
+		return ret;
+	}
+};
+
+Matrix ma;
+
+Matrix f(Matrix a, ll cnt) {
+	if (cnt == 1) return a;
+
+	Matrix res = f(a, cnt / 2);
+	if (cnt & 1) return res * res * ma;
+	else return res * res;
+}
 
 int main() {
+	#ifndef ONLINE_JUDGE
+	// Enter the absolute path of the local file input.txt, output.txt
+	// Or just enter the "input.txt", "output.txt"
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
+	#endif
+
 	cin.tie(NULL); cout.tie(NULL);
 	ios_base::sync_with_stdio(false);
+	
+	ll n; cin >> n;
 
-	arr.resize(2, vector<ll>(2));
-	arr[0][0] = arr[0][1] = arr[1][0] = 1;
-	arr[1][1] = 0;
-
-	ll N; cin >> N;
-
-	if (N <= 2) {
-		if (N == 0) cout << 0 << '\n';
-		else cout << 1 << '\n';
+	if (n == 0 || n == 1) {
+		cout << n;
 		return 0;
 	}
 
-	vvl ans = F(N - 2);
-	cout << (ans[0][0] + ans[1][0]) % MOD << '\n';
+	ma.a.resize(2, vt<ll>(2));
+	ma.a[0][0] = 1, ma.a[0][1] = 1;
+	ma.a[1][0] = 1, ma.a[1][1] = 0;
+
+	Matrix res = f(ma, n - 1);
+
+	cout << res.a[0][0];
 
 	return 0;
-}
-
-vvl F(ll x) {
-	if (x == 1) return arr;
-	vvl prv = F(x >> 1);
-	vvl ret = mul(prv, prv);
-	if (x & 1) ret = mul(ret, arr);
-	return ret;
-}
-
-vvl mul(vvl& a, vvl& b) {
-	vvl ret(2, vector<ll>(2));
-	for (int i = 0; i < 2; i++) {
-		for (int l = 0; l < 2; l++) {
-			for (int k = 0; k < 2; k++) {
-				ret[i][l] = (ret[i][l] + a[i][k] * b[k][l]) % MOD;
-			}
-		}
-	}
-	return ret;
 }
