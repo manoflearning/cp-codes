2bcd01eb9438fd43c8139a4db1427c278794a05e
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-07-22 01:43:40 +0900

    cf

--- Baekjoon OJ/16000-18999/16563.cpp
+++ Baekjoon OJ/16000-18999/16563.cpp
@@ -1,5 +1,54 @@
-// 정해
 #define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+const int MAXN = 5000000;
+
+vector<int> sp(MAXN + 1);
+vector<ll> prime;
+
+// Determine prime numbers between 1 and MAXN in O(MAXN) 
+void linearSieve() {
+	for (int i = 2;i <= MAXN; i++) {
+		if (!sp[i]) {
+			prime.push_back(i);
+			sp[i] = i;
+		}
+		for (auto j : prime) {
+			if (i * j > MAXN) break;
+			sp[i * j] = j;
+			if (i % j == 0) break;
+		}
+	}
+}
+
+// factorization in O(log x)
+void factorization(int x) {
+    while (x > 1) {
+		cout << sp[x] << ' ';
+		x /= sp[x];
+	}
+	cout << '\n';
+}
+
+int main() {
+	cin.tie(NULL); cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	
+	linearSieve();
+
+	int n; cin >> n;
+	while (n--) {
+		int x; cin >> x;
+		factorization(x);
+	}
+
+	return 0;
+}
+
+// 정해
+/*#define _USE_MATH_DEFINES
 #include <bits/stdc++.h>
 #include <cassert>
 using namespace std;
@@ -79,10 +128,10 @@ int main() {
 	solve();
 
 	return 0;
-}
+}*/
 
 // 내 풀이
-#define _USE_MATH_DEFINES
+/*#define _USE_MATH_DEFINES
 #include <bits/stdc++.h>
 #include <cassert>
 using namespace std;
@@ -189,4 +238,4 @@ int main() {
 	solve();
 
 	return 0;
-}
+}*/
