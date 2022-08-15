fb2d513961fe58d0a5bb7d7afe9c23d160f60c94
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-08-15 16:07:06 +0900

    SWEA

--- SW Expert Academy/2007.cpp
+++ SW Expert Academy/2007.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 string s;
 
+// 0부터 시작하는 길이 len의 부분문자열과 st부터 시작하는 길이 len의 부분문자열이 일치하면 1, 일치하지 않으면 0을 반환.
 bool isMatched(int st, int len) {
     for (int i = 0; i < min(i + len, sz(s) - st); i++) {
         if (s[i] != s[i + st]) return 0;
@@ -20,6 +21,7 @@ int main() {
         cout << '#' << tt << ' ';
 
         cin >> s;
+        // 1부터 문자열 최대 길이까지 패턴의 가능한 모든 길이를 완전탐색.
         for (int len = 1; len <= sz(s); len++) {
             int bit = 1;
             for (int i = len; i < sz(s); i += len) {
