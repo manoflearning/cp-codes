6530cfe4f85fd157e8ac44de45b82023429b6449
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-08-31 11:03:09 +0900

    boj

--- Baekjoon OJ/13000-13999/13275.cpp
+++ Baekjoon OJ/13000-13999/13275.cpp
@@ -1,36 +1,42 @@
-// Manacher's Algorithm
-// Find all palindromes in string in O(N)
-
-// BOJ 14444 AC Code
-// https://www.acmicpc.net/problem/14444
 #include <bits/stdc++.h>
+#include <cassert>
 using namespace std;
-#define sz(x) (x).size()
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define fr first
+#define sc second
+#define vt vector
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
 
-const int MAXS = 101010 * 2;
+#define EXPAND( x ) x // Use this if MS Visual Studio doesn't expand __VA_ARGS__ correctly
+#define F_OR(i, a, b, s) for (int i = (a); (s) > 0 ? i < (b) : i > (b); i += (s))
+#define F_OR1(e) F_OR(i, 0, e, 1)
+#define F_OR2(i, e) F_OR(i, 0, e, 1)
+#define F_OR3(i, b, e) F_OR(i, b, e, 1)
+#define F_OR4(i, b, e, s) F_OR(i, b, e, s)
+#define GET5(a, b, c, d, e, ...) e
+#define F_ORC(...) EXPAND( GET5(__VA_ARGS__, F_OR4, F_OR3, F_OR2, F_OR1) )
+#define FOR(...) EXPAND( F_ORC(__VA_ARGS__ )(__VA_ARGS__) )
+#define EACH(x, a) for (auto& x : a)
 
+const double EPS = 1e-9;
+const int INF = 1e9 + 7;
+const int MOD = 1e9 + 7;
+const int dy[] = { 0, 0, 1, -1, 1, 1, -1, -1 };
+const int dx[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+int n;
 string s;
-int n, p[MAXS];
-// n: length of string
-// p[i]: the radius of the palindrome at the current position i
-int r = -1, c = -1;
-// r: end of palindrome
-// c: center of palindrome
+vt<int> p;
 
-int main() {
-    #ifndef ONLINE_JUDGE
-	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
-	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
-	#endif
-
-    cin.tie(NULL); cout.tie(NULL);
-    ios_base::sync_with_stdio(false);
-
-    cin >> s;
+void manacher() {
     n = sz(s);
-
-    // Preprocessing for determining even-length pelindromes
-    s.resize(MAXS);
+    s.resize(n << 1 | 1);
+    p.resize(n << 1 | 1);
 
     for (int i = n - 1; i >= 0; i--) {
         s[i << 1 | 1] = s[i];
@@ -39,30 +45,41 @@ int main() {
     n <<= 1;
     s[n++] = '#';
 
-    // Processing
+    int r = -1, c = -1;
     for (int i = 0; i < n; i++) {
-        if (r >= i) p[i] = min(r - i, p[c * 2 - i]);
+        if (i <= r) p[i] = min(r - i, p[2 * c - i]);
         else p[i] = 0;
 
         while (1) {
-            if (i - p[i] - 1 < 0) break;
-            if (i + p[i] + 1 >= n) break;
-            if (s[i + p[i] + 1] != s[i - p[i] - 1]) break;
+            if (i - p[i] - 1 < 0 || n <= i + p[i] + 1) break;
+            if (s[i - p[i] - 1] != s[i + p[i] + 1]) break;
             p[i]++;
         }
 
-        if (i + p[i] > r) {
+        if (r < i + p[i]) {
             r = i + p[i], c = i;
         }
     }
+}
 
-    // get answer
-    int ans = 1;
-    for (int i = 0; i < n; i++) {
+int main() {
+	#ifndef ONLINE_JUDGE
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
+	#endif
+
+	cin.tie(NULL); cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	cin >> s;
+
+    manacher();
+
+    int ans = 0;
+    for (int i = 0; i < n; i++)
         ans = max(ans, p[i]);
-    }
 
     cout << ans;
 
-    return 0;
+	return 0;
 }
