cb363c83bdeebd6a28b175428b1d57784e9cd913
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-09-13 22:18:38 +0900

    boj

---
+++ Baekjoon OJ/23000-23999/23248.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+#include <cassert>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define fr first
+#define sc second
+#define vt vector
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+
+#define EXPAND( x ) x // Use this if MS Visual Studio doesn't expand __VA_ARGS__ correctly
+#define F_OR(i, a, b, s) for (int i = (a); (s) > 0 ? i < (b) : i > (b); i += (s))
+#define F_OR1(e) F_OR(i, 0, e, 1)
+#define F_OR2(i, e) F_OR(i, 0, e, 1)
+#define F_OR3(i, b, e) F_OR(i, b, e, 1)
+#define F_OR4(i, b, e, s) F_OR(i, b, e, s)
+#define GET5(a, b, c, d, e, ...) e
+#define F_ORC(...) EXPAND( GET5(__VA_ARGS__, F_OR4, F_OR3, F_OR2, F_OR1) )
+#define FOR(...) EXPAND( F_ORC(__VA_ARGS__ )(__VA_ARGS__) )
+#define EACH(x, a) for (auto& x : a)
+
+const double EPS = 1e-9;
+const int INF = 1e9 + 7;
+const int MOD = 1e9 + 7;
+const int dy[] = { 0, 0, 1, -1, 1, 1, -1, -1 };
+const int dx[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+int flag;  // array size
+struct Seg {  // 1-indexed
+	vector<ll> t;
+
+	void build(int n) {
+		for (flag = 1; flag < n; flag <<= 1);
+		t.resize(2 * flag);
+
+		//for (int i = flag; i < flag + n; i++) cin >> t[i];
+		//for (int i = flag - 1; i >= 1; i--) t[i] = t[i << 1] + t[i << 1 | 1];
+	}
+	void modify(int p, ll value) {  // set value at position p
+        t[p + flag - 1] = max(t[p + flag - 1], value);
+		for (p += flag - 1; p > 1; p >>= 1) t[p >> 1] = max(t[p], t[p ^ 1]);
+	}
+	ll query(int l, int r, int n = 1, int nl = 1, int nr = flag) {  // sum on interval [l, r]
+		if (r < nl || nr < l) return 0;
+		if (l <= nl && nr <= r) return t[n];
+
+		int mid = (nl + nr) / 2;
+		return max(query(l, r, n << 1, nl, mid), query(l, r, n << 1 | 1, mid + 1, nr));
+	}
+}seg;
+
+int n, m, k;
+vt<int> r[101010];
+
+void input() {
+    cin >> n >> m >> k;
+    FOR(k) {
+        int y, x;
+        cin >> y >> x;
+        r[y].push_back(x);
+    }
+}
+
+int main() {
+	#ifndef ONLINE_JUDGE
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
+	#endif
+
+	cin.tie(NULL); cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	input();
+
+    FOR(101010) sort(all(r[i]));
+
+    seg.build(101010);
+
+    int ans = 0;
+    FOR(101010) {
+        EACH(x, r[i]) {
+            int res = seg.query(1, x) + 1;
+            ans = max(ans, res);
+            seg.modify(x, res);
+
+            cout << i << ' ' << x << ' ' << res << '\n';
+        }
+    }
+
+    cout << ans;
+
+	return 0;
+}
