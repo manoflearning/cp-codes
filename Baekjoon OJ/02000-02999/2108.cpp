8a44f3ad4a04e61c75115d08a81a12068e990a10
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-10-03 21:11:42 +0900

    boj

--- Baekjoon OJ/02000-02999/2108.cpp
+++ Baekjoon OJ/02000-02999/2108.cpp
@@ -1,75 +1,73 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
+#include <cassert>
 using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define fr first
+#define sc second
+#define vt vector
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
 
-int n;
-int arr[8001];
+#define EXPAND( x ) x // Use this if MS Visual Studio doesn't expand __VA_ARGS__ correctly
+#define F_OR(i, a, b, s) for (int i = (a); (s) > 0 ? i < (b) : i > (b); i += (s))
+#define F_OR1(e) F_OR(i, 0, e, 1)
+#define F_OR2(i, e) F_OR(i, 0, e, 1)
+#define F_OR3(i, b, e) F_OR(i, b, e, 1)
+#define F_OR4(i, b, e, s) F_OR(i, b, e, s)
+#define GET5(a, b, c, d, e, ...) e
+#define F_ORC(...) EXPAND( GET5(__VA_ARGS__, F_OR4, F_OR3, F_OR2, F_OR1) )
+#define FOR(...) EXPAND( F_ORC(__VA_ARGS__ )(__VA_ARGS__) )
+#define EACH(x, a) for (auto& x : a)
 
-int sum;
+const double EPS = 1e-9;
+const int INF = 1e9 + 7;
+const int MOD = 1e9 + 7;
+const int dy[] = { 0, 0, 1, -1, 1, 1, -1, -1 };
+const int dx[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
 int main() {
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++) {
-		int flag;
-		scanf("%d", &flag);
+	#ifndef ONLINE_JUDGE
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
+	#endif
 
-		sum += flag;
-		arr[flag + 4000]++;
+	cin.tie(NULL); cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int n; cin >> n;
+	vt<int> a(n), cnt(8080);
+	ll sum = 0;
+	EACH(i, a) {
+		cin >> i;
+		sum += i;
+		cnt[i + 4040]++;
 	}
-	//1
-	printf("%.0lf\n", (double)sum / n);
-	//2
-	int count = 0;
-	for (int i = 0; ; i++) {
-		count += arr[i];
-		if (count >= n / 2 + 1) {
-			printf("%d\n", i - 4000);
-			break;
+
+	sort(all(a));
+	
+	cout << (sum / n + (abs(sum % n) >= (n + 1) / 2 ? sum / abs(sum) : 0)) << '\n';
+	
+	cout << a[n / 2] << '\n';
+
+	ll mx = 1;
+	vt<int> val;
+	FOR(8080) {
+		if (cnt[i] > mx) {
+			val.clear();
+			mx = cnt[i];
+			val.push_back(i - 4040);
+		}
+		else if (cnt[i] == mx) {
+			val.push_back(i - 4040);
 		}
 	}
-	//3
-	int value = -1;
-	for (int i = 0; i <= 8000; i++)
-		value = max(value, arr[i]);
-	int check = 0;
-	for (int i = 0; i <= 8000; i++) {
-		if (arr[i] == value) check++;
-		if (check == 2) {
-			printf("%d\n", i - 4000);
-			break;
-		}
-	}
-	if (check == 1) {
-		for (int i = 0; i <= 8000; i++) {
-			if (arr[i] == value) {
-				printf("%d\n", i - 4000);
-				break;
-			}
-		}
-	}
-	//4
-	int min, max;
-	for (int i = 0; ; i++) {
-		if (arr[i] != 0) {
-			min = i;
-			break;
-		}
-	}
-	for (int i = 8000; ; i--) {
-		if (arr[i] != 0) {
-			max = i;
-			break;
-		}
-	}
-	printf("%d\n", max - min);
+
+	cout << (sz(val) >= 2 ? val[1] : val[0]) << '\n';
+	cout << a.back() - a[0] << '\n';
 
 	return 0;
 }
-
-/*
-문제 해법		: 카운팅 정렬을 활용
-접근 방식		:
-결정적 깨달음	:
-오답 원인		: 1. 
-				  2.
-*/
--- Baekjoon OJ/10000-10999/10828.cpp
+++ Baekjoon OJ/10000-10999/10828.cpp
@@ -1,71 +1,73 @@
-#include <iostream>
-#include <cstring>
-#include <vector>
+#include <bits/stdc++.h>
+#include <cassert>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define fr first
+#define sc second
+#define vt vector
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
 
-std::vector<int> stack;
-int size=-1;
+#define EXPAND( x ) x // Use this if MS Visual Studio doesn't expand __VA_ARGS__ correctly
+#define F_OR(i, a, b, s) for (int i = (a); (s) > 0 ? i < (b) : i > (b); i += (s))
+#define F_OR1(e) F_OR(i, 0, e, 1)
+#define F_OR2(i, e) F_OR(i, 0, e, 1)
+#define F_OR3(i, b, e) F_OR(i, b, e, 1)
+#define F_OR4(i, b, e, s) F_OR(i, b, e, s)
+#define GET5(a, b, c, d, e, ...) e
+#define F_ORC(...) EXPAND( GET5(__VA_ARGS__, F_OR4, F_OR3, F_OR2, F_OR1) )
+#define FOR(...) EXPAND( F_ORC(__VA_ARGS__ )(__VA_ARGS__) )
+#define EACH(x, a) for (auto& x : a)
 
-void push();
-void pop();
-void size_();
-void empty();
-void top();
+const double EPS = 1e-9;
+const int INF = 1e9 + 7;
+const int MOD = 1e9 + 7;
+const int dy[] = { 0, 0, 1, -1, 1, 1, -1, -1 };
+const int dx[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+stack<int> stk;
 
 int main() {
-	int n;
-	std::cin>>n;
-	
-	for(int i=0; i<n; i++) {
-		char command[6];
-		std::cin>>command;
-		
-		if(strcmp(command, "push") == 0)
-			push();
-		else if(strcmp(command, "pop") == 0)
-			pop();
-		else if(strcmp(command, "size") == 0)
-			size_();
-		else if(strcmp(command, "empty") == 0)
-			empty();
-		else if(strcmp(command, "top") == 0)
-			top();
+	#ifndef ONLINE_JUDGE
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
+	#endif
+
+	cin.tie(NULL); cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int n; cin >> n;
+	while (n--) {
+		string op;
+		int x;
+		cin >> op;
+
+		if (op == "push") {
+			cin >> x;
+			stk.push(x);
+		}
+		else if (op == "pop") {
+			if (stk.empty()) cout << -1 << '\n';
+			else {
+				cout << stk.top() << '\n';
+				stk.pop();
+			}
+		}
+		else if (op == "size") {
+			cout << sz(stk) << '\n';
+		}
+		else if (op == "empty") {
+			cout << (stk.empty() ? 1 : 0) << '\n';
+		}
+		else if (op == "top") {
+			if (stk.empty()) cout << -1 << '\n';
+			else cout << stk.top() << '\n';
+		}
 	}
-	
+
 	return 0;
 }
-
-void push() {
-	stack.resize(size+2);
-	for(int i=size; i>=0; i--)
-		stack[i+1] = stack[i];
-	size++;
-	
-	std::cin>>stack[0];
-}
-void pop() {
-	if(size==-1)
-		std::cout<<"-1"<<'\n';
-	else {
-		std::cout<<stack[0]<<'\n';
-		
-		for(int i=1; i<=size; i++)
-			stack[i-1] = stack[i];
-		size--;
-	}
-}
-void size_() {
-	std::cout<<size+1<<'\n';
-}
-void empty() {
-	if(size==-1)
-		std::cout<<'1'<<'\n';
-	else
-		std::cout<<'0'<<'\n';
-}
-void top() {
-	if(size==-1)
-		std::cout<<"-1"<<'\n';
-	else
-		std::cout<<stack[0]<<'\n';
-}
-
