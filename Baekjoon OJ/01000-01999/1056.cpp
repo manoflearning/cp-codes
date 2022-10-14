3e45fb7142cd163832660e7a168d94c12202747e
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-10-14 20:07:51 +0900

    boj

--- Baekjoon OJ/01000-01999/1056.cpp
+++ Baekjoon OJ/01000-01999/1056.cpp
@@ -53,11 +53,6 @@ ll f(ll x) {
 }
 
 int main() {
-    #ifndef ONLINE_JUDGE
-    freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
-    freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
-    #endif
-
     cin.tie(NULL); cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
