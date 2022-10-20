2425d69a259339aed110aa9b0fe9a89ab66ecccd
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-10-20 21:39:00 +0900

    boj

---
+++ Baekjoon OJ/09000-09999/9417.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int gcd(int a, int b) {
+	if (b == 0) return a;
+	else return gcd(b, a % b);
+}
+
+int main() {
+    #ifndef ONLINE_JUDGE
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
+	#endif
+
+    cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int tc; cin >> tc;
+    
+    string du;
+    getline(cin, du);
+
+    while (tc--) {
+        string s;
+        getline(cin, s);
+
+        vector<int> a;
+
+        int prv = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == ' ') {
+                string num;
+                for (int j = prv; j < i; j++)
+                    num.push_back(s[j]);
+
+                prv = i + 1;
+                a.push_back(stoi(num));
+            }
+        }
+
+        string num;
+        for (int j = prv; j < s.size(); j++) {
+            num.push_back(s[j]);
+        }
+        if (num.size()) a.push_back(stoi(num));
+
+        int ans = 0;
+        for (int i = 0; i < a.size(); i++) {
+            for (int j = i + 1; j < a.size(); j++) {
+                ans = max(ans, gcd(a[i], a[j]));
+            }
+        }
+
+        cout << ans << '\n';
+    }
+}
