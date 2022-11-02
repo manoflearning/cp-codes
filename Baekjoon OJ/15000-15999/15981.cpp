eacca5024734c2e74eb80abca2ac7d49c81a1b2d
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-11-02 10:29:22 +0900

    boj

--- Baekjoon OJ/15000-15999/15981.cpp
+++ Baekjoon OJ/15000-15999/15981.cpp
@@ -3,37 +3,33 @@ using namespace std;
 #define sz(x) (int)(x).size()
 
 int main() {
-    #ifndef ONLINE_JUDGE
-	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
-	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
-	#endif
-
 	cin.tie(NULL); cout.tie(NULL);
 	ios_base::sync_with_stdio(false);
 
     int n; cin >> n;
 
-    if (n == 3) {
-        cout << n - 1 << '\n';
-        for (int i = 2; i <= n; i++) {
-            cout << 2 << ' ' << 1 << ' ' << i << '\n';
-        }
-        return 0;
-    }
+    int flag = 1;
+    for (; flag < n; flag <<= 1);
     
     vector<vector<int>> ans;
-    for (int i = n / 2; i >= 1; i >>= 1) {
-        for (int k = 1; k <= n; k += i) {
-            vector<int> res;
-            for (int j = k; j <= n; j++) {
-                if (sz(res) && sz(res) % i == 0) j += i;
+    for (int i = flag >> 1; i >= 1; i >>= 1) {
+        vector<int> res;
+        for (int j = 1; j <= n; j++) {
+            if (sz(res) && sz(res) % i == 0) j += i;
                 
-                if (j > n) break;
+            if (j > n) break;
 
-                res.push_back(j);
-            }
-            ans.push_back(res);
-        }   
+            res.push_back(j);
+        }
+        ans.push_back(res);
+    }
+
+    cout << sz(ans) << '\n';
+    for (auto& i : ans) {
+        cout << sz(i) << ' ';
+        for (auto& j : i) 
+            cout << j << ' ';
+        cout << '\n';
     }
 
 	return 0;
