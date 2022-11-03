d92911e5b9aa0d78ed10a417d1d69b91541f4c54
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-11-03 17:38:45 +0900

    boj

--- Baekjoon OJ/13000-13999/13974.cpp
+++ Baekjoon OJ/13000-13999/13974.cpp
@@ -1,54 +1,58 @@
-#pragma GCC optimize("O3")
-#pragma GCC optimize("unroll-loops")
+// Knuth Optimization
+// Recurrence: DP[i][j] = min(DP[i][k] + DP[k + 1][j]) + C[i][j] (i <= k < j)
+// Condition: C[i][j] is a monge array (satisfies C[a][c] + C[b][d] <= C[a][d] + C[b][c]),
+//            and satisfies C[a][d] >= C[b][c] for a <= b <= c <= d
+// Naive Complexity: O(n^3)
+// Optimized Complexity: O(n^2)
+
+// Let opt[i][j] be the value of k that minimizes DP[i][j]
+// The following holds: opt[i][j - 1] <= opt[i][j] <= opt[i + 1][j]
+
+// BOJ 13974 AC Code
+// https://www.acmicpc.net/problem/13974
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
 
 const ll INF = 1e18;
 
-int n;
-ll a[5050], dp[5050][5050], psum[5050];
-
-void init() {
-    
-}
+int n, opt[5050][5050];
+ll a[5050], DP[5050][5050], psum[5050];
 
 int main() {
-    #ifndef ONLINE_JUDGE
-	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
-	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
-	#endif
-
     cin.tie(NULL); cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
     int tc; cin >> tc;
     while (tc--) {
-        init();
-
         cin >> n;
         for (int i = 1; i <= n; i++) {
             cin >> a[i];
             psum[i] = a[i] + psum[i - 1];
         }
 
-        for (int i = 1; i <= n; i++) dp[i][i] = 0;
+        for (int i = 1; i <= n; i++) {
+            DP[i][i] = 0;
+            opt[i][i] = i;
+        }
 
-        for (int len = 2; len <= n; len++) {
-            for (int l = 1; l + len - 1 <= n; l++) {
-                int r = l + len - 1;
-                
-                dp[l][r] = INF;
-
-                for (int i = l; i < r; i++) {
-                    dp[l][r] = min(dp[l][r], dp[l][i] + dp[i + 1][r]);
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = i + 1; j <= n; j++) {
+                ll mn = INF, mnk = -1;
+                for (int k = opt[i][j - 1]; k <= opt[i + 1][j]; k++) {
+                    ll res = DP[i][k] + DP[k + 1][j] + (psum[j] - psum[i - 1]);
+                    if (res < mn) {
+                        mn = res;
+                        mnk = k;
+                    }
                 }
 
-                dp[l][r] += psum[r] - psum[l - 1];
+                DP[i][j] = mn;
+                opt[i][j] = mnk;
             }
         }
 
-        cout << dp[1][n] << '\n';
+        cout << DP[1][n] << '\n';
     }
 
     return 0;
