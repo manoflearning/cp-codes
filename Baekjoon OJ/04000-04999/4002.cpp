ec5dba7c95e83e2e597a05cad2f12db71ada310b
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-11-11 14:28:58 +0900

    boj

--- Baekjoon OJ/04000-04999/4002.cpp
+++ Baekjoon OJ/04000-04999/4002.cpp
@@ -1,47 +1,52 @@
-#pragma GCC optimize("O3")
-#pragma GCC optimize("unroll-loops")
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-#define sz(x) (int)(x).size()
+#define pll pair<ll, ll>
 #define fr first
 #define sc second
 
-int n, par[101010], r;
+int flag;
+struct Seg {
+    vector<pll> t;
+    
+    void build(int n) {
+        for (flag = 1; flag < n; flag <<= 1);
+        t.resize(flag << 1);
+    }
+    void modify(int p, ll x) {
+        for (t[p += flag - 1] = { x, p }; p > 1; p >>= 1)
+            t[p >> 1] = max(t[p], t[p ^ 1]);
+    }
+    pll query(int l, int r, int n = 1, int nl = 1, int nr = flag) {
+        if (nr < l || r < nl) return { 0, 0 };
+        if (l <= nl && nr <= r) return t[n];
+
+        int mid = (nl + nr) >> 1;
+        return max(query(l, r, n << 1, nl, mid), query(l, r, n << 1 | 1, mid + 1, nr));
+    }
+}seg;
+
+int n, par[101010], r, in[101010], out[101010], cnt;
 ll m, w[101010], l[101010];
 vector<int> chd[101010], dep[101010];
-
-int in[101010], out[101010], cnt; 
-ll mx[101010];
-set<int> s;
-int wbyin[101010];
-
-pair<ll, int> dp[101010];
+pll dp[101010];
 
 void input() {
     cin >> n >> m;
-    for (int i = 1; i <= n; i++) {
+    for (int v = 1; v <= n; v++) {
         int p;
-        cin >> p >> w[i] >> l[i];
-        if (p == 0) r = i;
-        else {
-            par[i] = p;
-            chd[p].push_back(i);
-        }
+        cin >> p >> w[v] >> l[v];
+        if (p == 0) r = v;
+        else chd[p].push_back(v);
     }
 }
 
-void dfs(int v, int prv, int d) {
+void dfs(int v, int d) {
     in[v] = ++cnt;
-    wbyin[in[v]] = w[v];
-    s.insert(in[v]);
-    mx[v] = max(l[v], mx[prv]);
-
     dep[d].push_back(v);
-    for (auto& i : chd[v]) {
-        dfs(i, v, d + 1);
-    }
 
+    for (auto& i : chd[v]) dfs(i, d + 1);
+    
     out[v] = cnt;
 }
 
@@ -55,49 +60,33 @@ int main() {
 	ios_base::sync_with_stdio(false);
 
 	input();
-    
-    dfs(r, 0, 1);
+
+    dfs(r, 1);
+
+    seg.build(n);
+    for (int v = 1; v <= n; v++) {
+        seg.modify(in[v], w[v]);
+    }
 
     ll ans = 0;
 
     for (int d = n; d >= 1; d--) {
         for (int v : dep[d]) {
             auto& res = dp[v];
+            res.fr += w[v], res.sc++;
 
-            res.fr += w[v], res.sc = 1;
-            for (int i : chd[v]) {
+            for (auto& i : chd[v]) {
                 res.fr += dp[i].fr, res.sc += dp[i].sc;
             }
-
-            if (l[v] <= mx[par[v]]) continue;
-
-            if (res.fr > m) {
-                vector<int> arr;
-
-                ll lb = (m + res.sc - 1) / res.sc;
-
-                auto it = s.lower_bound(in[v]);
-                while (it != s.end() && *it <= out[v]) {
-                    if (wbyin[*it] < lb) {
-                        it++;
-                        continue;
-                    }
-                    arr.push_back({ *it });
-                    it++;
-                }
-
-                sort(arr.begin(), arr.end(), [&](int u, int v) {
-                    return wbyin[u] > wbyin[v];
-                });
-
-                int idx = 0;
-                while (res.fr > m) {
-                    res.fr -= wbyin[arr[idx]], res.sc--;
-                    s.erase(arr[idx]);
-                    idx++;
-                }
+            
+            while (res.fr > m) {
+                pll mx = seg.query(in[v], out[v]);
+                assert(0 < mx.fr);
+                res.fr -= mx.fr, res.sc--;
+                assert(1 <= mx.sc && mx.sc <= n);
+                seg.modify(mx.sc, 0);
             }
-
+            
             ans = max(ans, res.sc * l[v]);
         }
     }
