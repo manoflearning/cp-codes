54f499891c5fde97639a0fe31d42e8e4723f0b0c
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-11-14 14:35:23 +0900

    APC

--- .Etc/2022 아주대학교 프로그래밍 경시대회 APC Open Contest/F.cpp
+++ .Etc/2022 아주대학교 프로그래밍 경시대회 APC Open Contest/F.cpp
@@ -1,49 +1,21 @@
-#pragma GCC optimize("O3")
-#pragma GCC optimize("unroll-loops")
 #include <bits/stdc++.h>
-#include <cassert>
 using namespace std;
 #define ll long long
-#define ull unsigned long long
-#define ld long double
-#define pii pair<int, int>
-#define pll pair<ll, ll>
-#define fr first
-#define sc second
-#define vt vector
-#define all(c) (c).begin(), (c).end()
-#define sz(x) (int)(x).size()
 
-#define EXPAND( x ) x // Use this if MS Visual Studio doesn't expand __VA_ARGS__ correctly
-#define F_OR(i, a, b, s) for (int i = (a); (s) > 0 ? i < (b) : i > (b); i += (s))
-#define F_OR1(e) F_OR(i, 0, e, 1)
-#define F_OR2(i, e) F_OR(i, 0, e, 1)
-#define F_OR3(i, b, e) F_OR(i, b, e, 1)
-#define F_OR4(i, b, e, s) F_OR(i, b, e, s)
-#define GET5(a, b, c, d, e, ...) e
-#define F_ORC(...) EXPAND( GET5(__VA_ARGS__, F_OR4, F_OR3, F_OR2, F_OR1) )
-#define FOR(...) EXPAND( F_ORC(__VA_ARGS__ )(__VA_ARGS__) )
-#define EACH(x, a) for (auto& x : a)
-
-const double EPS = 1e-9;
-const ll INF = 1e18;
-const int MOD = 1e9 + 7;
 const int MAX = 999 * 1000 + 999 + 100;
 
-inline int Hash(int y, int x) {
-    return (y - 1) * 1000 + (x - 1);
-}
-
-int n, sy, sx, ey, ex;
-vt<string> a;
+inline int Hash(int y, int x) { return (y - 1) * 1000 + (x - 1); }
 
 struct wv {
     ll w; int v;
     bool operator<(const wv& rhs) const {
-		return w > rhs.w;
-	}
+        return w > rhs.w;
+    }
 };
-vt<wv> adj[MAX];
+
+int n, sy, sx, ey, ex;
+vector<string> a;
+vector<wv> adj[MAX];
 
 void input() {
     cin >> n;
@@ -52,8 +24,12 @@ void input() {
         a[i].resize(n + 1);
         for (int j = 1; j <= n; j++) {
             cin >> a[i][j];
-            if (a[i][j] == 'P') sy = i, sx = j;
-            if (a[i][j] == 'K') ey = i, ex = j;
+            if (a[i][j] == 'P') {
+                sy = i, sx = j;
+            }
+            if (a[i][j] == 'K') {
+                ey = i, ex = j;
+            }
         }
     }
 }
@@ -61,68 +37,42 @@ void input() {
 const int dy[] = { 0, 0, 1, -1, 1, 1, -1, -1 };
 const int dx[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
-void qe(int y, int x) {
-    for (int i = 0; i < 8; i++) {
+void buildQRBedge(int y, int x, char c) {
+    int lb = 0, rb = 8;
+    if (c == 'R') rb = 4;
+    if (c == 'B') lb = 4;
+
+    for (int i = lb; i < rb; i++) {
         int ny = y, nx = x;
         while (1) {
-            ny = ny + dy[i], nx = nx + dx[i];
+            ny += dy[i], nx += dx[i];
             if (ny < 1 || n < ny || nx < 1 || n < nx) break;
-            if (a[ny][nx] == '0') continue;
+            if (a[ny][nx] == '0' || a[ny][nx] == 'P') continue;
             
-            int w = max(abs(ny - y), abs(nx - x));
-            adj[Hash(y, x)].push_back({ w, Hash(ny, nx) });
+            int w = max(abs(y - ny), abs(x - nx));
+            adj[Hash(y, x)].push_back({ w, Hash(ny, nx )});
             break;
         }
     }
 }
 
-void re(int y, int x) {
-    for (int i = 0; i < 4; i++) {
-        int ny = y, nx = x;
-        while (1) {
-            ny = ny + dy[i], nx = nx + dx[i];
-            if (ny < 1 || n < ny || nx < 1 || n < nx) break;
-            if (a[ny][nx] == '0') continue;
-            
-            int w = max(abs(ny - y), abs(nx - x));
-            adj[Hash(y, x)].push_back({ w, Hash(ny, nx) });
-            break;
-        }
-    }
-}
-
-void be(int y, int x) {
-    for (int i = 4; i < 8; i++) {
-        int ny = y, nx = x;
-        while (1) {
-            ny = ny + dy[i], nx = nx + dx[i];
-            if (ny < 1 || n < ny || nx < 1 || n < nx) break;
-            if (a[ny][nx] == '0') continue;
-            
-            int w = max(abs(ny - y), abs(nx - x));
-            adj[Hash(y, x)].push_back({ w, Hash(ny, nx) });
-            break;
-        }
-    }
-}
-
-void ne(int y, int x) {
-    const int dyn[] = { 2, 2, 1, 1, -2, -2, -1, -1 };
-    const int dxn[] = { 1, -1, 2, -2, 1, -1, 2, -2 };
+void buildNedge(int y, int x) {
+    const int dyn[] = { 1, 1, 2, 2, -1, -1, -2, -2 };
+    const int dxn[] = { 2, -2, 1, -1, 2, -2, 1, -1 };
 
     for (int i = 0; i < 8; i++) {
         int ny = y + dyn[i], nx = x + dxn[i];
         if (ny < 1 || n < ny || nx < 1 || n < nx) continue;
-        if (a[ny][nx] == '0') continue;
+        if (a[ny][nx] == '0' || a[ny][nx] == 'P') continue;
         adj[Hash(y, x)].push_back({ 2, Hash(ny, nx) });
     }
 }
 
-void pe(int y, int x) {
+void buildPedge(int y, int x) {
     for (int i = 0; i < 8; i++) {
         int ny = y + dy[i], nx = x + dx[i];
         if (ny < 1 || n < ny || nx < 1 || n < nx) continue;
-        if (a[ny][nx] == '0') continue;
+        if (a[ny][nx] == '0' || a[ny][nx] == 'P') continue;
         adj[Hash(y, x)].push_back({ 1, Hash(ny, nx) });
     }
 }
@@ -132,35 +82,34 @@ void buildGraph() {
         for (int j = 1; j <= n; j++) {
             if (a[i][j] == '0') continue;
 
-            if (a[i][j] == 'Q') qe(i, j);
-            else if (a[i][j] == 'R') re(i, j);
-            else if (a[i][j] == 'B') be(i, j);
-            else if (a[i][j] == 'N') ne(i, j);
-            else if (a[i][j] == 'P') pe(i, j);
+            if (a[i][j] == 'Q') buildQRBedge(i, j, a[i][j]);
+            else if (a[i][j] == 'R') buildQRBedge(i, j, a[i][j]);
+            else if (a[i][j] == 'B') buildQRBedge(i, j, a[i][j]);
+            else if (a[i][j] == 'N') buildNedge(i, j);
+            else if (a[i][j] == 'P') buildPedge(i, j);
         }
     }
 }
 
-vector<ll> dist(MAX, INF);
+ll dijkstra(int st, int en) {
+    vector<ll> dist(MAX, 1e18);
+    priority_queue<wv> pq;
+    dist[st] = 0;
+    pq.push({ 0, st });
 
-ll dijkstra() {
-	priority_queue<wv> pq;
-	pq.push({ 0, Hash(sy, sx) });
-	dist[Hash(sy, sx)] = 0;
-    
-	while (!pq.empty()) {
-		int v = pq.top().v;
-		ll w = pq.top().w;
-		pq.pop();
-		if (w > dist[v]) continue;
-        if (v == Hash(ey, ex)) return w;
-		for (auto& i : adj[v]) {
-			if (dist[i.v] > w + i.w) {
-				dist[i.v] = w + i.w;
-				pq.push({ w + i.w, i.v });
-			}
-		}
-	}
+    while (!pq.empty()) {
+        int v = pq.top().v;
+        ll w = pq.top().w;
+        pq.pop();
+        if (w > dist[v]) continue;
+        if (v == en) return w;
+        for (auto& i : adj[v]) {
+            if (dist[i.v] > w + i.w) {
+                dist[i.v] = w + i.w;
+                pq.push({ w + i.w, i.v });
+            }
+        }
+    }
     return -1;
 }
 
@@ -170,17 +119,14 @@ int main() {
 	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
 	#endif
 
-	cin.tie(NULL); cout.tie(NULL);
-	ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
 
-	input();
+    input();
 
     buildGraph();
-    
-    assert(a[sy][sx] == 'P');
-    assert(a[ey][ex] == 'K');
 
-    cout << dijkstra();
+    cout << dijkstra(Hash(sy, sx), Hash(ey, ex));
 
-	return 0;
+    return 0;
 }
