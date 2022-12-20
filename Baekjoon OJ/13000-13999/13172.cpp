c9d7f01f7c5b5ad07c0eeeae780e1110b9d57211
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-12-20 09:39:23 +0900

    boj

---
+++ Baekjoon OJ/13000-13999/13172.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#include <cassert>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define fr first
+#define sc second
+#define vt vector
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+
+const double EPS = 1e-9;
+const int INF = 1e9 + 7;
+const int MOD = 1e9 + 7;
+const int dy[] = { 0, 0, 1, -1, 1, 1, -1, -1 };
+const int dx[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+ll exp(ll val, ll cnt) {
+    if (cnt == 1) return val;
+    ll res = exp(val, cnt / 2);
+    return res * res % MOD * (cnt & 1 ? val : 1) % MOD;
+}
+
+int main() {
+	#ifndef ONLINE_JUDGE
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
+	#endif
+
+	cin.tie(NULL); cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int n; cin >> n;
+    ll ans = 0;
+    for (int i = 0; i < n; i++) {
+        ll x, y;
+        cin >> x >> y;
+        ans = (ans + y * exp(x, MOD - 2)) % MOD;
+    }
+    cout << ans;
+
+	return 0;
+}
