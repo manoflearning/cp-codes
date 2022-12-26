0c0cf255451a152748e93adddc31b8ba43d99182
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-12-26 14:56:55 +0900

    cf

--- Codeforces/Codeforces Round #829 (Div. 1)/A2.cpp
+++ Codeforces/Codeforces Round #829 (Div. 1)/A2.cpp
@@ -18,6 +18,40 @@ const int MOD = 1e9 + 7;
 const int dy[] = { 0, 0, 1, -1, 1, 1, -1, -1 };
 const int dx[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
+int n, a[202020];
+vector<pii> ans;
+
+void cal(int s, int e) {
+    int idx1 = -1, idx2 = -1;
+    for (int i = s; i <= e; i++) {
+        if (a[i] != 0) {
+            if (idx1 == -1) idx1 = i;
+            else idx2 = i;
+        }
+    }
+
+    if ((idx1 & 1) == (idx2 & 1) && a[idx1] != a[idx2]) {
+        ans.push_back({ s, e });
+    }
+    if ((idx1 & 1) != (idx2 & 1) && a[idx1] == a[idx2]) {
+        ans.push_back({ s, e });
+    }
+
+    if ((idx1 & 1) == (idx2 & 1) && a[idx1] == a[idx2]) {
+        for (int i = s; i <= e; i++) {
+            if (i == idx2 - 1) 
+                ans.push_back({ idx2 - 1, idx2 });
+            else if (i == idx2) continue;
+            else ans.push_back({ i, i });
+        }
+    }
+    if ((idx1 & 1) != (idx2 & 1) && a[idx1] != a[idx2]) {
+        for (int i = s; i <= e; i++) {
+            ans.push_back({ i, i });
+        }
+    }
+} 
+
 int main() {
 	#ifndef ONLINE_JUDGE
 	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
@@ -29,92 +63,35 @@ int main() {
 
 	int tc; cin >> tc;
 	for (int tt = 1; tt <= tc; tt++) {
-		int n; cin >> n;
+		cin >> n;
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            cin >> a[i];
+            sum += a[i];
+        }
+        
+        if (sum % 2) {
+            cout << -1 << '\n';
+            continue;
+        }
 
-		vector<int> a(n + 1), b(n + 1);
-		for (int i = 1; i <= n; i++) {
-			cin >> a[i];
-			b[i] = (i & 1 ? -1 : 1) * a[i];
-		}
+        for (int i = 1; i <= n; i++) {
+            int s = i, e = i;
 
-		// build psum
-		vector<int> ps1(n + 1), ps2(n + 1);
-		for (int i = 1; i <= n; i++) {
-			if (i & 1) {
-				if (a[i] == 1) ps1[i]++, ps2[i]--;
-				if (a[i] == -1) ps1[i]--, ps2[i]++;
-			}
-			else {
-				if (a[i] == 1) ps1[i]--, ps2[i]++;
-				if (a[i] == -1) ps1[i]++, ps2[i]--;
-			}
-			ps1[i] += ps1[i - 1];
-			ps2[i] += ps2[i - 1];
-		}
+            int cnt = (a[i] != 0);
+            while (e + 1 <= n && cnt < 2) {
+                e++;
+                cnt += (a[e] != 0);
+            }
 
-		// edge case
-		if (ps1[n] % 2) {
-			cout << -1 << '\n';
-			continue;
-		}
+            cal(s, e);
+            i = e;
+        }
 
-		// edge case
-		if (ps1[n] == 0) {
-			cout << 1 << '\n';
-			cout << 1 << ' ' << n << '\n';
-			continue;
-		}
-
-		// get answer
-		int val = -ps1[n] / 2;
-		
-		if (val < 0) { // wlog
-			val = -val;
-			for (int i = 1; i <= n; i++) b[i] = -b[i];
-		}
-		
-		vector<pii> res;
-		for (int i = 2; i <= n; i += 2) {
-			if (b[i] == -1) continue;
-
-			int s = i, e = i;
-
-			int sum = b[s];
-			while (e + 2 <= n && b[e + 1] > 0/* && b[e + 2] >= 0*/) {
-				if (sum + b[e + 1] + b[e + 2] > val) break;
-				sum += b[e + 1] + b[e + 2];
-				e = e + 2;
-			}
-			if (e == n - 1 && b[e + 1] > 0 && sum + b[e + 1] <= val) {
-				sum += b[e + 1];
-				e = e + 1;
-			}
-
-			if (sum <= val && sum > 0) {
-				val -= sum;
-				res.push_back({ s, e });
-				i = e;
-			}
-		}
-		
-		if (val > 0) {
-			cout << -1 << '\n';
-			continue;
-		}
-
-		vector<pii> ans;
-		ans.push_back({ 1, res.front().fr - 1 });
-		for (int i = 0; i < sz(res); i++) {
-			ans.push_back(res[i]);
-			if (i + 1 < sz(res)) {
-				ans.push_back({ res[i].sc + 1, res[i + 1].fr - 1 });
-			}
-		}
-		if (ans.back().sc != n) ans.push_back({ ans.back().sc + 1, n });
-
-		cout << sz(ans) << '\n';
-		for (auto& i : ans)
-			cout << i.fr << ' ' << i.sc << '\n';
+        cout << sz(ans) << '\n';
+        for (auto& i : ans)
+            cout << i.fr << ' ' << i.sc << '\n';
+        ans.clear();
 	}
 
 	return 0;
