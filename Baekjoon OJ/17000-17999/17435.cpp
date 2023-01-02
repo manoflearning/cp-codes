f6070aaf3bc93e8e234e5c9e7565af7b04e77c40
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-01-02 17:28:11 +0900

    boj

--- Baekjoon OJ/17000-17999/17435.cpp
+++ Baekjoon OJ/17000-17999/17435.cpp
@@ -1,48 +1,43 @@
-#include <iostream>
-#include <cmath>
+// INPUT: Given a function f: { 1, 2, ..., m } -> { 1, 2, ..., m }.
+// The function f is defined as f_1(x) = f(x), f_{n+1}(x) = f(f_n(x)).
+// OUTPUT: Given n and x, f_n(x).
+// TIME COMPLEXITY: O(mlogn) for build sparse table, O(logn) for each query.
+
+// BOJ 17435 AC Code
+// https://www.acmicpc.net/problem/17435
+
+#include <bits/stdc++.h>
 using namespace std;
 
-const int MAX = 200000;
-//f[a][b]는 a에서 2^b번 이동해서 도착하는 노드
-int f[MAX + 1][19];
+const int MAX = 202020, MAXD = 18;
+
+int m, sp[MAX][MAXD + 1];
 
 int main() {
 	cin.tie(NULL); cout.tie(NULL);
 	ios_base::sync_with_stdio(false);
 
-	int m; cin >> m;
-	
-	for (int i = 1; i <= m; i++)
-		cin >> f[i][0];
+	// input
+	cin >> m;
+	for (int i = 1; i <= m; i++) cin >> sp[i][0];
 
-	for (int i = 1; i <= 18; i++) {
+	// build sparse table
+	for (int i = 1; i <= MAXD; i++) {
 		for (int j = 1; j <= m; j++) {
-			f[j][i] = f[f[j][i - 1]][i - 1];
+			sp[j][i] = sp[sp[j][i - 1]][i - 1];
 		}
 	}
 
-	int Q; cin >> Q;
-	
-	while (Q--) {
+	// query
+	int q; cin >> q;
+	while (q--) {
 		int n, x;
 		cin >> n >> x;
-
-		for (int i = 18; i >= 0; i--) {
-			if (n >= 1 << i) {
-				n -= 1 << i;
-				x = f[x][i];
-			}
+		for (int i = MAXD; i >= 0; i--) {
+			if (n & (1 << i)) x = sp[x][i];
 		}
-
 		cout << x << '\n';
 	}
 
 	return 0;
 }
-/*////////////////////////////////////////////////////////////////////
-문제 해법		: 희소 테이블
-접근 방식		:
-결정적 깨달음		:
-오답 원인		: 1. 
-				  2.
-*/////////////////////////////////////////////////////////////////////
