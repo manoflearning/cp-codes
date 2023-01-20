3636cb3765f7494921895dc50b73ad1e51859444
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-01-20 19:12:27 +0900

    boj

--- Baekjoon OJ/25000-25999/25402.cpp
+++ Baekjoon OJ/25000-25999/25402.cpp
@@ -2,47 +2,38 @@
 using namespace std;
 #define ll long long
 
-vector<ll> p(252525, -1);
+vector<ll> uf(252525, -1);
 
 int find(int x) {
-    if (p[x] < 0) return x;
-    return p[x] = find(p[x]);
+    if (uf[x] < 0) return x;
+    return uf[x] = find(uf[x]);
 }
 
 void merge(int u, int v) {
     int U = find(u), V = find(v);
     if (U == V) return;
-    p[U] += p[V];
-    p[V] = U;
+    uf[U] += uf[V];
+    uf[V] = U;
 }
 
-int n, q, par[252525];
-vector<int> adj[252525];
+int n, k, par[252525], exist[252525];
+vector<int> adj[252525], arr;
 
-void dfs(int v, int prv) {
-    par[v] = prv;
-    for (auto& i : adj[v]) {
-        if (i == prv) continue;
-        dfs(i, v);
-    }
-}
-
-int m;
-vector<int> arr;
 int vi[252525];
-
-void init() {
-    for (auto& i : arr) {
-        vi[i] = 0;
-        p[i] = -1;
+void dfs(int v, int prv) {
+    vi[v] = 1;
+    for (auto& i : adj[v]) {
+        if (vi[i]) continue;
+        par[i] = v;
+        dfs(i, v);
     }
 }
 
 int main() {
     #ifndef ONLINE_JUDGE
-	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
-	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
-	#endif
+    freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
+    freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
+    #endif
 
     cin.tie(NULL); cout.tie(NULL);
     ios_base::sync_with_stdio(false);
@@ -57,33 +48,35 @@ int main() {
 
     dfs(1, 0);
 
-    cin >> q;
+    int q; cin >> q;
     while (q--) {
-        cin >> m;
-        arr.resize(m);
+        cin >> k;
+        arr.resize(k);
         for (auto& i : arr) {
             cin >> i;
-            vi[i] = 1;
+            exist[i] = 1;
         }
 
         for (auto& i : arr) {
-            if (vi[par[i]]) merge(i, par[i]);
+            if (exist[i] && exist[par[i]]) {
+                merge(i, par[i]);
+            }
         }
 
         set<int> s;
-        for (auto& i : arr) {
+        for (auto& i : arr)
             s.insert(find(i));
-        }
 
         ll ans = 0;
         for (auto& i : s) {
-            ans += -p[i] * (-p[i] - 1) / 2;
+            ans += -uf[i] * (-uf[i] - 1) / 2;
         }
 
         cout << ans << '\n';
 
-        init();
+        for (auto& i : arr) {
+            uf[i] = -1;
+            exist[i] = 0;
+        }
     }
-
-    return 0;
 }
