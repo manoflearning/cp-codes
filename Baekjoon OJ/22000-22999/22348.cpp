be15cad16c0013671010a4fcb5bade25a6642918
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-01-22 01:43:23 +0900

    boj

--- Baekjoon OJ/22000-22999/22348.cpp
+++ Baekjoon OJ/22000-22999/22348.cpp
@@ -1,52 +1,47 @@
-#pragma GCC optimize("O3")
-#pragma GCC optimize("unroll-loops")
 #include <bits/stdc++.h>
 using namespace std;
+#define ll long long
 
 const int MOD = 1e9 + 7;
+const int MXT = 455;
 
-int a, b;
-int dp[2][50505], psum[455][50505];
+ll dp[MXT][50505], psum[MXT][50505];
 
 int main() {
     #ifndef ONLINE_JUDGE
-	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
-	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
-	#endif
+    freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
+    freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
+    #endif
 
-	cin.tie(NULL); cout.tie(NULL);
-	ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
     
     dp[0][0] = 1;
-    for (int i = 1; i * (i + 1) / 2 <= 100000; i++) {
-        int bit = (i & 1);
-        memset(dp[bit], 0, sizeof(dp[bit]));
-
-        for (int j = 0; j <= 50000; j++) {
-            if (dp[!bit][j]) {
-                dp[bit][j + i] = (dp[bit][j + i] + dp[!bit][j]) % MOD;
-                dp[bit][j] = (dp[bit][j] + dp[!bit][j]) % MOD;
+    for (int i = 1; i < MXT; i++) {
+        for (int j = 0; j < 50505; j++) {
+            if (dp[i - 1][j]) {
+                dp[i][j] = (dp[i][j] + dp[i - 1][j]) % MOD;
+                if (j + i < 50505) dp[i][j + i] = (dp[i][j + i] + dp[i - 1][j]) % MOD;
             }
+        }
 
-            if (j == 0) psum[i][j] = dp[bit][j];
-            else psum[i][j] = (dp[bit][j] + psum[i][j - 1]) % MOD;
+        psum[i][0] = dp[i][0];
+        for (int j = 1; j < 50505; j++) {
+            psum[i][j] = (psum[i][j - 1] + dp[i][j]) % MOD;
         }
     }
 
-	int tc; cin >> tc;
+    int tc; cin >> tc;
     while (tc--) {
-        cin >> a >> b;
+        int x, y;
+        cin >> x >> y;
 
-        int ans = 0;
-
-        for (int i = 1; i * (i + 1) / 2 <= a + b; i++) {
-            int sum = i * (i + 1) / 2;
-            ans = (ans + psum[i][a] - (sum - b - 1 >= 0 ? psum[i][sum - b - 1] : 0)) % MOD;
+        ll ans = 0;
+        for (int i = 1; i * (i + 1) / 2 <= x + y; i++) {
+            int idx = i * (i + 1) / 2 - y - 1;
+            ll res = psum[i][x] - (0 <= idx && idx < 50505 ? psum[i][idx] : 0);
+            ans = (ans + res + MOD) % MOD;
         }
-
-        ans = (ans + MOD) % MOD;
         cout << ans << '\n';
     }
-
-	return 0;
 }
