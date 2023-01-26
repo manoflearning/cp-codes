cf90c374b49f2f216a512b847055d0651d8411ad
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-01-26 18:29:20 +0900

    boj

--- Baekjoon OJ/02000-02999/2042.cpp
+++ Baekjoon OJ/02000-02999/2042.cpp
@@ -1,65 +1,47 @@
-#define _USE_MATH_DEFINES
 #include <bits/stdc++.h>
-#include <cassert>
 using namespace std;
 #define ll long long
-#define pii pair<int, int>
-#define pll pair<ll, ll>
-#define vi vector<int>
-#define vvi vector<vector<int>>
-#define vl vector<ll>
-#define vvl vector<vector<ll>>
 
-const int INF = 1e9 + 7;
-const int MOD = 1e9 + 7;
-const int dy[] = { 0, 0, 1, -1 };
-const int dx[] = { 1, -1, 0, 0 };
-
-struct Seg {
-	int flag;  // array size
-	vector<ll> t;
-
-	void build(int N) {
-		for (flag = 1; flag < N; flag <<= 1);
-		t.resize(2 * flag);
-
-		for (int i = flag; i < flag + N; i++) cin >> t[i];
-		for (int i = flag - 1; i >= 1; i--) t[i] = t[i << 1] + t[i << 1 | 1];
+struct Fenwick { // 1-indexed
+	int flag; // array size
+	vector<ll> arr, t;
+	void build(int n) {
+		for (flag = 1; flag < n; flag <<= 1);
+		arr.resize(flag + 1);
+		t.resize(flag + 1);
 	}
-	void modify(int p, ll value) {  // set value at position p
-		for (t[p += flag - 1] = value; p > 1; p >>= 1) t[p >> 1] = t[p] + t[p ^ 1];
+	void modify(int p, ll value) { // set value at position p
+		ll dv = value - arr[p];
+		arr[p] = value;
+		for (; p <= flag; p += (p & -p)) t[p] += dv;
+	}
+	ll query(int x) {
+		ll ret = 0;
+		for (; x; x &= (x - 1)) ret += t[x];
+		return ret;
 	}
 	ll query(int l, int r) {
-		return query(l, r, 1, 1, flag);
+		return query(r) - query(l - 1);
 	}
-	ll query(int l, int r, int n, int nl, int nr) {  // sum on interval [l, r]
-		if (r < nl || nr < l) return 0;
-		if (l <= nl && nr <= r) return t[n];
-
-		int mid = (nl + nr) / 2;
-		return query(l, r, n << 1, nl, mid) + query(l, r, n << 1 | 1, mid + 1, nr);
-	}
-}seg;
-
-int n, m, k;
-
-void input() {
-    cin >> n >> m >> k;
-    seg.build(n);
-}
+}fw;
 
 int main() {
 	cin.tie(NULL); cout.tie(NULL);
 	ios_base::sync_with_stdio(false);
-	
-	input();
-	
-	for (int i = 0; i < m + k; i++) {
-	    ll a, b, c;
-	    cin >> a >> b >> c;
-	    if (a == 1) seg.modify(b, c);
-	    else cout << seg.query(b, c) << '\n';
+
+	int n, m, k;
+	cin >> n >> m >> k;
+
+	fw.build(n);
+	for (int i = 1; i <= n; i++) {
+		ll value; cin >> value;
+		fw.modify(i, value);
 	}
-	
-	return 0;
+
+	for (int i = 0; i < m + k; i++) {
+		ll op, x, y;
+		cin >> op >> x >> y;
+		if (op == 1) fw.modify(x, y);
+		if (op == 2) cout << fw.query(x, y) << '\n';
+	}
 }
