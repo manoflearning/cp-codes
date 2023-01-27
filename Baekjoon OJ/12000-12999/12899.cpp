cdf01bc493bac514b274dd7059167c9b4620d637
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-01-27 21:33:01 +0900

    boj

--- Baekjoon OJ/12000-12999/12899.cpp
+++ Baekjoon OJ/12000-12999/12899.cpp
@@ -1,67 +1,67 @@
-#define _USE_MATH_DEFINES
 #include <bits/stdc++.h>
-#include <cassert>
 using namespace std;
 #define ll long long
-#define pii pair<int, int>
-#define pll pair<ll, ll>
 
-const int INF = 1e9 + 7;
-const int MOD = 1e9 + 7;
-const int dy[] = { 0, 0, 1, -1 };
-const int dx[] = { 1, -1, 0, 0 };
-
-const int MAXN = 2e6;  // limit for array size
-
-struct Seg {
-	int flag;  // array size
-	vector<ll> t;
-
-	void build(int N) {
-		for (flag = 1; flag < N; flag <<= 1);
-		t.resize(2 * flag);
+struct Fenwick { // 0-indexed
+	int flag, cnt; // array size
+	vector<ll> arr, t;
+	void build(int n) {
+		for (flag = 1; flag < n; flag <<= 1, cnt++);
+		arr.resize(flag);
+		t.resize(flag);
 	}
-	void modify(int p, int val) {  // set value at position p
-		for (t[p += flag - 1] += val; p > 1; p >>= 1) t[p >> 1] = t[p] + t[p ^ 1];
-	}
-	ll query(int val) {
-		return query(1, 1, flag, val);
-	}
-	ll query(int n, int nl, int nr, int val) {  // sum on interval [l, r]
-		if (nl == nr) {
-		    modify(nl, -1);
-		    return nl;
+	void add(int p, ll value) { // add value at position p
+		arr[p] += value;
+		while (p < flag) {
+			t[p] += value;
+			p |= p + 1;
 		}
-
-		int mid = (nl + nr) / 2;
-		if (t[n << 1] >= val) return query(n << 1, nl, mid, val);
-		else return query(n << 1 | 1, mid + 1, nr, val - t[n << 1]);
 	}
-}seg;
-
-void init() {
-    seg.build((int)2e6);
-}
-
-void procedure() {
-    int n; cin >> n;
-	
-	while (n--) {
-	    int t, x;
-	    cin >> t >> x;
-	    
-	    if (t == 1) seg.modify(x, 1);
-	    if (t == 2) cout << seg.query(x) << '\n';
+	void modify(int p, ll value) { // set value at position p
+		add(p, value - arr[p]);
+	};
+	ll query(int x) {
+		ll ret = 0;
+		while (x >= 0) ret += t[x], x = (x & (x + 1)) - 1;
+		return ret;
 	}
-}
+	ll query(int l, int r) {
+		return query(r) - (l ? query(l - 1) : 0);
+	}
+	int kth(int k) { // find the kth smallest number (1-indexed)
+		assert(t.back() >= k);
+		int l = 0, r = arr.size();
+		for (int i = 0; i <= cnt; i++) {
+			int mid = (l + r) >> 1;
+			ll val = mid ? t[mid - 1] : t.back();
+			if (val >= k) r = mid;
+			else l = mid, k -= val;
+		}
+		return l;
+	}
+}fw;
 
 int main() {
+	#ifndef ONLINE_JUDGE
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
+	#endif
+
 	cin.tie(NULL); cout.tie(NULL);
 	ios_base::sync_with_stdio(false);
+
+	int n; cin >> n;
 	
-	init();
-	
-	procedure();
-	
-	return 0;
+	fw.build(2020202);
+	for (int i = 0; i < n; i++) {
+		int op, x;
+		cin >> op >> x;
+		
+		if (op == 1) fw.add(x, 1);
+		if (op == 2) {
+			int res = fw.kth(x);
+			cout << res << '\n';
+			fw.add(res, -1);
+		}
+	}
 }
