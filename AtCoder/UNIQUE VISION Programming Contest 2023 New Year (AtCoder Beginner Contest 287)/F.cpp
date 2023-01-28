95afe65d00d8712a2f5390a138fc1ae363e42d07
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-01-29 01:08:07 +0900

    abc

--- AtCoder/UNIQUE VISION Programming Contest 2023 New Year (AtCoder Beginner Contest 287)/F.cpp
+++ AtCoder/UNIQUE VISION Programming Contest 2023 New Year (AtCoder Beginner Contest 287)/F.cpp
@@ -1,57 +1,46 @@
-//#pragma GCC optimize("O3")
-//#pragma GCC optimize("Ofast")
-//#pragma GCC optimize("unroll-loops")
-
 #include <bits/stdc++.h>
-#include <cassert>
 using namespace std;
 #define ll long long
-#define ull unsigned long long
-#define ld long double
-#define pii pair<int, int>
-#define pll pair<ll, ll>
-#define fr first
-#define sc second
-#define all(c) (c).begin(), (c).end()
 #define sz(x) (int)(x).size()
 
-const double EPS = 1e-9;
-const int INF = 1e9 + 7;
 const int MOD = 998244353;
-const int dy[] = { 0, 0, 1, -1, 1, 1, -1, -1 };
-const int dx[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
-int n;
+int n, vi[5050];
 vector<int> adj[5050];
+vector<vector<ll>> dp[5050];
 
-void input() {
-	cin >> n;
-	for (int i = 0; i < n - 1; i++) {
-		int u, v;
-		cin >> u >> v;
-		adj[u].push_back(v);
-		adj[v].push_back(u);
-	}
-}
-
-int vi[5050], dep[5050], isLeaf[5050], par[5050];
-vector<int> bydep[5050], t[5050];
-
-void dfs(int v, int d) {
-	if (v != 1 && sz(adj[v]) == 1) isLeaf[v] = 1;
+void dfs(int v) {
 	vi[v] = 1;
-	dep[v] = d;
-	bydep[d].push_back(v);
+	auto& ret = dp[v];
+	ret.resize(2, vector<ll>(2));
+	
+	ret[0][0] = 1;
+	ret[1][1] = 1;
+	
 	for (auto& i : adj[v]) {
-		if (!vi[i]) {
-			dfs(i, d + 1);
-			par[i] = v;
-			t[v].push_back(i);
+		if (vi[i]) continue;
+		
+		dfs(i);
+
+		auto& arr = dp[i];
+
+		vector<vector<ll>> tmp(sz(ret) + sz(arr) - 1, vector<ll>(2));
+
+		for (int j = 0; j < sz(ret); j++) {
+			for (int k = 0; k < sz(arr); k++) {
+				tmp[j + k][0] += ret[j][0] * (arr[k][0] + arr[k][1]);
+				tmp[j + k][0] %= MOD;
+				tmp[j + k][1] += ret[j][1] * arr[k][0];
+				tmp[j + k][1] %= MOD;
+				if (j + k == 0) continue;
+				tmp[j + k - 1][1] += ret[j][1] * arr[k][1];
+				tmp[j + k - 1][1] %= MOD;
+			}
 		}
+
+		ret = tmp;
 	}
 }
-
-ll dp[5050][5050];
 
 int main() {
 	#ifndef ONLINE_JUDGE
@@ -62,33 +51,17 @@ int main() {
 	cin.tie(NULL); cout.tie(NULL);
 	ios_base::sync_with_stdio(false);
 
-	input();
-
-	if (n == 1) {
-		cout << 1;
-		return 0;
+	cin >> n;
+	for (int i = 0; i < n - 1; i++) {
+		int u, v;
+		cin >> u >> v;
+		adj[u].push_back(v);
+		adj[v].push_back(u);
 	}
 
-	par[1] = 0;
-	dfs(1, 1);
-
-	ll ans = 0;
-	for (int d = n; d >= 1; d--) {
-		for (auto& v : bydep[d]) {
-			if (isLeaf[v]) dp[v][1] = 1;
-			else {
-				ll res = 1;
-				for (auto& u : t[v]) {
-					res = res * (1 + dp[u][1]) % MOD;
-				}
-				dp[v][1] = (dp[v][1] + res) % MOD;
-			}
-			cout << v << ' ' << dp[v][1] << '\n';
-			ans = (ans + dp[v][1]) % MOD;
-		}
+	dfs(1);
+	
+	for (int i = 1; i <= n; i++) {
+		cout << (dp[1][i][0] + dp[1][i][1]) % MOD << '\n';
 	}
-
-	cout << ans;
-
-	return 0;
 }
