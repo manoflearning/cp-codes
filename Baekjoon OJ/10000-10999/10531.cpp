b4e577a5badf22c560736e76bae9cb99e3098c68
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-02-10 11:43:42 +0900

    boj

--- Baekjoon OJ/10000-10999/10531.cpp
+++ Baekjoon OJ/10000-10999/10531.cpp
@@ -1,5 +1,101 @@
+// solution 1: fft
 //#pragma GCC optimize("O3")
-#pragma GCC optimize("Ofast")
+//#pragma GCC optimize("Ofast")
+//#pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+#include <cassert>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define fr first
+#define sc second
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+
+const double EPS = 1e-9;
+const int INF = 1e9 + 7;
+const int MOD = 1e9 + 7;
+const int dy[] = { 0, 0, 1, -1, 1, 1, -1, -1 };
+const int dx[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+typedef complex<double> base;
+void fft(vector<base> &a, bool inv) {
+	int n = a.size(), j = 0;
+	vector<base> roots(n / 2);
+	for (int i = 1; i < n; i++){
+		int bit = (n >> 1);
+		while (j >= bit) {
+			j -= bit;
+			bit >>= 1;
+		}
+		j += bit;
+		if (i < j) swap(a[i], a[j]);
+	}
+	double ang = 2 * acos(-1) / n * (inv ? -1 : 1);
+	for (int i = 0; i < n / 2; i++){
+		roots[i] = base(cos(ang * i), sin(ang * i));
+	}
+	for (int i = 2; i <= n; i <<= 1){
+		int step = n / i;
+		for (int j = 0; j < n; j += i){
+			for (int k = 0; k < i / 2; k++) {
+				base u = a[j + k], v = a[j + k + i / 2] * roots[step * k];
+				a[j + k] = u + v;
+				a[j + k + i / 2] = u - v;
+			}
+		}
+	}
+	if (inv) for (int i = 0; i < n; i++) a[i] /= n;
+}
+void multiply(const vector<ll> &v, const vector<ll> &w, vector<ll>& res) {
+	vector<base> fv(v.begin(), v.end()), fw(w.begin(), w.end());
+	int n = 2; while (n < v.size() + w.size()) n <<= 1;
+	fv.resize(n); fw.resize(n);
+	fft(fv, 0); fft(fw, 0);
+	for (int i = 0; i < n; i++) fv[i] *= fw[i];
+	fft(fv, 1);
+	res.resize(n);
+	for (int i = 0; i < n; i++) res[i] = (ll)round(fv[i].real());
+}
+
+int main() {
+	#ifndef ONLINE_JUDGE
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
+	#endif
+
+	cin.tie(NULL); cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int n; cin >> n;
+    vector<ll> a(202020);
+    for (int i = 0; i < n; i++) {
+        int x; cin >> x;
+        a[x] = 1;
+    }
+
+    vector<ll> res;
+    multiply(a, a, res);
+
+    int q; cin >> q;
+    int ans = 0;
+    while (q--) {
+        int x; cin >> x;
+        if (a[x] || res[x]) ans++;
+    }
+
+    cout << ans;
+
+	return 0;
+}
+
+// solution 2: naive
+//#pragma GCC optimize("O3")
+/*#pragma GCC optimize("Ofast")
 #pragma GCC optimize("unroll-loops")
 
 #include <bits/stdc++.h>
@@ -69,4 +165,4 @@ int main() {
     cout << ans;
 
 	return 0;
-}
+}*/
