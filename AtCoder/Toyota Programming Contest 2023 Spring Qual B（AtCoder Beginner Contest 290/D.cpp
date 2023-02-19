d5d948ae2b3c06648c49f389692053c737b688cb
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-02-19 22:41:02 +0900

    atcoder

---
+++ AtCoder/Toyota Programming Contest 2023 Spring Qual B（AtCoder Beginner Contest 290/D.cpp
@@ -0,0 +1,61 @@
+//#pragma GCC optimize("O3")
+//#pragma GCC optimize("Ofast")
+//#pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+#include <cassert>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define fr first
+#define sc second
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+
+const double EPS = 1e-9;
+const int INF = 1e9 + 7;
+const int MOD = 1e9 + 7;
+const int dy[] = { 0, 0, 1, -1, 1, 1, -1, -1 };
+const int dx[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+ll gcd(ll a, ll b) {
+	if (b == 0) return a;
+	else return gcd(b, a % b);
+}
+ll lcm(ll a, ll b) {
+	return a * b / gcd(a, b);
+}
+
+int main() {
+	#ifndef ONLINE_JUDGE
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
+	#endif
+
+	cin.tie(NULL); cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int tc; cin >> tc;
+	for (int tt = 1; tt <= tc; tt++) {
+		ll n, d, k;
+        cin >> n >> d >> k;
+        
+        d %= n;
+        if (!d) d = 1;
+
+        ll x = lcm(n, d) / d;
+        if (k > x)
+
+        ll res = 0, res2 = 0;
+        while (k > (n - 1 - res) / d + 1) {
+            k -= (n - 1 - res) / d + 1;
+            res = ((n - 1 - res) / d * d + res + d) % n;
+        }
+        cout << (k - 1) * d + res << '\n';
+	}
+
+	return 0;
+}
