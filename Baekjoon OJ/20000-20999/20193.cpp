8ed8c35159bd3f4193a0242a61a77e5fe31836f1
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-02-21 23:12:55 +0900

    boj

--- Baekjoon OJ/20000-20999/20193.cpp
+++ Baekjoon OJ/20000-20999/20193.cpp
@@ -1,100 +1,112 @@
-// 관찰 1
-// 변의 길이를 최소로 하는 화려한 정사각형에는, 2개 이상의 변 위에 점이 놓여 있다.
-
-// 관찰 2
-// 변의 길이를 최소로 하는 화려한 정사각형 중 적어도 하나 이상에는, 3개 이상의 변 위에 점이 놓여 있다.
-// 2개의 변 위에만 점이 놓여 있는 화려한 정사각형도 존재할 수 있지만, 
-// 이 경우 화려한 정사각형을 적당히 평행이동하여 3개 이상의 변 위에 점이 놓여 있도록 만들 수 있다.
-
-
-
-// subtask 2
-/*#include <bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #define ll long long
+#define fr first
+#define sc second
 
-const int INF = 1e9 + 7;
+int flag;
+struct Seg {
+    vector<int> t, lazy;
+    void build(int N) {
+        for (flag = 1; flag < N; flag <<= 1);
+        t.resize(flag << 1);
+        lazy.resize(flag << 1);
+    }
+    void init() {
+        t.clear();
+        lazy.clear();
+        t.resize(flag << 1);
+        lazy.resize(flag << 1);
+    }
+    void propagate(int n) {
+        if (!lazy[n]) return;
 
-int n, k;
-unordered_set<int> s[55];
-int dp[55][2525][2525];
-
-int f(int co, int x, int y) {
-    int& ret = dp[co][x][y];
-    if (ret != -1) return ret;
-    if (s[co].count(x * 2525 + y)) return ret = 0;
-
-    ret = INF;
-    if (x < 2500) ret = min(ret, f(co, x + 1, y) + 1);
-    if (y < 2500) ret = min(ret, f(co, x, y + 1) + 1);
-    if (x < 2500 && y < 2500) ret = min(ret, f(co, x + 1, y + 1) + 1);
-
-    return ret;
-}
-
-int main() {
-    #ifndef ONLINE_JUDGE
-    freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
-    freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
-    #endif
-
-    cin.tie(NULL); cout.tie(NULL);
-    ios_base::sync_with_stdio(false);
-
-    // initialize
-    for (int i = 0; i < 55; i++) {
-        for (int j = 0; j < 2525; j++) {
-            for (int k = 0; k < 2525; k++) {
-                dp[i][j][k] = -1;
-            }
+        if (n < flag) {
+            lazy[n << 1] += lazy[n];
+            lazy[n << 1 | 1] += lazy[n];
         }
+        t[n] += lazy[n];
+        lazy[n] = 0;
     }
-    
-    // input
-    cin >> n >> k;
-    for (int i = 0; i < n; i++) {
-        int x, y, co;
-        cin >> x >> y >> co;
-        s[co].insert(x * 2525 + y);
-    }
-
-    // solve
-    int ans = INF;
-    for (int sx = 2500; sx >= 1; sx--) {
-        for (int sy = 2500; sy >= 1; sy--) {
-            int res = 0;
-            for (int co = 1; co <= k; co++) {
-                res = max(res, f(co, sx, sy));
-            }
-            ans = min(ans, res);
+    void modify(int l, int r, int val, int n = 1, int nl = 1, int nr = flag) {
+        propagate(n);
+        if (r < nl || nr < l) return;
+        if (l <= nl && nr <= r) {
+            lazy[n] += val;
+            propagate(n);
+            return;
         }
+
+        int mid = (nl + nr) >> 1;
+        modify(l, r, val, n << 1, nl, mid);
+        modify(l, r, val, n << 1 | 1, mid + 1, nr);
+        t[n] = max(t[n << 1], t[n << 1 | 1]);
     }
-
-    cout << ans;
-}*/
-
-// subtask 1
-// 모든 정사각형에 대해 완전탐색
-/*#include <bits/stdc++.h>
-using namespace std;
-#define ll long long
+    int query() {
+        propagate(1);
+        return t[1];
+    }
+}seg;
 
 struct Point {
-    ll x, y, co;
+    ll y; int c;
 };
 
 int n, k;
-vector<Point> a;
+vector<Point> p[252525];
+multiset<int> s[252525];
 
-ll cntColor(ll sx, ll sy, ll ex, ll ey) {
-    int ret = 0;
-    vector<int> vi(k + 1);
-    for (auto& p : a) {
-        if (p.x < sx || ex < p.x) continue;
-        if (p.y < sy || ey < p.y) continue;
-        if (!vi[p.co]) ret++, vi[p.co] = 1;
+void add(int y, int c, int len) {
+    int l = y, r = y + len;
+
+    auto it = s[c].lower_bound(y);
+    if (it != s[c].end()) r = min(r, *it - 1);
+    if (it != s[c].begin()) {
+        it--;
+        l = max(l, *it + len + 1);
     }
-    return ret;
+
+    seg.modify(l, r, 1);
+    s[c].insert(y);
+}
+void del(int y, int c, int len) {
+    int l = y, r = y + len;
+
+    s[c].erase(s[c].find(y));
+
+    auto it = s[c].lower_bound(y);
+    if (it != s[c].end()) r = min(r, *it - 1);
+    if (it != s[c].begin()) {
+        it--;
+        l = max(l, *it + len + 1);
+    }
+
+    seg.modify(l, r, -1);
+}
+
+bool possible(int len) {
+    // initialize
+    seg.init();
+    for (int i = 0; i < 252525; i++) s[i].clear();
+
+    // solve
+    for (int x = 0; x <= len; x++) {
+        for (auto& i : p[x]) 
+            add(i.y, i.c, len);
+    }
+
+    if (seg.query() == k) return 1;
+
+    for (int x = len + 1; x <= 250000; x++) {
+        for (auto& i : p[x])
+            add(i.y, i.c, len);
+        for (auto& i : p[x - len - 1])
+            del(i.y, i.c, len);
+
+        if (seg.query() == k) return 1;
+    }
+
+    return 0;
 }
 
 int main() {
@@ -107,19 +119,19 @@ int main() {
     ios_base::sync_with_stdio(false);
 
     cin >> n >> k;
-    a.resize(n);
-    for (auto& i : a)
-        cin >> i.x >> i.y >> i.co;
-
-    for (int len = 0; len <= 50; len++) {
-        for (int sx = 1; sx + len <= 50; sx++) {
-            for (int sy = 1; sy + len <= 50; sy++) {
-                int ex = sx + len, ey = sy + len;
-                if (cntColor(sx, sy, ex, ey) == k) {
-                    cout << len;
-                    return 0;
-                }
-            }
-        }
+    for (int i = 0; i < n; i++) {
+        int x, y, c;
+        cin >> x >> y >> c;
+        p[x].push_back({ y, c });
     }
-}*/
+
+    seg.build(2 * 250000);
+
+    int l = 0, r = 250000;
+    while (l < r) {
+        int mid = (l + r) >> 1;
+        if (possible(mid)) r = mid;
+        else l = mid + 1;
+    }
+    cout << l;
+}
