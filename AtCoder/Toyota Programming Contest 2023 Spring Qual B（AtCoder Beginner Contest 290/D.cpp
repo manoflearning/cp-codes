68958f3a32cc2565169af3c73480b308afd51a93
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-02-22 16:25:39 +0900

    atcoder

--- AtCoder/Toyota Programming Contest 2023 Spring Qual B（AtCoder Beginner Contest 290/D.cpp
+++ AtCoder/Toyota Programming Contest 2023 Spring Qual B（AtCoder Beginner Contest 290/D.cpp
@@ -1,25 +1,6 @@
-//#pragma GCC optimize("O3")
-//#pragma GCC optimize("Ofast")
-//#pragma GCC optimize("unroll-loops")
-
 #include <bits/stdc++.h>
-#include <cassert>
 using namespace std;
 #define ll long long
-#define ull unsigned long long
-#define ld long double
-#define pii pair<int, int>
-#define pll pair<ll, ll>
-#define fr first
-#define sc second
-#define all(c) (c).begin(), (c).end()
-#define sz(x) (int)(x).size()
-
-const double EPS = 1e-9;
-const int INF = 1e9 + 7;
-const int MOD = 1e9 + 7;
-const int dy[] = { 0, 0, 1, -1, 1, 1, -1, -1 };
-const int dx[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
 ll gcd(ll a, ll b) {
 	if (b == 0) return a;
@@ -41,20 +22,17 @@ int main() {
 	int tc; cin >> tc;
 	for (int tt = 1; tt <= tc; tt++) {
 		ll n, d, k;
-        cin >> n >> d >> k;
-        
-        d %= n;
-        if (!d) d = 1;
+		cin >> n >> d >> k;
 
-        ll x = lcm(n, d) / d;
-        if (k > x)
+		if (d >= n) d %= n;
+		if (d == 0) {
+			cout << k - 1 << '\n';
+			continue;
+		}
 
-        ll res = 0, res2 = 0;
-        while (k > (n - 1 - res) / d + 1) {
-            k -= (n - 1 - res) / d + 1;
-            res = ((n - 1 - res) / d * d + res + d) % n;
-        }
-        cout << (k - 1) * d + res << '\n';
+		ll cnt = lcm(n, d) / d;
+		
+		cout << (k - 1) % cnt * d % n + (k - 1) / cnt << '\n';
 	}
 
 	return 0;
