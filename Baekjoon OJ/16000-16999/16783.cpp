f0d36220742ac9acc8fb4dbb63ea3ac3f88b3395
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-02-26 14:01:21 +0900

    boj

--- Baekjoon OJ/16000-16999/16783.cpp
+++ Baekjoon OJ/16000-16999/16783.cpp
@@ -2,16 +2,18 @@
 using namespace std;
 #define ll long long
 
+const int INF = 1e9 + 7;
+
 int flag;
 struct Seg {
     struct Node {
-        int mx, l, r, len;
+        ll l, r, mx, all;
         Node operator+(const Node& rhs) const {
-            Node ret = { 0, 0, 0 };
-            ret.mx = max({ r + rhs.l, mx, rhs.mx });
-            ret.l = (mx == len ? mx + rhs.l : l);
-            ret.r = (rhs.mx == rhs.len ? rhs.mx + r : rhs.r);
-            ret.len = len + rhs.len;
+            Node ret;
+            ret.l = max(l, all + rhs.l);
+            ret.r = max(rhs.r, r + rhs.all);
+            ret.mx = max({ mx, rhs.mx, r + rhs.l });
+            ret.all = all + rhs.all;
             return ret;
         }
     };
@@ -19,41 +21,46 @@ struct Seg {
     void build(int n) {
         for (flag = 1; flag < n; flag <<= 1);
         t.resize(flag << 1);
-        for (int i = flag; i < flag + n; i++) t[i].len = 1;
-        for (int i = flag - 1; i >= 1; i--)
-            t[i].len = t[i << 1].len + t[i << 1 | 1].len;
+        for (int i = flag; i < flag + n; i++) {
+            t[i].l = t[i].r = t[i].mx = 0;
+            t[i].all -INF;
+        }
+        for (int i = flag - 1; i >= 1; i--) {
+            t[i] = t[i << 1] + t[i << 1 | 1];
+        }
     }
-    void modify(int p, int val) {
+    void modify(int p, ll val) {
         p += flag - 1;
-        t[p].l = t[p].r = t[p].mx = val;
+        t[p].l = t[p].r = t[p].mx = max(0ll, val);
+        t[p].all = val;
         for (; p > 1; p >>= 1) {
             if (p & 1) t[p >> 1] = t[p ^ 1] + t[p];
             else t[p >> 1] = t[p] + t[p ^ 1];
         }
     }
-    int query() { return t[1].mx; }
+    ll query() { return t[1].mx; }
 }seg;
 
 struct Point {
-    ll x, y; int co;
+    ll x, y, w;
     bool operator<(const Point& rhs) const {
         return tie(x, y) < tie(rhs.x, rhs.y);
     }
 };
 
 struct Line {
-    int i, j; ll dx, dy; // i < j, dx >= 0
+    int u, v; ll dx, dy; // u < v, dx >= 0;
     bool operator<(const Line& rhs) const {
         if (dy * rhs.dx != rhs.dy * dx) return dy * rhs.dx < rhs.dy * dx;
-        return tie(i, j) < tie(rhs.i, rhs.j);
+        return tie(u, v) < tie(rhs.u, rhs.v);
     }
     bool operator==(const Line& rhs) const {
-        return (dy * rhs.dx == rhs.dy * dx);
+        return dy * rhs.dx == rhs.dy * dx;
     }
 };
 
-int n, pos[1010];
-Point p[1010];
+int n, pos[2020];
+Point p[2020];
 
 int main() {
     #ifndef ONLINE_JUDGE
@@ -65,18 +72,14 @@ int main() {
     ios_base::sync_with_stdio(false);
 
     cin >> n;
-    for (int i = 1; i <= n; i++) {
-        cin >> p[i].x >> p[i].y;
-        char c; cin >> c;
-        p[i].co = (c == 'R');
-    }
-
-    seg.build(n);
+    for (int i = 1; i <= n; i++)
+        cin >> p[i].x >> p[i].y >> p[i].w;
 
     sort(p + 1, p + 1 + n);
+    seg.build(n);
     for (int i = 1; i <= n; i++) {
         pos[i] = i;
-        seg.modify(i, p[i].co);
+        seg.modify(i, p[i].w);
     }
 
     vector<Line> arr;
@@ -87,15 +90,15 @@ int main() {
     }
     sort(arr.begin(), arr.end());
 
-    int ans = seg.query();
+    ll ans = seg.query();
     for (int i = 0, j = 0; i < arr.size(); i = j) {
-        while (j < arr.size() && arr[i] == arr[j]) j++; // all lines in [i, j) are same
+        while (j < arr.size() && arr[j] == arr[i]) j++; // all lines in [i, j) are same
         for (int k = i; k < j; k++) {
-            int u = arr[k].i, v = arr[k].j;
-            seg.modify(pos[u], p[pos[v]].co);
-            seg.modify(pos[v], p[pos[u]].co);
+            int u = arr[k].u, v = arr[k].v;
             swap(p[pos[u]], p[pos[v]]);
             swap(pos[u], pos[v]);
+            seg.modify(pos[u], p[pos[u]].w);
+            seg.modify(pos[v], p[pos[v]].w);
         }
         ans = max(ans, seg.query());
     }
