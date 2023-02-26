a28364f1b38111b727de3ed57723b728eca8d5b7
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-02-26 15:53:02 +0900

    boj

--- Baekjoon OJ/11000-11999/11266.cpp
+++ Baekjoon OJ/11000-11999/11266.cpp
@@ -1,74 +1,99 @@
-#include <iostream>
-#include <vector>
-#include <cstring>
-#include <algorithm>
-#include <map>
+// INPUT: Given a undirected graph.
+// OUTPUT: Decompose this graph into BCCs.
+// A Biconnected Component (BCC) is a subset of vertices in an undirected graph that satisfies the following conditions:
+// (1) If you delete any vertex from a subset, the remaining vertices are connected to each other.
+// (2) Adding other vertices to this subset does not satisfy (1). (This is the largest set that satisfies (1))
+// TIME COMPLEXITY: O(V + E)
+
+// A vertex at which the graph is divided into two or more components when the vertex is removed is called a 'articulation point'.
+// After decomposing the graph into BCCs, vertices belonging to two or more BCCs are articulation point.
+
+// A edge at which the graph is divided into two or more components when the edge is removed is called a 'articulation cut'.
+// For all tree edges on a dfs spanning tree, if tmp > dfsn[now], the edge { now, next } is a articulation cut.
+
+#include <bits/stdc++.h>
 using namespace std;
+#define pii pair<int, int>
 
-const int MAXV = 1e4;
+const int MAXV = 101010;
 
-int dfsn[MAXV + 5], dCnt;
-vector<int> adj[MAXV + 5];
-map<int, int> aPoint;
+int n, m, dfsn[MAXV], dCnt;
+vector<int> adj[MAXV];
+stack<pii> stk;
+vector<vector<pii>> bcc;
+set<int> aPoint;
+set<pii> aCut;
 
-int dfs(int v, int prv = -1);
+void input() {
+	cin >> n >> m;
+	for (int i = 0; i < m; i++) {
+		int u, v;
+		cin >> u >> v;
+		adj[u].push_back(v);
+		adj[v].push_back(u);
+	} 
+}
+
+int dfs(int now, int prv) {
+	int ret = dfsn[now] = ++dCnt;
+
+	int childCnt = 0;
+	for (int next : adj[now]) {
+		if (next == prv) continue;
+
+		// If an edge { now, next } has not yet been visited, puts an edge on the stack.
+		if (dfsn[now] > dfsn[next]) stk.push({ now, next });
+		
+		// Back edge
+		if (dfsn[next] != -1) ret = min(ret, dfsn[next]);
+		// Tree edge
+		else {
+			childCnt++;
+			int tmp = dfs(next, now);
+			ret = min(ret, tmp);
+
+			if (prv != -1 && tmp >= dfsn[now]) 
+				aPoint.insert(now);
+
+			// If next cannot go to ancestor node of now, find BCC
+			if (tmp >= dfsn[now]) {
+				vector<pii> nowBCC;
+				while (true) {
+					pii t = stk.top();
+					stk.pop();
+					nowBCC.push_back(t);
+					if (t == make_pair(now, next)) break;
+				}
+				bcc.push_back(nowBCC);
+			}
+		}
+	}
+
+	if (prv == -1 && childCnt > 1)
+		aPoint.insert(now);
+	
+	return ret;
+}
+
+void getBCC() {
+	memset(dfsn, -1, sizeof(dfsn));
+	for (int v = 1; v <= n; v++)
+		if (dfsn[v] == -1) dfs(v, -1);
+}
 
 int main() {
+	#ifndef ONLINE_JUDGE
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
+	#endif
+
 	cin.tie(NULL); cout.tie(NULL);
 	ios_base::sync_with_stdio(false);
 
-	memset(dfsn, -1, sizeof(dfsn));
-
-	int N, M;
-	cin >> N >> M;
-
-	for (int i = 0; i < M; i++) {
-		int a, b; cin >> a >> b;
-		adj[a].push_back(b);
-		adj[b].push_back(a);
-	}
-
-	//dfs 트리
-	for (int v = 1; v <= N; v++)
-		if (dfsn[v] == -1) dfs(v);
-
-	//출력
-	cout << aPoint.size() << '\n';
-
-	for (map<int, int>::iterator v = aPoint.begin(); v != aPoint.end(); v++) 
-		cout << v->second << ' ';
-
-	return 0;
-}
-
-int dfs(int v, int prv) {
-	int ret = dfsn[v] = ++dCnt;
+	input();
+	getBCC();
 	
-	int child = 0;
-	for (int next : adj[v]) {
-		if (next == prv) continue;
-
-		//트리 간선
-		if (dfsn[next] == -1) {
-			child++;
-			int nRet = dfs(next, v);
-
-			if (prv == -1 && child >= 2)
-				aPoint.insert({ v, v });
-			else if (prv != -1 && nRet >= dfsn[v])
-				aPoint.insert({ v, v });
-			ret = min(ret, nRet);
-		}
-		//역방향 간선
-		else ret = min(ret, dfsn[next]);
-	}
-
-	return ret;
+	cout << aPoint.size() << '\n';
+	for (auto& i : aPoint)
+		cout << i << ' ';
 }
-/*////////////////////////////////////////////////////////////////////
-문제 해법		: 이중 연결 요소, 단절점
-결정적 깨달음		: 
-시간복잡도		: O(|V| + |E|)
-오답 원인		: 1. 
-				  2. 
-*/////////////////////////////////////////////////////////////////////
