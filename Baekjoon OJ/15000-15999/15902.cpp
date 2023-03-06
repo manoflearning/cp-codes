e31a971db77125f143c75dcab0f51cfdcbabe29e
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-03-06 17:23:14 +0900

    boj

--- Baekjoon OJ/15000-15999/15902.cpp
+++ Baekjoon OJ/15000-15999/15902.cpp
@@ -70,6 +70,18 @@ void splitBlock() {
     }
 }
 
+ll powxy(ll x, ll y) { 
+    if (y == 0) return 1;
+    if (y == 1) return x;
+    ll res = powxy(x, y / 2);
+    return res * res % MOD * (y & 1 ? x : 1) % MOD;
+}
+
+ll fac[3030], inv[3030], facInv[3030];
+ll binom(int n, int r) {
+	return fac[n] * facInv[r] % MOD * facInv[n - r] % MOD;
+}
+
 int main() {
     #ifndef ONLINE_JUDGE
     freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
@@ -83,5 +95,33 @@ int main() {
 
     splitBlock();
 
+    // Preprocessing in O(N)
+	fac[0] = fac[1] = inv[1] = 1;
+	facInv[0] = facInv[1] = 1;
+	for (int i = 2; i < 3030; i++) {
+		fac[i] = i * fac[i - 1] % MOD;
+		inv[i] = -(MOD / i) * inv[MOD % i] % MOD;
+		if (inv[i] < 0) inv[i] += MOD;
+		facInv[i] = facInv[i - 1] * inv[i] % MOD;
+	}
+
+    dp[1] = dp[2] = 1;
+    for (int i = 3; i < 3030; i++) {
+        for (int j = 1; j < i; j += 2) {
+            ll res = dp[j] * dp[i - j] % MOD;
+            res = res * binom(i - 1 - 1, i - j - 1);
+            dp[i] = (dp[i] + res) % MOD;
+        }
+    }
+
+    ll ans = 1;
+    for (auto& i : arr) ans = ans * dp[i] % MOD;
+
+    ll sum = 0;
+    for (auto& i : arr) sum += i - 1;
     
+    ans = ans * fac[sum] % MOD;
+    for (auto& i : arr)
+        ans = ans * powxy(fac[i - 1], MOD - 2) % MOD;
+    cout << sum << ' ' << ans;
 }
