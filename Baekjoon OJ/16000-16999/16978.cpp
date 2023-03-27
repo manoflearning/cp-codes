504fcf9387761f3e55609125a8490c88cdc607e3
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-03-27 17:24:16 +0900

    boj

--- Baekjoon OJ/16000-16999/16978.cpp
+++ Baekjoon OJ/16000-16999/16978.cpp
@@ -3,35 +3,41 @@ using namespace std;
 #define ll long long
 #define sz(x) (int)(x).size()
 
-struct PST {
-    struct Node { ll l, r, val; };
-    vector<Node> t;
+vector<ll> a;
 
-    void addNode() {
-        t.push_back({ -1, -1, 0 });
-    }
-    void build(ll l, ll r, ll n)  {
-        if (l == r) return;
+struct PST { // 1-indexed
+    int flag; // array size
+    struct Node { int l, r; ll val; };
+    vector<Node> t;
+    vector<int> root;
+
+    void addNode() { t.push_back({ -1, -1, 0 }); }
+    void build(int l, int r, int n)  {
+        assert(0 <= n && n < sz(t));
+        if (l == r) { t[n].val = a[l]; return; }
         addNode();
         t[n].l = sz(t) - 1;
         addNode();
         t[n].r = sz(t) - 1;
 
-        ll mid = (l + r) >> 1;
+        int mid = (l + r) >> 1;
         build(l, mid, t[n].l);
         build(mid + 1, r, t[n].r);
+        t[n].val = t[t[n].l].val + t[t[n].r].val;
     }
-    void modify(ll p, ll val, ll l, ll r, ll n1, ll n2) {
-        if (p < l || r < p) {
-            t[n2].l = t[n1].l;
-            t[n2].r = t[n1].r;
-            t[n2].val = t[n1].val;
-            return;
-        }
+    void build(int Flag) {
+        addNode();
+        root.push_back(sz(t) - 1);
+        flag = Flag;
+        build(1, flag, root[0]);
+    }
+    void modify(int p, ll val, int l, int r, int n1, int n2) {
+        assert(0 <= n1 && n1 < sz(t));
+        assert(0 <= n2 && n2 < sz(t));
+        if (p < l || r < p) { t[n2] = t[n1]; return; }
+        if (l == r) { t[n2].val = val; return; }
 
-        t[n2].val = t[n1].val + val;
-        if (l == r) return;
-        ll mid = (l + r) >> 1;
+        int mid = (l + r) >> 1;
         if (p <= mid) {
             t[n2].r = t[n1].r;
             addNode();
@@ -44,17 +50,27 @@ struct PST {
             t[n2].r = sz(t) - 1;
             modify(p, val, mid + 1, r, t[n1].r, t[n2].r);
         }
+        t[n2].val = t[t[n2].l].val + t[t[n2].r].val;
     }
-    ll query(ll l, ll r, ll n, ll nl, ll nr) {
+    void modify(int p, ll val) {
+        addNode();
+        root.push_back(sz(t) - 1);
+        modify(p, val, 1, flag, root[sz(root) - 2], root[sz(root) - 1]);
+    }
+    ll query(int l, int r, int n, int nl, int nr) {
+        assert(0 <= n && n < sz(t));
         if (r < nl || nr < l) return 0;
         if (l <= nl && nr <= r) return t[n].val;
         int mid = (nl + nr) >> 1;
-        return query(l, r, t[n].l, nl, mid) + query(l, r, t[n].r, mid + 1, r);
+        return query(l, r, t[n].l, nl, mid) + query(l, r, t[n].r, mid + 1, nr);
+    }
+    ll query(int l, int r, int n) {
+        assert(n < sz(root));
+        return query(l, r, root[n], 1, flag);
     }
 }pst;
 
-int n, m;
-ll a[101010];
+int n;
 
 int main() {
     #ifndef ONLINE_JUDGE
@@ -66,13 +82,19 @@ int main() {
     ios_base::sync_with_stdio(false);
 
     cin >> n;
+    a.resize(n + 1);
     for (int i = 1; i <= n; i++) cin >> a[i];
 
-    cin >> m;
-    for (int i = 0; i < m; i++) {
+    pst.build(n);
+
+    int m; cin >> m;
+    while (m--) {
         int op, x, y, z;
         cin >> op >> x >> y;
-        if (op == 2) cin >> z;
-        
+        if (op == 1) pst.modify(x, y);
+        if (op == 2) {
+            cin >> z;
+            cout << pst.query(y, z, x) << '\n';
+        }
     }
 }
