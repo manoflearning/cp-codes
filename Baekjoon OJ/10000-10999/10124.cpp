df5a4ad7a78c672e43c2a221f53055430787a5f9
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-03-27 19:24:54 +0900

    Change README.md

--- Baekjoon OJ/10000-10999/10124.cpp
+++ Baekjoon OJ/10000-10999/10124.cpp
@@ -1,4 +1,73 @@
+// solution 1 : persistent segment tree
 #include <bits/stdc++.h>
+using namespace std;
+#define sz(x) (int)(x).size()
+
+struct PST { // 1-indexed
+    int flag; // array size
+    struct Node { int l, r, val; };
+    vector<Node> t;
+    vector<int> root;
+
+    void addNode() { t.push_back({ -1, -1, 0 }); };
+    void build(int l, int r, int n) {
+        if (l == r) return;
+        addNode();
+        t[n].l = sz(t) - 1;
+        addNode();
+        t[n].r = sz(t) - 1;
+
+        int mid = (l + r) >> 1;
+        build(l, mid, t[n].l);
+        build(mid + 1, r, t[n].r);
+    }
+    void build(int Flag) {
+        addNode();
+        root.push_back(sz(t) - 1);
+        flag = Flag;
+        build(1, flag, root[0]);
+    }
+    void modify(int p, int l, int r, int n1, int n2) {
+        if (p < l || r < p) { t[n2] = t[n1]; return; }
+        if (l == r) { t[n2].val = t[n1].val + 1; return; }
+
+        int mid = (l + r) >> 1;
+        if (p <= mid) {
+            t[n2].r = t[n1].r;
+            addNode();
+        }
+    }
+    void modify(int p) {
+        addNode();
+        root.push_back(sz(t) - 1);
+        modify(p, 1, flag, root[sz(root) - 2], root[sz(root) - 1]);
+    }
+}pst;
+
+int n, m;
+int a[505050];
+
+void input() {
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++) cin >> a[i];
+}
+
+int main() {
+    #ifndef ONLINE_JUDGE
+    freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
+    freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
+    #endif
+
+    cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    input();
+
+
+}
+
+// solution 2 : random
+/*#include <bits/stdc++.h>
 #include <random>
 using namespace std;
 #define all(x) (x).begin(), (x).end()
@@ -8,11 +77,6 @@ int a[505050];
 vector<int> arr[505050];
 
 int main() {
-    #ifndef ONLINE_JUDGE
-    freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
-    freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
-    #endif
-
     cin.tie(NULL); cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
@@ -38,4 +102,4 @@ int main() {
         }
         cout << ans << '\n';
     }
-}
+}*/
--- README.md
+++ README.md
@@ -41,6 +41,7 @@ My codes for solving algorithm problems.
 - Linear Sieve, Extended GCD, Fermat's Little Theorem, Euler's Phi Function, FFT
 - Convex Hull Trick, Knuth Opt, Monotone Queue Opt, DnC Opt
 - Sort by Angular, Bulldozer Trick
+- Persistent Segment Tree
 
 ### Have to learn in the future
 
@@ -56,7 +57,7 @@ My codes for solving algorithm problems.
 - Sparse Table O(1) RMQ
 - Region Trick (https://github.com/koosaga/iamcoder/blob/master/lectures/%EB%82%98%EC%BD%94%EB%8D%94%201%20Sqrt%20Decomposition.pdf query caching)
 - Aliens Trick
-- Persistent Segment Tree, Splay Tree, Segment Tree Beats
+- Splay Tree, Segment Tree Beats
 - SOS DP
 - Random (https://justicehui.github.io/hard-algorithm/2020/03/23/random/)
 - 볼록 다각형의 접선을 이용해 최대/최소를 구하는 유형 (https://github.com/justiceHui/Unknown-To-Wellknown/blob/master/README.md#%EB%B3%BC%EB%A1%9D-%EB%8B%A4%EA%B0%81%ED%98%95%EC%9D%98-%EC%A0%91%EC%84%A0%EC%9D%84-%EC%9D%B4%EC%9A%A9%ED%95%B4-%EC%B5%9C%EB%8C%80%EC%B5%9C%EC%86%8C%EB%A5%BC-%EA%B5%AC%ED%95%98%EB%8A%94-%EC%9C%A0%ED%98%95)
