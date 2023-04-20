fd2bca4178ce8b3e15cc12b3ebfc28779c9ff0b4
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-04-20 21:01:41 +0900

    boj

--- Baekjoon OJ/01000-01999/1979.cpp
+++ Baekjoon OJ/01000-01999/1979.cpp
@@ -1,8 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
-
-ll n, k, x;
 
 int main() {
     #ifndef ONLINE_JUDGE
@@ -13,26 +10,31 @@ int main() {
     cin.tie(NULL); cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
+    int n, k;
     cin >> n >> k;
+
+    if (n == 1) { cout << k; return 0; }
+
+    vector<int> x(1, k);
+    vector<int> y(2);
+    y[0] = x[0] * n % 10;
+    y[1] = x[0] * n / 10;
+
+    int isAns = 0;
+    for (int i = 1; i < 101010; i++) {
+        if (y[i - 1] == x[0] && y[i] == 0 && x.back() != 0) { isAns = 1; break; }
+        x.resize(i + 1);
+        y.resize(i + 2);
+
+        x[i] = y[i - 1];
+        y[i] += x[i] * n;
+        y[i + 1] += y[i] / 10;
+        y[i] %= 10;
+    }
     
-    if (k < n) {
-        cout << 0;
-        return 0;
+    if (isAns) {
+        reverse(x.begin(), x.end());
+        for (auto& i : x) cout << i;
     }
-
-    x = k;
-    for (ll i = 1; ; i *= 10) {
-        if (x * n / i == 0) {
-            cout << 0;
-            return 0;
-        }
-
-        ll z = x * n / i % 10;
-        if (z == k) break;
-        x += z * i * 10;
-    }
-
-    cout << x;
-
-    return 0;
+    else cout << 0;
 }
