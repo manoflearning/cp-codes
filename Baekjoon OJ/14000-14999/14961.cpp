f66ee85edee7d1a11e406d91608834bd7032dce0
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-05-01 09:35:14 +0900

    boj

--- Baekjoon OJ/14000-14999/14961.cpp
+++ Baekjoon OJ/14000-14999/14961.cpp
@@ -2,9 +2,10 @@
 using namespace std;
 
 int n;
-vector<int> a, ans;
+vector<int> a;
 int mnx = 0, mxx = 1, mny = 0, mxy = 0;
 int dir = 1;
+int nowx = 1, nowy = 0;
 
 int main() {
     #ifndef ONLINE_JUDGE
@@ -22,34 +23,30 @@ int main() {
     }
     a.pop_back();
 
-    ans.push_back(1);
-    int nowx = 1, nowy = 0;
+    cout << 1 << ' ';
     for (auto& i : a) {
         dir = (dir - i + 4) % 4;
         int diff = -1;
         if (dir == 0) {
             diff = mxy - nowy + 1;
             nowy += diff;
+            mxy = nowy;
         }
         if (dir == 1) {
             diff = mxx - nowx + 1;
             nowx += diff;
+            mxx = nowx;
         }
         if (dir == 2) {
             diff = nowy - mny + 1;
             nowy -= diff;
+            mny = nowy;
         }
         if (dir == 3) {
             diff = nowx - mnx + 1;
             nowx -= diff;
+            mnx = nowx;
         }
-        ans.push_back(diff);
-        mnx = min(mnx, nowx);
-        mxx = max(mxx, nowx);
-        mny = min(mny, nowy);
-        mxy = max(mxy, nowy);
+        cout << diff << ' ';
     }
-
-    for (auto& i : ans)
-        cout << i << ' ';
 }
