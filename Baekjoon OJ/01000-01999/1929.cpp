6cd0ee9394e68e87d201c8674d94a8672cee0230
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-05-01 10:23:09 +0900

    boj

--- Baekjoon OJ/01000-01999/1929.cpp
+++ Baekjoon OJ/01000-01999/1929.cpp
@@ -1,17 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define ll long long
+
+const int MAX = 1e6;
+bool isPrime[MAX + 1];
+vector<int> prime(1, 2);
+void getPrime() {
+	fill(isPrime + 2, isPrime + MAX + 1, 1);
+	for (ll i = 4; i <= MAX; i += 2)
+		isPrime[i] = 0;
+	for (ll i = 3; i <= MAX; i += 2) {
+		if (!isPrime[i]) continue;
+		prime.push_back(i);
+		for (ll j = i * i; j <= MAX; j += i * 2)
+			isPrime[j] = 0;
+	}
+}
 
 int main() {
+	#ifndef ONLINE_JUDGE
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
+	#endif
+	
 	cin.tie(NULL); cout.tie(NULL);
 	ios_base::sync_with_stdio(false);
 
+	getPrime();
+
 	int M, N;
 	cin >> M >> N;
-	for (int i = max(M, 2); i <= N; i++) {
-		int isPrime = 1;
-		for (int j = 2; j <= sqrt(i); j++) {
-			if (i % j == 0) { isPrime = 0; break; }
-		}
-		if (isPrime) cout << i << '\n';
-	}
+	for (auto& i : prime) 
+		if (M <= i && i <= N) cout << i << '\n';
 }
