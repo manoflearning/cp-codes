b0d84b65a56b58668a7adb25c59d33ce9b3e09d0
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-05-12 10:50:01 +0900

    boj

--- Baekjoon OJ/23000-23999/23248.cpp
+++ Baekjoon OJ/23000-23999/23248.cpp
@@ -1,69 +1,28 @@
 #include <bits/stdc++.h>
-#include <cassert>
 using namespace std;
-#define ll long long
-#define ull unsigned long long
-#define ld long double
-#define pii pair<int, int>
-#define pll pair<ll, ll>
-#define fr first
-#define sc second
-#define vt vector
-#define all(c) (c).begin(), (c).end()
-#define sz(x) (int)(x).size()
 
-#define EXPAND( x ) x // Use this if MS Visual Studio doesn't expand __VA_ARGS__ correctly
-#define F_OR(i, a, b, s) for (int i = (a); (s) > 0 ? i < (b) : i > (b); i += (s))
-#define F_OR1(e) F_OR(i, 0, e, 1)
-#define F_OR2(i, e) F_OR(i, 0, e, 1)
-#define F_OR3(i, b, e) F_OR(i, b, e, 1)
-#define F_OR4(i, b, e, s) F_OR(i, b, e, s)
-#define GET5(a, b, c, d, e, ...) e
-#define F_ORC(...) EXPAND( GET5(__VA_ARGS__, F_OR4, F_OR3, F_OR2, F_OR1) )
-#define FOR(...) EXPAND( F_ORC(__VA_ARGS__ )(__VA_ARGS__) )
-#define EACH(x, a) for (auto& x : a)
-
-const double EPS = 1e-9;
-const int INF = 1e9 + 7;
-const int MOD = 1e9 + 7;
-const int dy[] = { 0, 0, 1, -1, 1, 1, -1, -1 };
-const int dx[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
-
-int flag;  // array size
-struct Seg {  // 1-indexed
-	vector<ll> t;
-
+int flag;
+struct Seg {
+	vector<int> t;
 	void build(int n) {
 		for (flag = 1; flag < n; flag <<= 1);
-		t.resize(2 * flag);
-
-		//for (int i = flag; i < flag + n; i++) cin >> t[i];
-		//for (int i = flag - 1; i >= 1; i--) t[i] = t[i << 1] + t[i << 1 | 1];
+		t.resize(flag << 1);
 	}
-	void modify(int p, ll value) {  // set value at position p
-        t[p + flag - 1] = max(t[p + flag - 1], value);
-		for (p += flag - 1; p > 1; p >>= 1) t[p >> 1] = max(t[p], t[p ^ 1]);
+	void modify(int p, int val) {
+		for (t[p += flag - 1] = val; p > 1; p >>= 1)
+			t[p >> 1] = max(t[p], t[p ^ 1]);
 	}
-	ll query(int l, int r, int n = 1, int nl = 1, int nr = flag) {  // sum on interval [l, r]
+	int query(int l, int r, int n = 1, int nl = 1, int nr = flag) {
 		if (r < nl || nr < l) return 0;
 		if (l <= nl && nr <= r) return t[n];
-
-		int mid = (nl + nr) / 2;
+		int mid = (nl + nr) >> 1;
 		return max(query(l, r, n << 1, nl, mid), query(l, r, n << 1 | 1, mid + 1, nr));
 	}
 }seg;
 
-int n, m, k;
-vt<int> r[101010];
-
-void input() {
-    cin >> n >> m >> k;
-    FOR(k) {
-        int y, x;
-        cin >> y >> x;
-        r[y].push_back(x);
-    }
-}
+int m, n, k;
+struct Node { int y, x; };
+vector<Node> a;
 
 int main() {
 	#ifndef ONLINE_JUDGE
@@ -74,24 +33,22 @@ int main() {
 	cin.tie(NULL); cout.tie(NULL);
 	ios_base::sync_with_stdio(false);
 
-	input();
+	cin >> m >> n >> k;
+	a.resize(k);
+	for (auto& i : a)
+		cin >> i.y >> i.x;
 
-    FOR(101010) sort(all(r[i]));
+	sort(a.begin(), a.end(), [&](const Node& p1, const Node& p2) {
+		return (p1.y ^ p2.y ? p1.y < p2.y : p1.x < p2.x);
+	});
 
-    seg.build(101010);
+	seg.build(n);
+	for (auto& i : a) {
+		int res = seg.query(i.x + 1, n);
+		if (seg.query(i.x, i.x) < res + 1) {
+			seg.modify(i.x, res + 1);
+		}
+	}
 
-    int ans = 0;
-    FOR(101010) {
-        EACH(x, r[i]) {
-            int res = seg.query(1, x) + 1;
-            ans = max(ans, res);
-            seg.modify(x, res);
-
-            cout << i << ' ' << x << ' ' << res << '\n';
-        }
-    }
-
-    cout << ans;
-
-	return 0;
+	cout << seg.query(1, n);
 }
