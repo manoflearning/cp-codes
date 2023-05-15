37650897e5cbbc9d63d44543f7cac9d15d39d6f5
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-05-15 16:53:28 +0900

    boj

---
+++ Baekjoon OJ/15000-15999/15311.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+
+    cout << 2'000 << '\n';
+    for (int i = 1; i <= 1000; i++) cout << 1 << ' ';
+    for (int i = 1; i <= 1000; i++) cout << 1000 << ' ';
+}
