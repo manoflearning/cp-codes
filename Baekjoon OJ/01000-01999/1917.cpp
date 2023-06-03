1d58b345c6c07c50971141fda2c4f818cbef49ed
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-06-04 02:08:29 +0900

    boj

---
+++ Baekjoon OJ/01000-01999/1917.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int dy[] = { -1, 0, 1, 0 };
+const int dx[] = { 0, 1, 0, -1 };
+
+int a[6][6], vi[6][6], cnt[6], prv[6][6];
+vector<vector<int>> nxt = {
+    { 4, 2, 1, 3 },
+    { 0, 2, 5, 3 },
+    { 4, 5, 1, 0 },
+    { 4, 0, 1, 5 },
+    { 5, 2, 0, 3 },
+    { 4, 3, 1, 2 }
+};
+
+int dfs(int y, int x, int label) {
+    cnt[label]++;
+    vi[y][x] = 1;
+    prv[y][x] = label;
+    if (cnt[label] > 1) return 0;
+
+    int add = 0;
+    for (int d = 0; d < 4; d++) {
+        int ny = y + dy[d], nx = x + dx[d];
+        if (ny < 0 || 6 <= ny || nx < 0 || 6 <= nx) continue;
+        if (!vi[ny][nx]) continue;
+        if (!a[ny][nx]) continue;
+
+        for (int i = 0; i < 4; i++) {
+            if (nxt[label][(d + i) % 4] == prv[ny][nx]) { add = i; break; }
+        }
+    }
+
+    int ret = 1;
+    for (int d = 0; d < 4; d++) {
+        int ny = y + dy[d], nx = x + dx[d];
+        if (ny < 0 || 6 <= ny || nx < 0 || 6 <= nx) continue;
+        if (vi[ny][nx]) continue;
+        if (!a[ny][nx]) continue;
+        ret &= dfs(ny, nx, nxt[label][(d + add) % 4]);
+    }
+
+    return ret;
+}
+
+int main() {
+    int tc = 3;
+    for (int tt = 0; tt < tc; tt++) {
+        // initialize
+        for (int i = 0; i < 6; i++) {
+            memset(vi[i], 0, sizeof(vi[i]));
+            memset(prv[i], 0, sizeof(prv[i]));
+            cnt[i] = 0;
+        }
+
+        // input
+        for (int i = 0; i < 6; i++) 
+            for (int j = 0; j < 6; j++) cin >> a[i][j];
+        
+        // solve
+        int ans = -1;
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                if (a[i][j]) {
+                    ans = dfs(i, j, 0);
+                    i = j = 6;
+                    break;
+                }
+            }
+        }
+
+        cout << (ans ? "yes" : "no") << '\n';
+    }
+}
