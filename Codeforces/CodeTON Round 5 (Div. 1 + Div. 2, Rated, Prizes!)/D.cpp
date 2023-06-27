54babb54722f978a5c495e55df1a3cd81ea81200
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-06-27 21:20:58 +0900

    cf

--- Codeforces/CodeTON Round 5 (Div. 1 + Div. 2, Rated, Prizes!)/D.cpp
+++ Codeforces/CodeTON Round 5 (Div. 1 + Div. 2, Rated, Prizes!)/D.cpp
@@ -1,51 +1,33 @@
-//#pragma GCC optimize("O3")
-//#pragma GCC optimize("Ofast")
-//#pragma GCC optimize("unroll-loops")
-
 #include <bits/stdc++.h>
-#include <cassert>
 using namespace std;
 #define ll long long
-#define ull unsigned long long
-#define ld long double
-#define pii pair<int, int>
-#define pll pair<ll, ll>
-#define fr first
-#define sc second
-#define all(c) (c).begin(), (c).end()
-#define sz(x) (int)(x).size()
 
-const double EPS = 1e-9;
-const ll INF = 1e18 + 7;
-const int MOD = 1e9 + 7;
-const int dy[] = { 0, 0, 1, -1, 1, 1, -1, -1 };
-const int dx[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+const ll INF = 1e18;
 
-const int MAX = 111;
+struct wv { ll w; int v; };
 
 int n, m;
-struct wv { ll w; int v; };
-vector<wv> adj[MAX];
+vector<wv> adj[101];
+vector<ll> upper(101, INF);
 
-void input() {
-    cin >> n >> m;
-    for (int i = 0; i < m; i++) {
-        int u, v, w;
-        cin >> u >> v >> w;
-        adj[u].push_back({ w, v });
-        adj[v].push_back({ w, u });
+int bellmanFord() {
+    upper[1] = 0;
+    int update = 1;
+    for (int i = 0; i <= n; i++) {
+        update = 0;
+        for (int now = 1; now <= n; now++) {
+            if (upper[now] == INF) continue;
+            for (auto& e : adj[now]) {
+                int next = e.v;
+                if (upper[next] > upper[now] + e.w) {
+                    upper[next] = upper[now] + e.w;
+                    update = 1;
+                }
+            }
+        }
+        if (!update) break;
     }
-}
-
-int vi[MAX];
-int isInf(int v) {
-    if (v == n) return 0;
-    int ret = 1;
-    vi[v] = 1;
-    for (auto& i : adj[v]) {
-        if (!vi[i.v]) ret &= isInf(i.v);
-    }
-    return ret;
+    return !update;
 }
 
 int main() {
@@ -57,60 +39,41 @@ int main() {
     cin.tie(NULL); cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
-    input();
-
-    // 1과 n이 같은 컴포넌트가 아니면 답은 "inf".
-    if (isInf(1)) {
-        cout << "inf";
-        return 0;
+    cin >> n >> m;
+    for (int i = 0; i < m; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        adj[u].push_back({ w, v });
+        adj[v].push_back({ w, u });
     }
-    memset(vi, 0, sizeof(vi));
 
-    // solve
-    ll total = 0;
-    vector<pair<string, ll>> ans;
+    if (bellmanFord() && upper[n] != INF) {
+        vector<int> arr(n);
+        iota(arr.begin(), arr.end(), 1);
+        sort(arr.begin(), arr.end(), [&](int p1, int p2) {
+            return upper[p1] < upper[p2];
+        });
 
-    vector<int> cur(1, 1);
-    vi[1] = 1;
+        vector<pair<string, ll>> ans;
 
-    while (1) {
         string s(n, '0');
-        for (auto& i : cur) s[i - 1] = '1';
-
-        ll res = INF;
-        for (auto& v : cur) {
-            for (auto& i : adj[v]) {
-                if (vi[i.v]) continue;
-                res = min(res, i.w);
+        int idx = 0;
+        ll prvT = 0;
+        while (idx < n) {
+            while (idx + 1 < n && upper[arr[idx]] == upper[arr[idx + 1]]) {
+                s[arr[idx++] - 1] = '1';
             }
+            s[arr[idx++] - 1] = '1';
+            if (s[n - 1] == '1') break;
+            ll nowT = upper[arr[idx]];
+            ans.push_back({ s, nowT - prvT });
+            prvT = nowT;
         }
 
-        if (res == INF) break;
-
-        if (res > 0) {
-            total += res;
-            ans.push_back({ s, res });
+        cout << upper[n] << ' ' << ans.size() << '\n';
+        for (auto& i : ans) {
+            cout << i.first << ' ' << i.second << '\n';
         }
-
-        vector<int> ncur;
-        for (auto& v : cur) {
-            for (auto& i : adj[v]) {
-                if (vi[i.v]) continue;
-                i.w -= res;
-                if (i.w == 0) ncur.push_back(i.v);
-            }
-        }
-
-        for (auto& v : ncur) {
-            cur.push_back(v);
-            vi[v] = 1;
-        }
-
-        if (vi[n]) break;
     }
-
-    cout << total << ' ' << sz(ans) << '\n';
-    for (auto& i : ans) {
-        cout << i.fr << ' ' << i.sc << '\n';
-    }
+    else cout << "inf";
 }
