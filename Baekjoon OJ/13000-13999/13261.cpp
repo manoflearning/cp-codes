1d43988946856d818d8eef537de35d6648f755a4
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-06-27 17:55:33 +0900

    boj

--- Baekjoon OJ/13000-13999/13261.cpp
+++ Baekjoon OJ/13000-13999/13261.cpp
@@ -1,14 +1,18 @@
-// dp 최적화 혹은 파라메트릭 서치?
+// Divide and Conquer Optimization 
+// Recurrence: dp[t][i] = min(dp[t - 1][j] + c[j][i]) (j < i)
+// Condition: Let opt[t][i] be j with the smallest value of dp[t - 1][j] + c[j][i]. It must satisfy opt[t][i] <= opt[t][i + 1].
+// Naive Complexity: O(m * n^2)
+// Optimized Complexity: O(m * nlogn)
+
+// BOJ 13261 AC Code
+// https://www.acmicpc.net/problem/13261
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-
 const ll INF = 1e18;
-
 int n, m;
 ll a[8080], psum[8080];
 ll dp[808][8080];
-
 void f(int gr, int l, int r, int nl, int nr) {
     int mid = (l + r) >> 1, idx = -1;
     ll& res = dp[gr][mid];
@@ -27,11 +31,6 @@ void f(int gr, int l, int r, int nl, int nr) {
 }
 
 int main() {
-    #ifndef ONLINE_JUDGE
-    freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
-    freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
-    #endif
-
     cin.tie(NULL); cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
@@ -47,9 +46,8 @@ int main() {
     for (int i = 1; i <= n; i++)
         dp[1][i] = i * psum[i];
 
-    for (int i = 2; i <= m; i++) {
+    for (int i = 2; i <= m; i++)
         f(i, 0, n, 0, n);
-    }
 
     // output
     cout << dp[m][n];
