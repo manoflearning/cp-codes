a3a4626ec4876a85bdad388734da89373268cd23
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-06-27 23:24:43 +0900

    cf

--- Codeforces/CodeTON Round 5 (Div. 1 + Div. 2, Rated, Prizes!)/E.cpp
+++ Codeforces/CodeTON Round 5 (Div. 1 + Div. 2, Rated, Prizes!)/E.cpp
@@ -1,42 +1,65 @@
-#pragma GCC optimize("O3")
-//#pragma GCC optimize("Ofast")
-#pragma GCC optimize("unroll-loops")
-
 #include <bits/stdc++.h>
-#include <cassert>
 using namespace std;
 #define ll long long
-#define ull unsigned long long
-#define ld long double
-#define pii pair<int, int>
-#define pll pair<ll, ll>
-#define fr first
-#define sc second
-#define all(c) (c).begin(), (c).end()
-#define sz(x) (int)(x).size()
 
-const double EPS = 1e-9;
-const ll INF = 1e18;
-const int MOD = 1e9 + 7;
-const int dy[] = { 0, 0, 1, -1, 1, 1, -1, -1 };
-const int dx[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+int flag;  // array size
+struct Seg {  // 1-indexed
+    vector<ll> t, lazy;
+    void build(int n) {
+        for (flag = 1; flag < n; flag <<= 1);
+        t.resize(2 * flag);
+        lazy.resize(2 * flag);
+    }
+    // add a value to all elements in interval [l, r]
+    void modify(int l, int r, ll value, int n = 1, int nl = 1, int nr = flag) {
+        propagate(n, nl, nr);
+        if (r < nl || nr < l) return;
+        if (l <= nl && nr <= r) {
+            lazy[n] += value;
+            propagate(n, nl, nr);
+            return;
+        }
+        int mid = (nl + nr) >> 1;
+        modify(l, r, value, n << 1, nl, mid);
+        modify(l, r, value, n << 1 | 1, mid + 1, nr);
+        t[n] = max(t[n << 1], t[n << 1 | 1]);
+    }
+    ll query(int l, int r, int n = 1, int nl = 1, int nr = flag) {  // sum on interval [l, r]
+        propagate(n, nl, nr);
+        if (r < nl || nr < l) return 0;
+        if (l <= nl && nr <= r) return t[n];
+        int mid = (nl + nr) / 2;
+        return max(query(l, r, n << 1, nl, mid), query(l, r, n << 1 | 1, mid + 1, nr));
+    }
+    void propagate(int n, int nl, int nr) {
+        if (lazy[n] != 0) {
+            if (n < flag) {
+                lazy[n << 1] += lazy[n];
+                lazy[n << 1 | 1] += lazy[n];
+            }
+            t[n] += lazy[n];
+            lazy[n] = 0;
+        }
+    }
+}seg;
 
 int n, k;
-ll w;
-map<pii, ll> a;
-ll all[202020];
+ll A, total;
+struct Point { int x; ll c; };
+vector<Point> p[202020];
+ll dp[202020];
 
 void input() {
-    cin >> n >> k >> w;
+    cin >> n >> k >> A;
+    k += 2;
     for (int i = 0; i < n; i++) {
         int x, y, c;
         cin >> x >> y >> c;
-        a[{ x, y }] = c;
-        all[x] += c;
+        x++, y++;
+        p[y].push_back({ x, c });
+        total += c;
     }
 }
-
-ll dp[2][202020];
 
 int main() {
     #ifndef ONLINE_JUDGE
@@ -49,40 +72,17 @@ int main() {
 
     input();
 
-    // naive
-    // dp[x][y]는 0 <= x_i <= x인 모든 점들을 삭제했을 때 비용의 최솟값.
-    // 마지막 삼각형의 yco = y임을 의미. 단 yco = k라면 삼각형을 설치하지 않은 것.
+    seg.build(202020);
 
-    // base case
-    ll rem = all[0];
-    dp[0][k] = rem;
-    for (int y = k - 1; y >= 0; y--) {
-        if (a.count({ 0, y })) rem -= a[{ 0, y }];
-        dp[0][y] = rem + (k - (0 + y)) * w;
-    }
-
-    // inductive step
-    for (int x = 1; x < k; x++) {
-        int b = (x & 1);
-        ll prv = INF;
-
-        rem = all[x];
-        for (int y = k; y > k - x - 1; y--) {
-            dp[b][y] = rem + dp[!b][y];
-            prv = min(prv, dp[!b][y]);
+    for (int i = 2; i <= k; i++) {
+        for (auto& j : p[k - i]) {
+            seg.modify(1, j.x, j.c);
         }
+        seg.modify(1, i - 1, -A);
         
-        for (int y = k - x - 1; y >= 0; y--) {
-            if (a.count({ x, y })) rem -= a[{ x, y }];
-            dp[b][y] = min(rem + dp[!b][y], prv + rem + (k - (x + y)) * w);
-            prv = min(prv, dp[!b][y]);
-        }
+        dp[i] = max(seg.query(1, i - 1), dp[i - 1]);
+        seg.modify(i, i, dp[i]);
     }
 
-    // output
-    ll ans = INF;
-    for (int y = 0; y <= k; y++)
-        ans = min(ans, dp[(k - 1) & 1][y]);
-
-    cout << ans;
+    cout << total - dp[k];
 }
