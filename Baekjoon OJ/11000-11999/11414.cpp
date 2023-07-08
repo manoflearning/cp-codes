d6ecda0bbdd048addc023b1667f9a90c12de9b7c
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-07-09 02:55:05 +0900

    boj

--- Baekjoon OJ/11000-11999/11414.cpp
+++ Baekjoon OJ/11000-11999/11414.cpp
@@ -21,22 +21,30 @@ int main() {
 
     ll A, B;
     cin >> A >> B;
-    if (A > B) swap(A, B); // wlog, A <= B
 
     if (A == B) {
         cout << 1;
         return 0;
     }
 
-    ll diff = B - A; // invariant
-    // diff >= gcd(A + N, B + N)
+    if (A > B) swap(A, B); // wlog, A <= B
 
-    ll mn = lcm(A + diff, B + diff), ans = diff;
-    for (ll N = 1; N < diff && B + N < mn; N++) {
+    ll diff = B - A;
+    vector<ll> div;
+    for (ll i = 1; i * i <= diff; i++) {
+        if (diff % i == 0) {
+            div.push_back(i);
+            if (i != diff / i) 
+                div.push_back(diff / i);
+        }
+    }
+
+    ll mn = 2e18, ans = 1e18;
+    for (auto& i : div) {
+        ll N = i - A % i;
         ll res = lcm(A + N, B + N);
-        if (res < mn) {
-            mn = res;
-            ans = N;
+        if (res < mn || (res == mn && N < ans)) {
+            mn = res, ans = N;
         }
     }
 
