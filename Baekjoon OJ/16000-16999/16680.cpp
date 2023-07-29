04cfe466856e689f31bd2fe8947564d2e277f0b5
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-07-29 17:19:13 +0900

    boj

--- Baekjoon OJ/16000-16999/16680.cpp
+++ Baekjoon OJ/16000-16999/16680.cpp
@@ -1,3 +1,4 @@
+// solution 1
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
@@ -20,6 +21,21 @@ int isAnSooBinNumber(ll n) {
     return sum & 1;
 }
 
+ll sol1(ll n) {
+    return ((ll)1e9 - 1) * n;
+}
+ll sol2(ll n) {
+    srand(time(NULL));
+    while (1) {
+        ll x = rand() % (ll)1e9;
+        if (isAnSooBinNumber(x * n)) return x * n;
+    }
+}
+ll sol3(ll n) {
+    while (!isAnSooBinNumber(n) && n <= 2 * p10[17]) n *= 2;
+    return n;
+}
+
 int main() {
     #ifndef ONLINE_JUDGE
     freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
@@ -34,7 +50,6 @@ int main() {
     int tc; cin >> tc;
     while (tc--) {
         ll n; cin >> n;
-        while (!isAnSooBinNumber(n) && n <= 2 * p10[17]) n *= 2;
-        cout << n << '\n';
+        cout << sol2(n) << '\n';
     }
 }
