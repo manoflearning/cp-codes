189e81e72fc0c070dd9bde90e003b3525353c97d
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-09-07 11:55:21 +0900

    scpc

--- SCPC/SCPC 2022/Final Round/2-12.cpp
+++ SCPC/SCPC 2022/Final Round/2-12.cpp
@@ -1,3 +1,18 @@
+// naive
+// dp[i][j] = max(a[k] + min(dp[i][k - 1], dp[k + 1][j])) (i <= k <= j)
+
+// dp[i][j] <= dp[i][j + 1], dp[i][j] <= dp[i - 1][j]
+
+// k <= K[i][j]인 경우 dp[i][k - 1] < dp[k + 1][j]이고
+// k > K[i][j]인 경우 dp[i][k - 1] >= dp[k + 1][j]인 K[i][j]가 존재한다.
+
+// K[i - 1][j] <= K[i][j] <= K[i][j + 1]
+// k > K[i][j]에 대해 dp[i - 1][k - 1] >= dp[i][k - 1] >= dp[k + 1][j]이므로
+// K[i - 1][j] <= K[i][j]이다.
+
+// k <= K[i][j]에 대해 dp[i][k - 1] < dp[k + 1][j] <= dp[k + 1][j + 1]이므로
+// K[i][j] <= K[i][j + 1]
+
 // #pragma GCC optimize("O3")
 // #pragma GCC optimize("Ofast")
 // #pragma GCC optimize("unroll-loops")
@@ -12,43 +27,22 @@ const int MAXN = 3030;
 
 int n;
 ll a[MAXN];
-// ll psum[MAXN];
 ll dp[MAXN][MAXN];
+int opt[MAXN][MAXN];
 
 void init() {}
 
 void input() {
     cin >> n;
-    a[0] = 0;
     for (int i = 1; i <= n; i++) cin >> a[i];
-    a[n + 1] = 0;
 }
 
-// void buildPsum() {
-//     for (int i = 1; i < n; i++)
-//         psum[i] = a[i] + psum[i - 1];
-// }
-
-// ll sum(int l, int r) {
-//     return psum[r] - psum[l - 1];
-// }
-
-void f() { // dp[l][r] = [l, r] 범위에서 최적해
-    // base case
-    for (int i = 1; i <= n; i++) {
-        dp[i][i] = a[i];
-    }
-    
-    // inductive step
+void f() {
+    for (int i = 1; i <= n; i++) dp[i][i] = a[i];
     for (int len = 2; len <= n; len++) {
-        for (int l = 1; ; l++) {
-            int r = l + len - 1;
-            if (n < r) break;
-
-            dp[l][r] = 0;
-            for (int i = l; i <= r; i++) {
-                dp[l][r] = max(dp[l][r], a[i] + min(dp[l][i - 1], dp[i + 1][r]));
-            }
+        int prv = 1;
+        for (int l = 1, r = len; r <= n; l++, r++) {
+            for (int opt = prv; dp[l][opt - 1] < dp[opt + 1][r])
         }
     }
 }
@@ -69,12 +63,6 @@ int main() {
         init();
 
         input();
-
-        // buildPsum();
-
-        f();
-        
-        cout << dp[1][n] << '\n';
 
         cout << flush;
     }
