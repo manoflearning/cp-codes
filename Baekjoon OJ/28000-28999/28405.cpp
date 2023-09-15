30ac4db745b2ab4f0f617e65a8ccb38c37aace7c
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-09-16 00:09:57 +0900

    boj

--- Baekjoon OJ/28000-28999/28405.cpp
+++ Baekjoon OJ/28000-28999/28405.cpp
@@ -1,9 +1,13 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
 #define pll pair<ll, ll>
 #define fr first
 #define sc second
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
 
 const int MAXN = 505050;
 const ll INF = 1e18;
@@ -35,78 +39,66 @@ void getStartIndex() {
 
 int flag;
 struct Seg {
-    const pll e = { INF, INF };
-    vector<pll> t, lazy;
+    vector<ll> t;
     void build(int n) {
         for (flag = 1; flag < n; flag <<= 1);
-        t.resize(flag << 1, e);
-        lazy.resize(flag << 1, e);
+        t.resize(flag << 1, INF);
     }
-    void modify(int l, int r, pll val, int n = 1, int nl = 1, int nr = flag) {
-        propagate(n);
-        if (r < nl || nr < l) return;
-        if (l <= nl && nr <= r) {
-            lazy[n] = val;
-            propagate(n);
-            return;
-        }
-        int mid = (nl + nr) >> 1;
-        modify(l, r, val, n << 1, nl, mid);
-        modify(l, r, val, n << 1 | 1, mid + 1, nr);
-        if (t[n << 1].fr + t[n << 1].sc <= t[n << 1 | 1].fr + t[n << 1 | 1].sc) t[n] = t[n << 1];
-        else t[n] = t[n << 1 | 1];
-        cout << nl << ' ' << nr << ' ' << t[n].fr + t[n].sc << '\n';
+    void modify(int p, ll val) {
+        for (t[p += flag - 1] = val; p > 1; p >>= 1)
+            t[p >> 1] = min(t[p], t[p ^ 1]);
     }
     ll query(int l, int r, int n = 1, int nl = 1, int nr = flag) {
-        propagate(n);
-        if (r < nl || nr < l) return INF;
-        if (l <= nl && nr <= r) return t[n].fr + t[n].sc;
+        if (nr < l || r < nl) return INF;
+        if (l <= nl && nr <= r) return t[n];
         int mid = (nl + nr) >> 1;
-        return min(query(l, r, n << 1, nl, mid), 
-        query(l, r, n << 1 | 1, mid + 1, nr));
-    }
-    void propagate(int n) {
-        if (lazy[n] == e) return;
-        if (n < flag) {
-            lazy[n << 1].fr += lazy[n].fr;
-            lazy[n << 1].sc = max(lazy[n << 1].sc, lazy[n].sc);
-            lazy[n << 1 | 1].fr += lazy[n].fr;
-            lazy[n << 1 | 1].sc = max(lazy[n << 1 | 1].sc, lazy[n].sc);
-        }
-        if (t[n] == e) t[n] = { 0, 0 };
-        t[n].fr += lazy[n].fr;
-        t[n].sc = max(t[n].sc, lazy[n].sc);
-        lazy[n] = e;
+        return min(query(l, r, n << 1, nl, mid), query(l, r, n << 1 | 1, mid + 1, nr));
     }
 }seg;
 
 ll dp[MAXN];
+
+inline int add(int idx) { return n - idx - 1; }
+inline int del(int idx) { return idx - n; }
+
+int stk[MAXN];
 void f() {
-    seg.build(MAXN);
-    seg.modify(1, 1, { 0, 0 });
-    for (int i = 1; i <= n; i++) {
-        seg.modify(1, i - 1, { 1, 0 });
-        seg.modify(1, i, { 0, a[i] });
-        dp[i] = seg.query(st[i], i);
-        for (int j = st[i]; j <= i; j++)
-            cout << seg.query(j, j) << ' ';
-        cout << seg.query(st[i], i) << '\n';
-        cout << dp[i] << '\n';
-        seg.modify(i + 1, i + 1, { dp[i], 0 });
+    int top = 1;
+    seg.build(n + 1);
+
+    // base case
+    dp[0] = 0;
+    seg.modify(top, a[1] + dp[0] + add(0));
+
+    // inductive step
+    for (int i = 1; i <= n; i++) { // O(nlogn)
+        while (top > 1 && a[stk[top]] <= a[i]) top--;
+        stk[++top] = i;
+
+        ll val = a[i] + dp[stk[top - 1]] + add(stk[top - 1]);
+        seg.modify(top, val);
+
+        int l = lower_bound(stk + 1, stk + top + 1, st[i]) - stk + 1;
+        int r = top;
+        dp[i] = seg.query(l, r) + del(i);
+        
+        int idx = *lower_bound(stk + 1, stk + top + 1, st[i]);
+        dp[i] = min(dp[i], a[idx] + dp[st[i] - 1] + i - st[i]);
     }
 }
-void naive() {
-    dp[1] = a[1];
-    for (int i = 2; i <= n; i++) {
-        ll mx = a[i];
-        dp[i] = mx + dp[i - 1];
-        for (int j = i - 1; j >= 1; j--) {
-            mx = max(mx, a[j]);
-            if (mx > p - (i - j)) break;
-            dp[i] = min(dp[i], i - j + mx + dp[j - 1]);
-        }
-    }
-}
+
+// void naive() {
+//     dp[1] = a[1];
+//     for (int i = 2; i <= n; i++) {
+//         ll mx = a[i];
+//         dp[i] = mx + dp[i - 1];
+//         for (int j = i - 1; j >= 1; j--) {
+//             mx = max(mx, a[j]);
+//             if (mx > p - (i - j)) break;
+//             dp[i] = min(dp[i], i - j + mx + dp[j - 1]);
+//         }
+//     }
+// }
 
 int main() {
     #ifndef ONLINE_JUDGE
