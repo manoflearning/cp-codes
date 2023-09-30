4674f4141279e8b5c2a3b3f9c52c18a1d83cc20c
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-09-30 18:35:30 +0900

    boj

--- Baekjoon OJ/05000-05999/5466.cpp
+++ Baekjoon OJ/05000-05999/5466.cpp
@@ -4,105 +4,132 @@ using namespace std;
 #define pll pair<ll, ll>
 #define fr first
 #define sc second
-#define all(x) (x).begin(), (x).end()
 #define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
 
-const int MAXN = 505050;
-const int MAXL = 500001;
 const int MAXT = 500001;
+const int MAXL = 500001;
 const ll INF = 1e18;
 
 int flag;
 struct Seg {
     vector<ll> t;
-    void build() {
-        for (flag = 1; flag < MAXL; flag <<= 1);
+    void build(int n) {
+        for (flag = 1; flag < n; flag <<= 1);
         t.resize(flag << 1, -INF);
     }
     void modify(int p, ll val) {
-        t[p + flag - 1] = max(val, t[p + flag - 1]);
-        for (p += flag - 1; p > 1; p >>= 1)
+        if (t[p + flag - 1] >= val) return;
+        for (t[p += flag - 1] = val; p > 1; p >>= 1)
             t[p >> 1] = max(t[p], t[p ^ 1]);
     }
     ll query(int l, int r, int n = 1, int nl = 1, int nr = flag) {
-        if (r < nl || nr < l) return -INF;
+        if (nr < l || r < nl) return -INF;
         if (l <= nl && nr <= r) return t[n];
         int mid = (nl + nr) >> 1;
         return max(query(l, r, n << 1, nl, mid), query(l, r, n << 1 | 1, mid + 1, nr));
     }
-}segR, segL; 
+}segR, segL;
 
-struct Market {
-    int x; ll M;
-    bool operator<(const Market& rhs) const {
-        return x < rhs.x;
-    }
-};
+struct Market { int p; ll w; };
 
 int N, S;
-ll U, D;
-vector<Market> a[MAXN];
-ll dp[MAXN];
+ll lw, rw;
+vector<Market> a[505050];
 
 void input() {
-    cin >> N >> U >> D >> S;
-    for (int i = 0; i < N; i++) {
-        int T, x, M;
-        cin >> T >> x >> M;
-        a[T].push_back({ x, M });
-    }
+    cin >> N >> lw >> rw >> S;
     a[0].push_back({ S, 0 });
+    for (int i = 0; i < N; i++) {
+        int T, p, w;
+        cin >> T >> p >> w;
+        a[T].push_back({ p, w });
+    }
     a[MAXT].push_back({ S, 0 });
 }
 
 void sorting() {
-    for (int i = 1; i <= MAXT; i++) {
-        sort(all(a[i]));
+    for (int T = 0; T <= MAXT; T++) {
+        sort(all(a[T]), [&](const Market& p1, const Market& p2) {
+            return p1.p < p2.p;
+        });
     }
 }
 
-void modifyR(int p, ll val) { segR.modify(p, val - D * (MAXL - p)); }
-void modifyL(int p, ll val) { segL.modify(p, val - U * (p - 1)); }
-ll queryR(int p) { return segR.query(1, p - 1) + D * (MAXL - p); }
-ll queryL(int p) { return segL.query(p + 1, MAXL) + U * (p - 1); }
+void modifyL(int p, ll val) { segL.modify(p, val - lw * (p - 1)); }
+void modifyR(int p, ll val) { segR.modify(p, val - rw * (MAXL - p)); }
+ll queryL(int p) { return segL.query(p + 1, MAXL) + lw * (p - 1); }
+ll queryR(int p) { return segR.query(1, p - 1) + rw * (MAXL - p); }
 
-void f() {
+ll dp[505050];
+
+void solve() {
+    segL.build(MAXL);
+    segR.build(MAXL);
     for (int i = 1; i <= MAXL; i++) dp[i] = -INF;
-    segR.build();
-    segL.build();
     
+    // base case
     dp[S] = 0;
-    modifyR(S, dp[S]);
     modifyL(S, dp[S]);
+    modifyR(S, dp[S]);
 
-    for (int i = 1; i <= MAXT; i++) {
-        vector<ll> psum(sz(a[i])), ssum(sz(a[i]));
-        for (int j = 1; j < sz(a[i]); j++) {
-            ll len = a[i][j].x - a[i][j - 1].x;
-            psum[j] = max(0ll, a[i][j - 1].M - len * (U + D) + max(0ll, psum[j - 1]));
-        }
-        for (int j = sz(a[i]) - 2; j >= 0; j--) {
-            ll len = a[i][j + 1].x - a[i][j].x;
-            ssum[j] = max(0ll, a[i][j + 1].M - len * (U + D) + max(0ll, ssum[j + 1]));
+    // inductive step
+    for (int T = 1; T <= MAXT; T++) {
+        const auto& arr = a[T];
+        if (arr.empty()) continue;
+
+        vector<ll> LR(sz(arr)), RL(sz(arr));
+        // LR[i]는 i에서 시작해서 왼쪽으로 이동했다가, 다시 오른쪽으로 이동하여 i에 도착했을 때 얻을 수 있는 최댓값
+        // RL[i]는 i에서 시작해서 오른쪽으로 이동했다가, 다시 왼쪽으로 이동하여 i에 도착했을 때 얻을 수 있는 최댓값
+
+        vector<pll> lazyModifyR, lazyModifyL;
+        for (int i = 0; i < sz(arr); i++) {
+            const auto& it = arr[i];
+            LR[i] = it.w;
+            if (i == 0) continue;
+            LR[i] += max(0ll, LR[i - 1] - (lw + rw) * (it.p - arr[i - 1].p));
+
+            ll res = it.w;
+            res += LR[i - 1];
+            res += queryL(arr[i - 1].p);
+            res -= rw * (it.p - arr[i - 1].p);
+            dp[it.p] = max(dp[it.p], res);
+            lazyModifyR.push_back({ it.p, res });
         }
 
-        for (int j = 0; j < sz(a[i]); j++) {
-            int x = a[i][j].x; ll M = a[i][j].M;
-            ll res = M + queryR(x) + ssum[j];
-            dp[x] = max(dp[x], res);
-            modifyR(x, res);
-        }
-        for (int j = sz(a[i]) - 1; j >= 0; j--) {
-            int x = a[i][j].x; ll M = a[i][j].M;
-            ll res = M + queryL(x) + psum[j];
-            dp[x] = max(dp[x], res);
-            modifyL(x, res);
+        for (int i = sz(arr) - 1; i >= 0; i--) {
+            const auto& it = arr[i];
+            RL[i] = arr[i].w;
+            if (i == sz(arr) - 1) continue;
+            RL[i] += max(0ll, RL[i + 1] - (lw + rw) * (arr[i + 1].p - arr[i].p));
+
+            ll res = it.w;
+            res += RL[i + 1];
+            res += queryR(arr[i + 1].p);
+            res -= lw * (arr[i + 1].p - it.p);
+            dp[it.p] = max(dp[it.p], res);
+            lazyModifyL.push_back({ it.p, res });
         }
 
-        // update
-        for (int j = 0; j < sz(a[i]); j++) {
-            modifyR(a[i][j].x, dp[a[i][j].x]);
-            modifyL(a[i][j].x, dp[a[i][j].x]);
+        for (auto& i : lazyModifyR) modifyR(i.fr, i.sc);
+        for (auto& i : lazyModifyL) modifyL(i.fr, i.sc);
+
+        for (int i = 0; i < sz(arr); i++) {
+            const auto& it = arr[i];
+            ll res = it.w + queryR(it.p);
+            dp[it.p] = max(dp[it.p], res);
+            modifyR(it.p, res);
+        }
+        for (int i = sz(arr) - 1; i >= 0; i--) {
+            const auto& it = arr[i];
+            ll res = it.w + queryL(it.p);
+            dp[it.p] = max(dp[it.p], res);
+            modifyL(it.p, res);
+        }
+
+        for (auto& i : arr) {
+            modifyL(i.p, dp[i.p]);
+            modifyR(i.p, dp[i.p]);
         }
     }
 }
@@ -120,7 +147,7 @@ int main() {
 
     sorting();
 
-    f();
+    solve();
 
     cout << dp[S];
 }
