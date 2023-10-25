0c9c31d9e2bc95c6e7935e01ef28ceda3603071a
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-10-26 01:56:28 +0900

    CodeChef

--- CodeChef/Starters 105 Division 4 (Rated)/RIP.cpp
+++ CodeChef/Starters 105 Division 4 (Rated)/RIP.cpp
@@ -23,28 +23,14 @@ const int dx[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
 int n, m;
 int a[101010], b[101010], c[101010];
-vector<int> isInC;
+vector<int> isInC, isInC2;
 ll ans = 0;
 
-int flag;  // array size
-struct Seg {  // 1-indexed
-	vector<ll> t;
-	void build(int n) {
-		for (flag = 1; flag < n; flag <<= 1);
-		t.resize(2 * flag);
-	}
-	void modify(int p, ll value) {  // set value at position p
-		for (t[p += flag - 1] = value; p > 1; p >>= 1) t[p >> 1] = max(t[p], t[p ^ 1]);
-	}
-	ll query(int l, int r, int n = 1, int nl = 1, int nr = flag) {
-		if (r < nl || nr < l) return 0;
-		if (l <= nl && nr <= r) return t[n];
-		int mid = (nl + nr) / 2;
-		return max(query(l, r, n << 1, nl, mid), query(l, r, n << 1 | 1, mid + 1, nr));
-	}
-}seg;
-
-void init() {}
+void init() {
+    isInC.clear();
+    isInC2.clear();
+    ans = 0;
+}
 
 void input() {
     cin >> n >> m;
@@ -52,28 +38,38 @@ void input() {
     for (int i = 1; i <= n; i++) cin >> b[i];
     for (int i = 1; i <= m; i++) cin >> c[i];
     isInC.resize(n + 1);
-    for (int i = 1; i <= m; i++) isInC[c[i]] = i;
+    isInC2.resize(n + 1);
+    for (int i = 2; i <= m; i++) {
+        isInC[c[i]] = 1;
+        if (a[c[i - 1]] < a[c[i]]) isInC2[c[i]] = 1;
+    }
 }
 
 void solve() {
-    vector<int> arr1, arr2;
+    vector<int> arr;
     for (int i = 1; i <= n; i++) {
-        if (isInC[i]) arr2.push_back(i);
-        else arr1.push_back(i);
+        arr.push_back(i);
         ans += b[i];
     }
 
-    sort(all(arr1), [&](int p1, int p2) {
+    sort(all(arr), [&](int p1, int p2) {
         return a[p1] ^ a[p2] ? a[p1] > a[p2] : b[p1] < b[p2];
     });
-    sort(all(arr2), [&](int p1, int p2) {
-        return isInC[p1] < isInC[p2];
-    });
 
-    for (int i = 0; i < sz(arr2); i++) {
-        int val = a[arr2[i]];
-        
+    priority_queue<ll, vector<ll>, greater<ll>> pq;
+    
+    int isMaxSkipped = 0;
+
+    for (int i = 0; i < sz(arr); i++) {
+        int v = arr[i];
+        if (isInC2[v]) {
+            if (!pq.empty() && pq.top() <= b[v]) { ans -= pq.top(); pq.pop(); pq.push(b[v]); }
+            else ans -= b[v];
+            if (a[v] == a[arr[0]]) isMaxSkipped = 1;
+        }
+        else if (!isInC[v]) pq.push(b[v]);
     }
+    if (!isMaxSkipped) ans -= b[arr[0]];
 }
 
 int main() {
