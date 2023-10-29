51919fd5b28264722095713f8c92e20d7173a6bb
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-10-29 14:19:59 +0900

    icpc

--- ICPC/2023 ICPC Seoul Regional First Round/C.cpp
+++ ICPC/2023 ICPC Seoul Regional First Round/C.cpp
@@ -1,8 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int h, s;
-
 int main() {
     #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
@@ -12,21 +10,26 @@ int main() {
     cin.tie(NULL); cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
-    string S;
-    while (cin >> S) {
-        for(auto& i : S) {
-            if(i == 'H') h++;
-            else if(i == 'A') h++, s++;
-            else if(i == 'P') h++;
-            else if(i == 'Y') h++;
-            else if(i == 'S') s++;
-            else if(i == 'D') s++;
+    int h = 0, s = 0;
+
+    while (1) {
+        string S; cin >> S;
+        if (cin.eof()) break;
+        for (auto& i : S) {
+            if (i == 'H') h++;
+            else if (i == 'A') h++, s++;
+            else if (i == 'P') h++;
+            else if (i == 'Y') h++;
+            else if (i == 'S') s++;
+            else if (i == 'D') s++;
         }
     }
 
     cout << fixed;
     cout.precision(2);
 
+    const double eps = 1e-8;
+
     if (h == 0 && s == 0) cout << "50.00";
-    else cout << ((double)h / (h + s)) * 100;
+    else cout << ((double)h / (h + s) + eps) * 100;
 }
