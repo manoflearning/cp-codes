e25b66379286ccf822682baaf803812dfd35ede1
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-12-09 20:39:04 +0900

    scpc

---
+++ SCPC/template.cpp
@@ -0,0 +1,36 @@
+// #pragma GCC optimize("O3")
+// #pragma GCC optimize("Ofast")
+// #pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+
+void init() {}
+
+void input() {
+
+}
+
+int main() {
+    #ifndef ONLINE_JUDGE
+    freopen("/Users/jeongwoo-kyung/programming/cp-codes/input.txt", "r", stdin);
+    freopen("/Users/jeongwoo-kyung/programming/cp-codes/output.txt", "w", stdout);
+    #endif
+
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int tc; cin >> tc;
+    for (int tt = 1; tt <= tc; tt++) {
+        cout << "Case #" << tt << '\n';
+
+        init();
+
+        input();
+
+        cout << flush;
+    }
+}
