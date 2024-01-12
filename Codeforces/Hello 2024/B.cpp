7145a8c918dab84739c7648b5a60920f9aca1ccb
Jeong Woo-kyung <77jwk0724@gmail.com>
2024-01-12 21:23:07 +0900

    Codeforces

--- Codeforces/Hello 2024/B.cpp
+++ Codeforces/Hello 2024/B.cpp
@@ -35,7 +35,7 @@ int main() {
         int n; cin >> n;
         string s; cin >> s;
 
-        vector<ll> a(n + 1), psum(n + 1), dp(n + 1);
+        vector<ll> a(n + 1), psum(n + 1);
         for (int i = 1; i <= n; i++) {
             a[i] = (s[i - 1] == '+' ? 1 : -1);
             psum[i] = a[i] + psum[i - 1];
