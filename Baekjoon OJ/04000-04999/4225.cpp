3c9e8cad13061d0a9f5b7c69fab777ea8b7a4d80
Jeong Woo-kyung <77jwk0724@gmail.com>
2024-01-17 17:58:40 +0900

    Baekjoon

--- Baekjoon OJ/04000-04999/4225.cpp
+++ Baekjoon OJ/04000-04999/4225.cpp
@@ -3,140 +3,89 @@ using namespace std;
 #define ll long long
 #define sz(x) (int)(x).size()
 
-const double EPS = 1e-8;
-
-struct Fraction {
-    __int128_t n, d; // represent n/d
-
-    Fraction(__int128_t N, __int128_t D) { n = N, d = D; reduce(); }
-    Fraction(__int128_t N) { n = N, d = 1; }
-    Fraction() { n = 0, d = 1; }
-
-    __int128_t gcd(__int128_t a, __int128_t b) {
-        if (b == 0) return a;
-        else return gcd(b, a % b);
-    }
-    void reduce() {
-        __int128_t g = gcd(n, d);
-        n /= g, d /= g;
-        if (d < 0) n *= -1, d *= -1;
-    }
-
-    bool operator<(const Fraction& rhs) const {
-        return n * rhs.d < rhs.n * d;
+struct Point {
+    ll x, y;
+    bool operator<(const Point& rhs) const {
+        if (y != rhs.y) return y < rhs.y;
+        return x < rhs.x;
     }
 };
 
-struct point {
-	ll x, y;
-	bool operator<(const point& rhs) const {
-		if (y != rhs.y) return y < rhs.y;
-		return x < rhs.x;
-	}
-};
-
-struct line {
-    __int128_t a, b, c;
-    int idx1, idx2;
-    Fraction dist(const point& p) {
-        Fraction ret;
-        ret.n = (a * p.x + b * p.y + c) * (a * p.x + b * p.y + c);
-        ret.d = (a * a + b * b);
-        ret.reduce();
-        return ret;
-    }
-};
-
-int N;
-vector<point> p;
+int n;
+vector<Point> p;
 vector<int> st;
-vector<line> li;
 
 void init() {
     p.clear();
     st.clear();
-    li.clear();
 }
 
 void input() {
-    cin >> N;
-    if (N == 0) exit(0);
-    p.resize(N);
-    for (auto& i : p)
-        cin >> i.x >> i.y;
+    cin >> n;
+    if (n == 0) exit(0);
+    p.resize(n);
+    for (auto& i : p) cin >> i.x >> i.y;
 }
 
-ll ccw(const point& a, const point& b, const point& c) {
-	// res > 0 -> ccw, res < 0 -> cw, res = 0 -> colinear
-	ll res = (b.x - a.x) * (c.y - a.y) - (c.x - a.x) * (b.y - a.y);
-	return (res > 0 ? 1 : (res < 0 ? -1 : 0));
+// Convex Hull
+ll ccw(const Point& a, const Point& b, const Point& c) {
+    // res > 0 -> ccw, res < 0 -> cw, res = 0 -> colinear
+    ll res = (b.x - a.x) * (c.y - a.y) - (c.x - a.x) * (b.y - a.y);
+    return (res > 0 ? 1 : (res < 0 ? -1 : 0));
 }
-ll dist(const point& p1, const point& p2) {
+
+ll dist(const Point& p1, const Point& p2) {
     return (p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y);
 }
-bool cmp(const point& p1, const point& p2) {
+
+bool cmp(const Point& p1, const Point& p2) {
     return (ccw(p[0], p1, p2) > 0 || (ccw(p[0], p1, p2) == 0 && dist(p[0], p1) < dist(p[0], p2)));
 }
-void grahamScan() {
-	sort(p.begin(), p.end());
-	sort(p.begin() + 1, p.end(), cmp);
-	st.push_back(0);
-	st.push_back(1);
-	for (int next = 2; next < N; next++) {
-		while (st.size() >= 2) {
-			int first = st.back();
-			st.pop_back();
-			int second = st.back();
-			if (ccw(p[second], p[first], p[next]) > 0) {
-				st.push_back(first);
-				break;
-			}
-		}
-		st.push_back(next);
-	}
-}
 
-void getLines() {
-    for (int i = 0; i < sz(st); i++) {
-        point u = p[st[i]], v = p[st[(i + 1) % sz(st)]];
-        __int128_t a, b, c;
-        a = (__int128_t)v.y - u.y, b = (__int128_t)u.x - v.x;
-        c = -(a * u.x + b * u.y);
-        li.push_back({ a, b, c, st[i], st[(i + 1) % sz(st)]});
+void grahamScan() {
+    sort(p.begin(), p.end());
+    sort(p.begin() + 1, p.end(), cmp);
+    st.push_back(0);
+    st.push_back(1);
+    for (int next = 2; next < n; next++) {
+        while (st.size() >= 2) {
+            int first = st.back();
+            st.pop_back();
+            int second = st.back();
+            if (ccw(p[second], p[first], p[next]) > 0) {
+                st.push_back(first);
+                break;
+            }
+        }
+        st.push_back(next);
     }
 }
 
-Fraction solve() {
-    Fraction ret(5e8, 1);
-    for (auto& i : st) {
-        point v = p[i];
-        for (auto& j : li) {
-            if (j.idx1 == i || j.idx2 == i) continue;
-            Fraction res = j.dist(v);
-            if (res < ret) ret = res;
+// get lines
+double solve() {
+    double ans = 5e8;
+    for (int i = 0; i < sz(st); i++) {
+        Point u = p[st[i]], v = p[st[(i + 1) % sz(st)]];
+        double res = 0;
+        if (u.x == v.x) {
+            for (int j = 0; j < sz(st); j++) {
+                res = max<double>(res, abs(u.x - p[st[j]].x));
+            }
+            ans = min(ans, res);
+        }
+        else {
+            double a = (double)(v.y - u.y) / (v.x - u.x);
+            double b = -1;
+            double c = u.y - a * u.x;
+            
+            for (int j = 0; j < sz(st); j++) {
+                res = max(res, abs(a * p[st[j]].x + b * p[st[j]].y + c) / sqrt(a * a + b * b));
+            }
+            ans = min(ans, res);
         }
     }
-    return ret;
-}
 
-void print(Fraction res, int tt) {
-    res.n *= 10'000;
-    res.reduce();
-
-    ll l = 0, r = 5e6;
-    while (l < r) {
-        ll mid = (l + r) / 2;
-
-        if (mid * mid * res.d < res.n) l = mid + 1;
-        else r = mid;
-    }
-    
-    assert(l - sqrt((double)res.n / res.d) < 1);
-
-    cout << fixed;
-    cout.precision(2);
-    cout << "Case " << tt << ": ";
-    cout << (double)l / 100 + EPS << '\n';
+    return ans;
 }
 
 int main() {
@@ -155,10 +104,10 @@ int main() {
 
         grahamScan();
 
-        getLines();
-
-        Fraction res = solve();
-        
-        print(res, tt);
+        cout << "Case " << tt << ": ";
+        double ans = solve();
+        cout << fixed;
+        cout.precision(2);
+        cout << ans + 0.00499999 << '\n';
     }
 }
