8dc3766df1f21d3fef7f4a423cec5ecd223f7457
Jeong Woo-kyung <77jwk0724@gmail.com>
2024-04-07 17:24:17 +0900

    boj

--- Baekjoon OJ/23000-23999/23577.cpp
+++ Baekjoon OJ/23000-23999/23577.cpp
@@ -1,79 +1,80 @@
+#pragma gcc optimize ("O3")
+#pragma gcc target ("avx2")
+
 #include <bits/stdc++.h>
+#include <x86intrin.h>
 using namespace std;
 #define ll long long
 
-const int d[] = { 1000, 100, 10, 1 };
+const int MAXN = 2020;
 
-int n;
-ll cnt[10101];
-vector<string> a;
-ll ans = 0;
+int N;
+string A[MAXN];
+short C[MAXN][MAXN] __attribute__ ((aligned (64)));
 
-ll res;
-void bt(int idx, const string& s) {
-    if (idx == 4) {
-        cnt[res]++;
-        return;
-    }
-
-    bt(idx + 1, s);
-
-    res += (s[idx] - '0') * d[idx];
-    bt(idx + 1, s);
-    res -= (s[idx] - '0') * d[idx];
+void input() {
+    cin >> N;
+    for (int i = 0; i < N; i++) cin >> A[i];
 }
 
-ll cal(int idx, int x, const string& s1, const string& s2) {
-    if (idx == 4) {
-        return (x & 1 ? -1 : 1) * cnt[res];
+void getC() { // O(N^2)
+    for (int i = 0; i < N; i++) {
+        for (int j = i + 1; j < N; j++) {
+            short res = 0;
+            for (int k = 0; k < 4; k++) {
+                res *= 10;
+                if (A[i][k] == A[j][k]) res += A[i][k] - '0';
+            }
+            C[i][j] = C[j][i] = res;
+        }
     }
-    
+}
+
+ll solve() { // O(N^3 / 6 / 16)
     ll ret = 0;
-    if (s1[idx] == s2[idx]) {
-        res += (s1[idx] - '0') * d[idx];
-        ret += cal(idx + 1, x, s1, s2);
-        res -= (s1[idx] - '0') * d[idx];
+    for (int i = 0; i < N; i++) {
+        for (int j = i + 1; j < N; j++) {
+            __m256i Cij = _mm256_set1_epi16(C[i][j]);
+            
+            __m256i acc = _mm256_setzero_si256();
+            for (int k = j + 1; k < N; k += 16) {
+                if (k + 16 <= N) {
+                    __m256i Cik = _mm256_set1_epi16(C[i][k]);
+                    __m256i Cjk = _mm256_set1_epi16(C[j][k]);
+                    __m256i cmp1 = _mm256_cmpeq_epi16(Cik, Cij);
+                    __m256i cmp2 = _mm256_cmpeq_epi16(Cjk, Cij);
 
-        return ret;
+                    __m256i res = _mm256_and_si256(cmp1, cmp2);
+                    acc = _mm256_sub_epi16(acc, res);
+                } else {
+                    for (int kk = k; kk < N; kk++) {
+                        if (C[i][j] == C[i][k] && C[i][j] == C[j][k]) ret++;
+                    }
+                }
+            }
+
+            short tmp[16];
+            _mm256_store_si256((__m256i*)tmp, acc);
+            for (int k = 0; k < 16; k++)
+                ret += tmp[k];
+        }
     }
-    else {        
-        ret += cal(idx + 1, x, s1, s2);
-
-        res += (s1[idx] - '0') * d[idx];
-        ret += cal(idx + 1, x + 1, s1, s2);
-        res -= (s1[idx] - '0') * d[idx];
-
-        res += (s2[idx] - '0') * d[idx];
-        ret += cal(idx + 1, x + 1, s1, s2);
-        res -= (s2[idx] - '0') * d[idx];
-
-        return ret;
-    }
+    return ret;
 }
 
 int main() {
     #ifndef ONLINE_JUDGE
-	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
-	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
-	#endif
+    freopen("./input.txt", "r", stdin);
+    freopen("./output.txt", "w", stdout);
+    #endif
 
     cin.tie(NULL); cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
-    cin >> n;
-    a.resize(n);
-    for (auto& i : a) {
-        cin >> i;
-        bt(0, i);
-    }
+    input();
 
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-            ans += cal(0, 0, a[i], a[j]);
-        }
-    }
+    getC();
 
-    cout << ans / 3;
-
-    return 0;
+    ll ans = solve();
+    cout << ans;
 }
