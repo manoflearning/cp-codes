fb4b2b22e825ef1c1709d8000ac79d76537c5900
Jeong Woo-kyung <77jwk0724@gmail.com>
2024-05-06 22:33:37 +0900

    boj

--- Baekjoon OJ/01000-01999/1704.cpp
+++ Baekjoon OJ/01000-01999/1704.cpp
@@ -6,7 +6,9 @@ const int dx[] = { 0, 1, -1, 0, 0 };
 
 int n, m;
 vector<vector<int>> a;
+
 vector<vector<int>> ans;
+int ans_cnt = 1e9 + 7;
 
 void input() {
     cin >> n >> m;
@@ -18,23 +20,24 @@ void input() {
 void solve() {
     ans.resize(n, vector<int>(m, 2));
 
-    for (int bit = 0; bit < (1 << n); bit++) {
+    for (int bit = 0; bit < (1 << m); bit++) {
         vector<vector<int>> b = a;
         vector<vector<int>> res(n, vector<int>(m));
+        int res_cnt = 0;
 
         // first row
         for (int x = 0; x < m; x++) {
             if (!(bit & (1 << x))) continue;
-            int y = 0;
             for (int d = 0; d < 5; d++) {
-                int ny = y + dy[d], nx = x + dx[d];
+                int ny = 0 + dy[d], nx = x + dx[d];
                 if (ny < 0 || nx < 0 || n <= ny || m <= nx) continue;
                 b[ny][nx] = !b[ny][nx];
             }
-            res[y][x] = 1;
+            res[0][x] = 1;
+            res_cnt++;
         }
 
-        // x(>=2) th row
+        // y(>=1) th row
         for (int y = 1; y < n; y++) {
             for (int x = 0; x < m; x++) {
                 if (!b[y - 1][x]) continue;
@@ -44,18 +47,20 @@ void solve() {
                     b[ny][nx] = !b[ny][nx];
                 }
                 res[y][x] = 1;
+                res_cnt++;
             }
         }
 
         // is it valid?
         bool is_valid = 1;
-        for (int y = 0; y < n; y++) {
-            for (int x = 0; x < m; x++) {
-                if (b[y][x]) is_valid = 0;
-            }
+        for (int x = 0; x < m; x++) {
+            if (b[n - 1][x]) is_valid = 0;
         }
 
-        if (is_valid) ans = min(ans, res);
+        if (is_valid) {
+            if (res_cnt < ans_cnt) ans = res, ans_cnt = res_cnt;
+            else if (res_cnt == ans_cnt) ans = min(ans, res), ans_cnt = res_cnt;
+        }
     }
 }
 
@@ -72,11 +77,6 @@ void output() {
 }
 
 int main() {
-    #ifndef ONLINE_JUDGE
-    freopen("../input.txt", "r", stdin);
-    freopen("../output.txt", "w", stdout);
-    #endif
-
     cin.tie(NULL); cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
