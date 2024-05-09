9af44540f1effa4c15885cb0ab964f17383c22cc
Jeong Woo-kyung <77jwk0724@gmail.com>
2024-05-09 21:08:22 +0900

    boj

---
+++ Baekjoon OJ/01000-01999/1648.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MOD = 9901;
+
+int main() {
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    
+}
