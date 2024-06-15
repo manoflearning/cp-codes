00aab670adf7e7f14a3ecbaeb6726ec4ffb7ac93
Jeong Woo-kyung <77jwk0724@gmail.com>
2024-06-15 22:37:56 +0900

    boj

--- Baekjoon OJ/14000-14999/14252.cpp
+++ Baekjoon OJ/14000-14999/14252.cpp
@@ -2,25 +2,48 @@
 using namespace std;
 #define ll long long
 
-ll gcd(ll x, ll y) {
+int n, a[55];
+
+int gcd(int x, int y) {
     if (!y) return x;
     return gcd(y, x % y);
 }
 
-const int MAX = 100;
-
 int main() {
     #ifndef ONLINE_JUDGE
-    freopen("../input.txt", "r", stdin);
-    freopen("../output.txt", "w", stdout);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
     #endif
 
     cin.tie(NULL); cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
-    for (int l = 1; l <= MAX; l++) {
-        for (int r = l + 1; r <= MAX; r++) {
-            for (int i = l + 1; r <)
+    cin >> n;
+    for (int i = 0; i < n; i++) cin >> a[i];
+
+    sort(a, a + n);
+
+    int ans = 0;
+    for (int i = 0; i + 1 < n; i++) {
+        if (gcd(a[i], a[i + 1]) == 1) continue;
+
+        int res = 3;
+        for (int x = a[i] + 1; x <= a[i + 1] - 1; x++) {
+            if (gcd(x, a[i]) != 1) continue;
+            
+            if (gcd(x, a[i + 1]) == 1) { res = 1; break; }
+
+            for (int y = x + 1; y <= a[i + 1] - 1; y++) {
+                if (res < 3) break;
+                if (gcd(x, y) == 1 && gcd(y, a[i + 1]) == 1) {
+                    res = min(res, 2);
+                    break;
+                }
+            }
         }
+
+        ans += res;
     }
+
+    cout << ans;
 }
