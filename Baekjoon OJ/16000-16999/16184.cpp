9171165f72180b1e9405f5aaefa77a7952089492
Jeong Woo-kyung <77jwk0724@gmail.com>
2024-09-10 18:38:06 +0900

    boj

--- Baekjoon OJ/16000-16999/16184.cpp
+++ Baekjoon OJ/16000-16999/16184.cpp
@@ -5,74 +5,42 @@ using namespace std;
 #define fr first
 #define sc second
 #define sz(x) (int)(x).size()
-#define all(x) (x).begin(), (x).end()
 
 ll n;
 vector<pll> arr;
 unordered_map<ll, ll> idx;
 unordered_map<ll, ll> idx_rev;
-vector<ll> ord;
 
 void init() {
     arr.clear();
     idx.clear();
     idx_rev.clear();
-    ord.clear();
 }
 
-void get_order() {
-    set<ll> vis;
-
-    priority_queue<ll> pq;
-    pq.push(n);
-    vis.insert(n);
-
-    while (!pq.empty()) {
-        ll v = pq.top();
-        pq.pop();
-
-        if (v <= 0) break;
-
-        ord.push_back(v);
-
-        if (v & 1) {
-            if (!vis.count(v / 2)) {
-                vis.insert(v / 2);
-                pq.push(v / 2);
-            }
-            if (!vis.count(v / 2 + 1)) {
-                vis.insert(v / 2 + 1);
-                pq.push(v / 2 + 1);
-            }
-        }
-        else {
-            if (!vis.count(v / 2)) {
-                vis.insert(v / 2);
-                pq.push(v / 2);
-            }
-        }
+void f(ll x) {
+    if (idx.count(x)) return;
+    if (x == 1) {
+        idx[0] = -1;
+        idx_rev[-1] = 0;
+        idx[x] = sz(arr);
+        idx_rev[sz(arr)] = x;
+        arr.push_back({ -1, -1 });
+        return;
     }
+    
+    if (x & 1) {
+        f(x / 2);
+        f(x / 2 + 1);
 
-    reverse(all(ord));
-}
+        idx[x] = sz(arr);
+        idx_rev[sz(arr)] = x;
+        arr.push_back({ idx[x / 2 + 1], idx[x / 2] });
+    } else {
+        f(x / 2);
 
-void solve() {
-    idx[0] = -1;
-    for (auto x : ord) {
-        if (x == 1) {
-            idx_rev[-1] = 0;
-            idx[x] = sz(arr);
-            idx_rev[sz(arr)] = x;
-            arr.push_back({ -1, -1 });
-        } else if (x & 1) {
-            idx[x] = sz(arr);
-            idx_rev[sz(arr)] = x;
-            arr.push_back({ idx[x / 2 + 1], idx[x / 2] });
-        } else {
-            idx[x] = sz(arr);
-            idx_rev[sz(arr)] = x;
-            arr.push_back({ idx[x / 2], idx[x / 2] });
-        }
+        idx[x] = sz(arr);
+        idx_rev[sz(arr)] = x;
+        arr.push_back({ idx[x / 2], idx[x / 2] });
     }
 }
 
@@ -87,24 +55,16 @@ int main() {
 
     int tc; cin >> tc;
     while (tc--) {
-    // for (n = 1e18; n >= 1e18 - 1e4; n--) {
         init();
 
         cin >> n;
 
-        get_order();
-
-        solve();
-
-        assert(sz(arr) <= 125);
+        f(n);
 
         cout << sz(arr) << '\n';
         for (int i = 0; i < sz(arr); i++) {
-            assert(-1 <= arr[i].fr && arr[i].fr <= i - 1);
-            assert(-1 <= arr[i].sc && arr[i].sc <= i - 1);
-            assert(i == 0 || idx_rev[arr[i].fr] + idx_rev[arr[i].sc] == idx_rev[i]);
             cout << arr[i].fr << ' ' << arr[i].sc << '\n';
         }
-        cout << sz(arr) - 1 << '\n';
+        cout << idx[n] << '\n';
     }
 }
