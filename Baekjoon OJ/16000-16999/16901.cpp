ab3f9af349c2c3fe8341f64fa9e5a718d9980d20
Jeong Woo-kyung <77jwk0724@gmail.com>
2024-09-19 20:48:06 +0900

    boj

--- Baekjoon OJ/16000-16999/16901.cpp
+++ Baekjoon OJ/16000-16999/16901.cpp
@@ -1,114 +1,92 @@
-#define _USE_MATH_DEFINES
 #include <bits/stdc++.h>
-#include <cassert>
 using namespace std;
 #define ll long long
-#define ull unsigned long long
-#define ld long double
-#define pii pair<int, int>
-#define pll pair<ll, ll>
-#define fr first 
-#define sc second
-#define vt vector
-#define all(c) (c).begin(), (c).end()
-#define sz(x) (int)(x).size()
 
-#define EXPAND( x ) x // Use this if MS Visual Studio doesn't expand __VA_ARGS__ correctly
-#define F_OR(i, a, b, s) for (int i = (a); (s) > 0 ? i < (b) : i > (b); i += (s))
-#define F_OR1(e) F_OR(i, 0, e, 1)
-#define F_OR2(i, e) F_OR(i, 0, e, 1)
-#define F_OR3(i, b, e) F_OR(i, b, e, 1)
-#define F_OR4(i, b, e, s) F_OR(i, b, e, s)
-#define GET5(a, b, c, d, e, ...) e
-#define F_ORC(...) EXPAND( GET5(__VA_ARGS__, F_OR4, F_OR3, F_OR2, F_OR1) )
-#define FOR(...) EXPAND( F_ORC(__VA_ARGS__ )(__VA_ARGS__) )
-#define EACH(x, a) for (auto& x : a)
+const int MAXN = 202020;
+const ll INF = 1e18;
 
-const double EPS = 1e-9;
-const int INF = 1e9 + 7;
-const int MOD = 1e9 + 7;
-const int dy[] = { 0, 0, 1, -1, 1, 1, -1, -1 };
-const int dx[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
-
-const int MAXD = 31; 
-
-struct Node {
-    int child[2];
+const int MAXC = 2;
+struct trie {
+	trie* child[MAXC];
+	bool term;
+	trie() {
+		fill(child, child + MAXC, nullptr);
+		term = false;
+	}
+	~trie() {
+		for (int i = 0; i < MAXC; i++)
+			if (child[i]) delete child[i];
+	}
+	void insert(int val, int key = 29) {
+		if (key == -1) term = true;
+		else {
+			int next = (val & (1 << key) ? 1 : 0);
+			if (!child[next]) child[next] = new trie;
+			child[next]->insert(val, key - 1);
+		}
+	}
+	ll find(int val, int key = 29) {
+		if (key == -1) return 0;
+		else {
+			int next = (val & (1 << key) ? 1 : 0);
+			if (child[next]) return child[next]->find(val, key - 1);
+            else return (1 << key) + child[!next]->find(val, key - 1);
+		}
+	}
 };
 
 int n;
-vt<ll> a;
-vt<Node> trie(1, { -1, -1 });
+vector<int> a(MAXN);
 
 void input() {
     cin >> n;
-    a.resize(n);
-    EACH(i, a) cin >> i;
-    sort(all(a));
-    a.erase(unique(all(a)), a.end());
+    for (int i = 1; i <= n; i++) cin >> a[i];
 }
 
-void insert(int bit, int x = 0, int key = MAXD) {
-    auto& now = trie[x];
-    if (key == 0) return;
-        
-    int next = (bit & (1 << (key - 1)) ? 1 : 0);
-
-    if (now.child[next] == -1) { 
-        now.child[next] = sz(trie);
-        trie.push_back({ -1, -1 });
+ll dnc(int s, int e, int d) {
+    if (s >= e) return 0;
+    if (d == -1) return 0;
+    
+    int mid = -1;
+    for (int i = s; i + 1 <= e; i++) {
+        if (!(a[i] & (1 << d)) && (a[i + 1] & (1 << d))) {
+            mid = i;
+            break;
+        }
     }
 
-    insert(bit, now.child[next], key - 1);
-}
-
-ll query(ll x, int bit, int bit2, int key) {
-    auto& now = trie[x];
-    if (key == 0) return bit;
-    else {
-        int next = (x & (1 << (key - 1)) ? 1 : 0);
-        if (now.child[next]) return query(x, bit, bit2, key - 1);
-        else return query(x, bit, bit2 | (1 << (key - 1)), key - 1);
-    }
-}
-
-ll solve(int x, int xl, int xr, int prv, int key = MAXD) {
-    if (key == 0) return 0;
+    if (mid == -1) return dnc(s, e, d - 1);
 
     ll ret = 0;
-    auto& now = trie[x];
-    int mid = lower_bound(a.begin() + xl, a.begin() + xr + 1, prv + (1 << (key - 1))) - a.begin() - 1;
 
-    if (now.child[0] != -1) ret += solve(now.child[0], xl, mid, prv, key - 1);
-    if (now.child[1] != -1) ret += solve(now.child[1], mid + 1, xr, prv + (1 << (key - 1)), key - 1);
+    trie* root = new trie;
+    for (int i = s; i <= mid; i++) root->insert(a[i]);
 
-    if (now.child[0] == -1 || now.child[1] == -1) return ret;
-    
-    ll res = 1e18;
-    FOR(i, mid + 1, xr + 1) {
-        res = min(res, query(now.child[0], a[i] - prv - (1 << (key - 1)), 0, key - 1));
+    ll res = INF;
+    for (int i = mid + 1; i <= e; i++) {
+        res = min(res, root->find(a[i]));
     }
+    delete root;
+
     ret += res;
+    ret += dnc(s, mid, d - 1);
+    ret += dnc(mid + 1, e, d - 1);
     
     return ret;
 }
 
 int main() {
-	#ifndef ONLINE_JUDGE
-	// Enter the absolute path of the local file input.txt, output.txt
-	// Or just enter the "input.txt", "output.txt"
-	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
-	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
-	#endif
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
 
-	cin.tie(NULL); cout.tie(NULL);
-	ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
 
-	input();
+    input();
 
-    EACH(i, a) insert(i);
+    sort(a.begin() + 1, a.begin() + 1 + n);
 
-    cout << solve(0, 0, sz(a) - 1, 0);
-
-	return 0;
+    cout << dnc(1, n, 29);
 }
