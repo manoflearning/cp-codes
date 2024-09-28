59920ff329e10ac92f4dac8cec5da50a50be12ae
Jeong Woo-kyung <77jwk0724@gmail.com>
2024-09-28 23:55:46 +0900

    2023 KAIST 13th ICPC Mock Competition

--- Misc/2023 KAIST 13th ICPC Mock Competition/C.cpp
+++ Misc/2023 KAIST 13th ICPC Mock Competition/C.cpp
@@ -3,25 +3,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-#define pii pair<int, int>
 #define sz(x) (int)(x).size()
 #define all(x) (x).begin(), (x).end()
 
-const int MAXN = 2020, MAXN2 = MAXN * MAXN;
+const int MAXN = 2020;
 const int dy[] = { -1, 0, 1, 0 };
 const int dx[] = { 0, -1, 0, 1 };
 const char dir[] = { 'U', 'L', 'D', 'R' };
 
 int n, m, k;
 string a[MAXN];
-vector<int> adj[MAXN2];
-
-int label[MAXN2], labelCnt;
-int SCCnum[MAXN2], SCCcnt, finished[MAXN2];
-stack<int> stk;
-vector<vector<int>> SCC;
-
-int ans = 0;
+vector<int> adj[MAXN * MAXN];
 
 void input() {
     cin >> n >> m >> k;
@@ -33,13 +25,13 @@ void input() {
     a[n + 1] = string(m + 2, '*');
 }
 
-int ah[MAXN][MAXN];
 int Hash(int y, int x) {
     if (y == 0 || y == n + 1) return n * m;
     if (x == 0 || x == m + 1) return n * m;
     return (y - 1) * m + (x - 1);
 }
 
+int ah[MAXN][MAXN];
 void graph_modeling() {
     for (int i = 0; i <= n + 1; i++) {
         for (int j = 0; j <= m + 1; j++) {
@@ -68,33 +60,14 @@ void graph_modeling() {
     assert(edge_cnt <= 4 * (n + 1) * (m + 1));
 }
 
-int dfs(int v) {
-    label[v] = labelCnt++;
-    stk.push(v);
-    int ret = label[v];
-    for (auto& next : adj[v]) {
-        if (label[next] == -1) ret = min(ret, dfs(next));
-        else if (!finished[next]) ret = min(ret, label[next]);
-    }
-    if (ret == label[v]) {
-        SCC.resize(sz(SCC) + 1);
-        while (1) {
-            int t = stk.top();
-            stk.pop();
-            SCC.back().push_back(t);
-            SCCnum[t] = SCCcnt;
-            finished[t] = 1;
-            if (t == v) break;
-        }
-        SCCcnt++;
-    }
+int ans = 0;
+bool vis[MAXN * MAXN];
+void dfs(int v) {
     ans++;
-    return ret;
-}
-
-void getSCC() {
-    memset(label, -1, sizeof(label));
-    dfs(n * m);
+    vis[v] = 1;
+    for (auto& i : adj[v]) {
+        if (!vis[i]) dfs(i);
+    }
 }
 
 int main() {
@@ -110,7 +83,7 @@ int main() {
 
     graph_modeling();
 
-    getSCC();
+    dfs(n * m);
 
     cout << ans - 1;
 }
