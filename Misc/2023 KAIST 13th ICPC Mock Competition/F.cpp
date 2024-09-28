9b1bba88bf03d810ff0f8c916f5171caa3ada6c6
Jeong Woo-kyung <77jwk0724@gmail.com>
2024-09-28 23:59:56 +0900

    2023 KAIST 13th ICPC Mock Competition

--- Misc/2023 KAIST 13th ICPC Mock Competition/F.cpp
+++ Misc/2023 KAIST 13th ICPC Mock Competition/F.cpp
@@ -4,28 +4,8 @@ using namespace std;
 
 const int MAXN = 101010;
 
-int flag;
-struct Seg {
-    vector<int> t;
-    void build(int n) {
-        t.clear();
-        for (flag = 1; flag < n; flag <<= 1);
-        t.resize(flag << 1);
-    }
-    void modify(int p, int val) {
-        for (t[p += flag - 1] = val; p > 1; p >>= 1)
-            t[p >> 1] = max(t[p], t[p ^ 1]);
-    }
-    int query(int l, int r, int n = 1, int nl = 1, int nr = flag) {
-        if (nr < l || r < nl) return 0;
-        if (l <= nl && nr <= r) return t[n];
-        int mid = (nl + nr) >> 1;
-        return max(query(l, r, n << 1, nl, mid), query(l, r, n << 1 | 1, mid + 1, nr));
-    }
-}seg;
-
 int n;
-vector<int> p(MAXN);
+vector<int> p(MAXN), pmax(MAXN);
 
 int main() {
     #ifndef ONLINE_JUDGE
@@ -41,20 +21,18 @@ int main() {
         cin >> n;
         for (int i = 1; i <= n; i++) cin >> p[i];
 
-        seg.build(n);
-        for (int i = 1; i <= n; i++) seg.modify(i, p[i]);
+        for (int i = 1; i <= n; i++) 
+            pmax[i] = max(p[i], pmax[i - 1]);
 
         vector<int> ans;
 
         int idx = 1;
         while (idx < n) {
-            int pmax = seg.query(1, idx);
-            if (idx == pmax) {
+            int val = pmax[idx];
+            if (idx == val) {
                 swap(p[idx], p[idx + 1]);
-                ans.push_back(idx);
-                seg.modify(idx, p[idx]);
-                seg.modify(idx + 1, p[idx + 1]);
-            } else idx = pmax;
+                ans.push_back(idx++);
+            } else idx = val;
         }
 
         cout << sz(ans) << '\n';
