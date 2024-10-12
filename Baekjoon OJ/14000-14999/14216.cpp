1b63cbe21d8f7ef905067852746dee5c0f7da602
Jeong Woo-kyung <77jwk0724@gmail.com>
2024-10-12 18:23:52 +0900

    boj

--- Baekjoon OJ/14000-14999/14216.cpp
+++ Baekjoon OJ/14000-14999/14216.cpp
@@ -1,76 +1,156 @@
-#pragma GCC optimize("O3")
-#pragma GCC optimize("unroll-loops")
 #include <bits/stdc++.h>
 using namespace std;
-#define p pair<int, int>
-#define x first
-#define y second
 
-const int INF = 1e9 + 7;
-const int MAXV = 1010;
+template <typename Cost = long, typename CostSum = Cost>
+auto fast_dense_hungarian(const vector<vector<Cost>>& cost) {
+    static constexpr CostSum inf = numeric_limits<CostSum>::max() / 3;
+    int V = cost.size();
 
-struct MCMF{
-	int s, t; //source, sink
-	struct Edge{ int v, c, d, dual; };
-	vector<Edge> g[MAXV];
-	void addEdge(int s, int e, int c, int d){
-		g[s].push_back({e, c, d, (int)g[e].size()});
-		g[e].push_back({s, 0, -d, (int)g[s].size()-1});
-	}
+    vector<int> row_mate(V, -1), col_mate(V, -1);
+    vector<CostSum> pi(V, 0);
 
-	int dst[MAXV], inq[MAXV];
-	bool spfa(){
-		memset(dst, 0x3f, sizeof dst);
-		memset(inq, 0, sizeof inq);
-		queue<int> q; q.push(s); inq[s] = 1; dst[s] = 0;
-		while(q.size()){
-			int now = q.front(); q.pop(); inq[now] = 0;
-			for(auto i : g[now]){
-				if(i.c && dst[i.v] > dst[now] + i.d){
-					dst[i.v] = dst[now] + i.d;
-					if(!inq[i.v]) inq[i.v] = 1, q.push(i.v);
-				}
-			}
-		}
-		return dst[t] < 1e9;
-	}
+    auto residual = [&](int r, int c) { return cost[r][c] - pi[c]; };
 
-	int chk[MAXV], work[MAXV];
+    // column reduction, mate columns greedily
+    vector<bool> transferrable(V, false);
+    for (int col = 0; col < V; col++) {
+        int row = 0;
+        for (int u = 1; u < V; u++) {
+            if (cost[row][col] > cost[u][col]) {
+                row = u;
+            }
+        }
+        pi[col] = cost[row][col];
+        if (row_mate[row] == -1) {
+            row_mate[row] = col;
+            col_mate[col] = row;
+            transferrable[row] = true;
+        } else {
+            transferrable[row] = false;
+        }
+    }
 
-	int dfs(int now, int fl){
-        chk[now] = 1;
-        if(now == t) return fl;
-        for(; work[now] < g[now].size(); work[now]++){
-            auto &i = g[now][work[now]];
-            if(!chk[i.v] && dst[i.v] == dst[now] + i.d && i.c){
-                int ret = dfs(i.v, min(fl, i.c));
-                if(ret){
-                    i.c -= ret; g[i.v][i.dual].c += ret;
-                    return ret;
+    // reduction transfer
+    for (int row = 0; row < V; row++) {
+        if (transferrable[row]) {
+            int col = row_mate[row];
+            int c = -1;
+            for (int v = 0; v < V; v++) {
+                if (v != col && (c == -1 || residual(row, c) > residual(row, v))) {
+                    c = v;
+                }
+            }
+            pi[col] -= residual(row, c);
+        }
+    }
+
+    // augmenting row reduction
+    for (int it = 0; it < 2; it++) {
+        for (int row = 0; row < V; row++) {
+            if (row_mate[row] != -1) {
+                continue;
+            }
+            auto u1 = residual(row, 0);
+            auto u2 = inf;
+            int c1 = 0;
+            for (int c = 0; c < V; c++) {
+                auto u = residual(row, c);
+                if (u < u1 || (u == u1 && col_mate[c1] != -1)) {
+                    u2 = u1, u1 = u, c1 = c;
+                } else if (u < u2) {
+                    u2 = u;
+                }
+            }
+            if (u1 < u2) {
+                pi[c1] -= u2 - u1;
+            }
+            if (int r1 = col_mate[c1]; r1 != -1) {
+                row_mate[r1] = col_mate[c1] = -1;
+            }
+            row_mate[row] = c1;
+            col_mate[c1] = row;
+        }
+    }
+
+    vector<int> cols(V);
+    iota(begin(cols), end(cols), 0);
+
+    for (int row = 0; row < V; row++) {
+        if (row_mate[row] != -1) {
+            continue;
+        }
+        vector<CostSum> dist(V);
+        for (int c = 0; c < V; c++) {
+            dist[c] = residual(row, c);
+        }
+        vector<int> pred(V, row);
+
+        int scanned = 0, labeled = 0, last = 0;
+        int col = -1;
+
+        while (true) {
+            if (scanned == labeled) {
+                last = scanned;
+                auto min = dist[cols[scanned]];
+                for (int j = scanned; j < V; j++) {
+                    int c = cols[j];
+                    if (dist[c] <= min) {
+                        if (dist[c] < min) {
+                            min = dist[c];
+                            labeled = scanned;
+                        }
+                        swap(cols[j], cols[labeled++]);
+                    }
+                }
+                for (int j = scanned; j < labeled; j++) {
+                    if (int c = cols[j]; col_mate[c] == -1) {
+                        col = c;
+                        goto done;
+                    }
+                }
+            }
+            assert(scanned < labeled);
+            int c1 = cols[scanned++];
+            int r1 = col_mate[c1];
+            for (int j = labeled; j < V; j++) {
+                int c2 = cols[j];
+                auto len = residual(r1, c2) - residual(r1, c1);
+                assert(len >= 0);
+                if (dist[c2] > dist[c1] + len) {
+                    dist[c2] = dist[c1] + len;
+                    pred[c2] = r1;
+                    if (len == 0) {
+                        if (col_mate[c2] == -1) {
+                            col = c2;
+                            goto done;
+                        }
+                        swap(cols[j], cols[labeled++]);
+                    }
                 }
             }
         }
-        return 0;
-	}
-	p run(int _s, int _t){ //{cost, flow}
-		s = _s, t = _t;
-		int cst = 0, fl = 0;
-		while(spfa()){
-			memset(chk, 0, sizeof chk);
-            memset(work, 0, sizeof work);
-			int now = 0;
-			while(now = dfs(s, 1e9)){
-				cst += dst[t] * now;
-				fl += now;
-				memset(chk, 0, sizeof chk);
-			}
-		}
-		return p(cst, fl);
-	}
-} mcmf;
 
-int n, a[505][505];
-int st = 0, en = 1001;
+done:;
+        for (int i = 0; i < last; i++) {
+            int c = cols[i];
+            pi[c] += dist[c] - dist[col];
+        }
+
+        int t = col;
+        while (t != -1) {
+            col = t;
+            int r = pred[col];
+            col_mate[col] = r;
+            swap(row_mate[r], t);
+        }
+    }
+
+    CostSum total = 0;
+    for (int u = 0; u < V; u++) {
+        total += cost[u][row_mate[u]];
+    }
+    return make_pair(total, move(row_mate));
+}
 
 int main() {
     #ifndef ONLINE_JUDGE
@@ -81,15 +161,12 @@ int main() {
     cin.tie(NULL); cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
-    cin >> n;
-    for (int i = 1; i <= n; i++) {
-        mcmf.addEdge(st, i, 1, 0);
-        for (int j = 1; j <= n; j++) {
+    int n; cin >> n;
+    vector<vector<int>> a(n, vector<int>(n));
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
             cin >> a[i][j];
-            mcmf.addEdge(i, 500 + j, 1, a[i][j]);
-        }
-        mcmf.addEdge(500 + i, en, 1, 0);
-    }
 
-    cout << mcmf.run(st, en).x;
+    auto ans = fast_dense_hungarian(a);
+    cout << ans.first;
 }
