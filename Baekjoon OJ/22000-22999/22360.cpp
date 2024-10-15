7fab74808e00ef0f0bb0dea8ea8199e0c0680643
Jeong Woo-kyung <77jwk0724@gmail.com>
2024-10-15 16:10:40 +0900

    boj

--- Baekjoon OJ/22000-22999/22360.cpp
+++ Baekjoon OJ/22000-22999/22360.cpp
@@ -4,41 +4,31 @@ using namespace std;
 #define fr first
 #define sc second
 
-const int MAXN = 202020;
-
 int n;
 string s;
-vector<int> adj[MAXN];
-
-void input() {
-    cin >> n >> s;
-    s = "*" + s;
-    for (int i = 0; i < n - 1; i++) {
-        int u, v;
-        cin >> u >> v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-    }
-}
-
-int siz[MAXN];
-bool used[MAXN];
 ll ans = 0;
 
-int get_size(int v, int prv) {
-    siz[v] = 1;
-    for (auto& i : adj[v]) {
-        if (!used[i] && i != prv)
-            siz[v] += get_size(i, v);
-    }
-    return siz[v];
+namespace CD {
+const int MAXN = 202020;
+
+vector<int> adj[MAXN], cdchd[MAXN];
+int siz[MAXN], cdpar[MAXN];
+bool used[MAXN];
+
+int getSize(int v, int prv) {
+  siz[v] = 1;
+  for (auto i : adj[v]) {
+    if (used[i] || prv == i) continue;
+    siz[v] += getSize(i, v);
+  }
+  return siz[v];
 }
-int get_cent(int v, int prv, int mxsiz) {
-    for (auto& i : adj[v]) {
-        if (used[i] || i == prv) continue;
-        if (siz[i] > mxsiz / 2) return get_cent(i, v, mxsiz);
-    }
-    return v;
+int getCent(int v, int prv, int mxsiz) {
+  for (auto &i : adj[v]) {
+    if (used[i] || i == prv) continue;
+    if (siz[i] > mxsiz / 2) return getCent(i, v, mxsiz);
+  }
+  return v;
 }
 void dfs(int v, int prv, int u, int p, int c, map<tuple<int, int>, ll>& mp) {
     u += (s[v] == 'U');
@@ -51,10 +41,9 @@ void dfs(int v, int prv, int u, int p, int c, map<tuple<int, int>, ll>& mp) {
     }
 }
 void cd(int v, int prv) {
-    int mxsiz = get_size(v, prv);
-    int cent = get_cent(v, prv, mxsiz);
-
-    map<tuple<int, int>, ll> acc;
+  int mxsiz = getSize(v, prv);
+  int cent = getCent(v, prv, mxsiz);
+  map<tuple<int, int>, ll> acc;
     acc[{ (s[cent] == 'U') - (s[cent] == 'P'), (s[cent] == 'U') + (s[cent] == 'P') - (s[cent] == 'C') }]++;
 
     for (auto& i : adj[cent]) {
@@ -78,10 +67,21 @@ void cd(int v, int prv) {
             acc[nkey] += val;
         }
     }
+  used[cent] = 1;
+  for (auto i : adj[cent])
+    if (!used[i]) cd(i, cent);
+}
+}
 
-    used[cent] = 1;
-    for (auto& i : adj[cent])
-        if (!used[i]) cd(i, v);
+void input() {
+    cin >> n >> s;
+    s = "*" + s;
+    for (int i = 0; i < n - 1; i++) {
+        int u, v;
+        cin >> u >> v;
+        CD::adj[u].push_back(v);
+        CD::adj[v].push_back(u);
+    }
 }
 
 int main() {
@@ -95,7 +95,7 @@ int main() {
 
     input();
 
-    cd(1, 0);
+    CD::cd(1, 0);
 
     cout << ans;
 }
