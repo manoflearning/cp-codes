e47d0ce875bbbcdd310ccf7fb51a273a0a349760
Jeong Woo-kyung <77jwk0724@gmail.com>
2025-04-02 03:50:36 +0900

    icpc

--- ICPC/ICPC Central Russia Regional Contest, 2023/D.cpp
+++ ICPC/ICPC Central Russia Regional Contest, 2023/D.cpp
@@ -1,51 +1,54 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-#define pii pair<int, int>
+#define pll pair<ll, ll>
 #define fr first
 #define sc second
 #define sz(x) (int)(x).size()
 #define all(x) (x).begin(), (x).end()
 
-int n;
-string s;
-map<char, char> mp;
+const int MOD1 = 1e9 + 7;
+const int MOD2 = 1e9 + 9;
+const int MAX = 101010;
+const int D = 27;
 
-vector<int> p; // p[i]: the radius of the palindrome at the current position i
+string s, t;
+ll dexp1[MAX], dexp2[MAX];
+ll hs1[MAX], hs2[MAX];
+ll ht1[MAX], ht2[MAX];
 
-void manacher() {
-    mp['A'] = 'T';
-    mp['T'] = 'A';
-    mp['G'] = 'C';
-    mp['C'] = 'G';
-    mp['#'] = '#';
-
-  // Preprocessing for determining even-length pelindromes
-  n = sz(s);
-  s.resize(n << 1 | 1);
-  p.resize(n << 1 | 1);
-  for (int i = n - 1; i >= 0; i--) {
-    s[i << 1 | 1] = s[i];
-    s[i << 1] = '#';
-  }
-  n <<= 1;
-  s[n++] = '#';
-  // Processing
-  int r = -1, c = -1;
-  // r: end of palindrome
-  // c: center of palindrome
-  for (int i = 0; i < n; i += 2) {
-    if (i <= r) p[i] = min(r - i, p[c * 2 - i]);
-    else p[i] = 0;
-    while (1) {
-      if (i - p[i] - 1 < 0 || i + p[i] + 1 >= n) break;
-      if (mp[s[i + p[i] + 1]] != s[i - p[i] - 1]) break;
-      p[i]++;
+void rabin_karp() {
+    dexp1[0] = dexp2[0] = 1;
+    for (int i = 1; i <= sz(s); i++) {
+        dexp1[i] = D * dexp1[i - 1] % MOD1;
+        dexp2[i] = D * dexp2[i - 1] % MOD2;
     }
-    if (i + p[i] > r) {
-      r = i + p[i], c = i;
+
+    for (int i = 0; i < sz(s); i++) {
+        hs1[i + 1] = (D * hs1[i] + s[i] - 'A') % MOD1;
+        hs2[i + 1] = (D * hs2[i] + s[i] - 'A') % MOD2;
+
+        ht1[i + 1] = (D * ht1[i] + t[i] - 'A') % MOD1;
+        ht2[i + 1] = (D * ht2[i] + t[i] - 'A') % MOD2;
     }
-  }
+}
+
+int get_start(int len) {
+    unordered_set<ll> vis;
+    for (int i = len; i <= sz(s); i++) {
+        ll res1 = ((hs1[i] - hs1[i - len] * dexp1[len]) % MOD1 + MOD1) % MOD1;
+        ll res2 = ((hs2[i] - hs2[i - len] * dexp2[len]) % MOD2 + MOD2) % MOD2;
+        vis.insert(res1 + res2 * MOD1);
+    }
+
+    for (int i = len; i <= sz(t); i++) {
+        ll res1 = ((ht1[i] - ht1[i - len] * dexp1[len]) % MOD1 + MOD1) % MOD1;
+        ll res2 = ((ht2[i] - ht2[i - len] * dexp2[len]) % MOD2 + MOD2) % MOD2;
+        if (vis.count(res1 + res2 * MOD1)) return i - len;
+    }
+    return -1;
 }
 
 int main() {
@@ -58,28 +61,27 @@ int main() {
     ios_base::sync_with_stdio(false);
 
     cin >> s;
-
-    manacher();
-
-    int mx = 0, mx_idx = -1;
-    for (int i = 0; i < sz(p); i += 2) {
-        if (p[i] > mx) {
-            mx = p[i];
-            mx_idx = i;
-        }
+    t = s;
+    reverse(all(t));
+    for (int i = 0; i < sz(t); i++) {
+        if (t[i] == 'A') t[i] = 'T';
+        else if (t[i] == 'G') t[i] = 'C';
+        else if (t[i] == 'C') t[i] = 'G';
+        else if (t[i] == 'T') t[i] = 'A';
+        else assert(0);
     }
-    
-    if (mx == 0) {
-        cout << 0;
-    } else {
-        assert(!(mx & 1));
-        int len = mx / 2;
-        int idx = mx_idx / 2;
-        cout << mx << '\n';
-        for (int i = idx - len; i < idx + len; i++) {
-            int j = i << 1 | 1;
-            assert(0 <= j && j < sz(s));
-            cout << s[j];
-        }
+
+    rabin_karp();
+
+    int l = 0, r = sz(s), ans = -1;
+    while (l < r) {
+        int mid = (l + r + 1) >> 1;
+
+        int res = get_start(mid);
+        if (res == -1) r = mid - 1;
+        else l = mid, ans = res;
     }
+
+    cout << l << '\n';
+    if (l) cout << s.substr(ans, l);
 }
